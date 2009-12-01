1610e1cba7b1753117a231fefbf1c184d38a12b8
clifford <clifford@b57f626f-c46c-0410-a088-ec61d464b74c>
2009-12-01 19:56:22 +0000

    Marius Kintel: 	minor macosx packaging patch
    git-svn-id: http://svn.clifford.at/openscad/trunk@141 b57f626f-c46c-0410-a088-ec61d464b74c

--- release-macosx.sh
+++ release-macosx.sh
@@ -1,24 +1,32 @@
 #!/bin/sh
 # WARNING: This script might only work with the authors setup...
 
-mkdir openscad.app/Contents/Frameworks
-cp ../OpenCSG-1.1.1/lib/libopencsg.dylib openscad.app/Contents/Frameworks
-cp /opt/local/lib/libGLEW.1.5.1.dylib openscad.app/Contents/Frameworks
-cp /Library/Frameworks/QtOpenGL.framework/Versions/4/QtOpenGL openscad.app/Contents/Frameworks
-cp /Library/Frameworks/QtGui.framework/Versions/4/QtGui openscad.app/Contents/Frameworks
-cp /Library/Frameworks/QtCore.framework/Versions/4/QtCore openscad.app/Contents/Frameworks
-install_name_tool -change libopencsg.1.dylib @executable_path/../Frameworks/libopencsg.dylib openscad.app/Contents/MacOS/openscad
-install_name_tool -change QtOpenGL.framework/Versions/4/QtOpenGL @executable_path/../Frameworks/QtOpenGL openscad.app/Contents/MacOS/openscad
-install_name_tool -change QtGui.framework/Versions/4/QtGui @executable_path/../Frameworks/QtGui openscad.app/Contents/MacOS/openscad
-install_name_tool -change QtCore.framework/Versions/4/QtCore @executable_path/../Frameworks/QtCore openscad.app/Contents/MacOS/openscad
-install_name_tool -change QtGui.framework/Versions/4/QtGui @executable_path/../Frameworks/QtGui openscad.app/Contents/Frameworks/QtOpenGL 
-install_name_tool -change QtCore.framework/Versions/4/QtCore @executable_path/../Frameworks/QtCore openscad.app/Contents/Frameworks/QtOpenGL 
-install_name_tool -change QtCore.framework/Versions/4/QtCore @executable_path/../Frameworks/QtCore openscad.app/Contents/Frameworks/QtGui
- install_name_tool -change /opt/local/lib/libGLEW.1.5.1.dylib @executable_path/../Frameworks/libGLEW.1.5.1.dylib openscad.app/Contents/MacOS/openscad
-install_name_tool -change /opt/local/lib/libGLEW.1.5.1.dylib @executable_path/../Frameworks/libGLEW.1.5.1.dylib openscad.app/Contents/Frameworks/libopencsg.dylib
+VERSION=2009.11
 
-mkdir -p openscad/examples
-cp examples/* openscad/examples/
-chmod -R 644 openscad/examples/*
-mv OpenSCAD.app openscad
+echo "Preparing executable.."
+mkdir OpenSCAD.app/Contents/Frameworks
+cp ../OpenCSG-1.1.1/lib/libopencsg.dylib OpenSCAD.app/Contents/Frameworks
+cp /opt/local/lib/libGLEW.1.5.1.dylib OpenSCAD.app/Contents/Frameworks
+cp /Library/Frameworks/QtOpenGL.framework/Versions/4/QtOpenGL OpenSCAD.app/Contents/Frameworks
+cp /Library/Frameworks/QtGui.framework/Versions/4/QtGui OpenSCAD.app/Contents/Frameworks
+cp /Library/Frameworks/QtCore.framework/Versions/4/QtCore OpenSCAD.app/Contents/Frameworks
+install_name_tool -change libopencsg.1.dylib @executable_path/../Frameworks/libopencsg.dylib OpenSCAD.app/Contents/MacOS/openscad
+install_name_tool -change QtOpenGL.framework/Versions/4/QtOpenGL @executable_path/../Frameworks/QtOpenGL OpenSCAD.app/Contents/MacOS/openscad
+install_name_tool -change QtGui.framework/Versions/4/QtGui @executable_path/../Frameworks/QtGui OpenSCAD.app/Contents/MacOS/openscad
+install_name_tool -change QtCore.framework/Versions/4/QtCore @executable_path/../Frameworks/QtCore OpenSCAD.app/Contents/MacOS/openscad
+install_name_tool -change QtGui.framework/Versions/4/QtGui @executable_path/../Frameworks/QtGui OpenSCAD.app/Contents/Frameworks/QtOpenGL 
+install_name_tool -change QtCore.framework/Versions/4/QtCore @executable_path/../Frameworks/QtCore OpenSCAD.app/Contents/Frameworks/QtOpenGL 
+install_name_tool -change QtCore.framework/Versions/4/QtCore @executable_path/../Frameworks/QtCore OpenSCAD.app/Contents/Frameworks/QtGui
+ install_name_tool -change /opt/local/lib/libGLEW.1.5.1.dylib @executable_path/../Frameworks/libGLEW.1.5.1.dylib OpenSCAD.app/Contents/MacOS/openscad
+install_name_tool -change /opt/local/lib/libGLEW.1.5.1.dylib @executable_path/../Frameworks/libGLEW.1.5.1.dylib OpenSCAD.app/Contents/Frameworks/libopencsg.dylib
 
+echo "Creating directory structure.."
+rm -rf openscad-$VERSION
+mkdir -p openscad-$VERSION/examples
+cp examples/* openscad-$VERSION/examples/
+chmod -R 644 openscad-$VERSION/examples/*
+mv OpenSCAD.app openscad-$VERSION
+
+echo "Creating archive.."
+zip -qr openscad-$VERSION.zip openscad-$VERSION
+echo "Mac OS X binary created: openscad-$VERSION.zip"
