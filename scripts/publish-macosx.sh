3d10ddb00612154f1205ef26ee629fb7f264d8ed
kintel <kintel@b57f626f-c46c-0410-a088-ec61d464b74c>
2010-01-30 18:33:23 +0000

    minor publish bug
    git-svn-id: http://svn.clifford.at/openscad/trunk@377 b57f626f-c46c-0410-a088-ec61d464b74c

--- scripts/publish-macosx.sh
+++ scripts/publish-macosx.sh
@@ -5,7 +5,7 @@ VERSION=`date "+%Y.%m.%d"`
 
 export OPENCSGDIR=$PWD/../OpenCSG-1.2.0
 
-./release-common.sh -v $VERSION
+`dirname $0`/release-common.sh -v $VERSION
 cp OpenSCAD-$VERSION.dmg ~/Documents/Dropbox/Public
 ln -sf OpenSCAD-$VERSION.dmg ~/Documents/Dropbox/Public/OpenSCAD-latest.dmg
 
