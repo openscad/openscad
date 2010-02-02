eaedac975ca01b28cb4cf35c270bbca820452831
clifford <clifford@b57f626f-c46c-0410-a088-ec61d464b74c>
2010-02-02 19:33:39 +0000

    Clifford Wolf: 	Cross-platform fixes for scripts/batch-2d.sh
    git-svn-id: http://svn.clifford.at/openscad/trunk@413 b57f626f-c46c-0410-a088-ec61d464b74c

--- scripts/batch-2d.sh
+++ scripts/batch-2d.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
+cmd="openscad"
+[ -x "./openscad" ] && cmd="./openscad"
+[ -x "./OpenSCAD.app/Contents/MacOS/OpenSCAD" ] && cmd="./OpenSCAD.app/Contents/MacOS/OpenSCAD"
+
 mkdir -p output
 for f in testdata/*.dxf; do
   echo `basename $f`
   cat > tmp.scad << EOF
   import_dxf("$f");
 EOF
-  ./OpenSCAD.app/Contents/MacOS/OpenSCAD -x output/`basename $f` tmp.scad
+  "$cmd" -x output/`basename $f` tmp.scad
 done
+
