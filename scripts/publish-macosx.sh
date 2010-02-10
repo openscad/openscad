393bff745b8fbd45059d92aea90d25c9db8d309a
kintel <kintel@b57f626f-c46c-0410-a088-ec61d464b74c>
2010-02-10 00:40:44 +0000

    OpenCSG -> 1.3.0
    git-svn-id: http://svn.clifford.at/openscad/trunk@426 b57f626f-c46c-0410-a088-ec61d464b74c

--- scripts/publish-macosx.sh
+++ scripts/publish-macosx.sh
@@ -3,7 +3,7 @@
 VERSION=`date "+%Y.%m.%d"`
 #VERSION=2010.02
 
-export OPENCSGDIR=$PWD/../OpenCSG-1.2.0
+export OPENCSGDIR=$PWD/../OpenCSG-1.3.0
 
 `dirname $0`/release-common.sh -v $VERSION
 cp OpenSCAD-$VERSION.dmg ~/Documents/Dropbox/Public
