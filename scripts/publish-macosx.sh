0046756d9634df65a64cb6c471e71ec773853438
kintel <kintel@b57f626f-c46c-0410-a088-ec61d464b74c>
2010-02-15 18:38:54 +0000

    Specify EIGEN2DIR
    git-svn-id: http://svn.clifford.at/openscad/trunk@439 b57f626f-c46c-0410-a088-ec61d464b74c

--- scripts/publish-macosx.sh
+++ scripts/publish-macosx.sh
@@ -4,6 +4,7 @@ VERSION=`date "+%Y.%m.%d"`
 #VERSION=2010.02
 
 export OPENCSGDIR=$PWD/../OpenCSG-1.3.0
+export EIGEN2DIR=/opt/local/include/eigen2
 
 `dirname $0`/release-common.sh -v $VERSION
 if [[ $? != 0 ]]; then
