419e8900fcb274f99606fe9cbeb4298e957c3efc
kintel <kintel@b57f626f-c46c-0410-a088-ec61d464b74c>
2010-03-31 14:47:43 +0000

    Install CGAL in a version-tagged directory
    git-svn-id: http://svn.clifford.at/openscad/trunk@499 b57f626f-c46c-0410-a088-ec61d464b74c

--- scripts/publish-macosx.sh
+++ scripts/publish-macosx.sh
@@ -4,7 +4,7 @@ VERSION=`date "+%Y.%m.%d"`
 #VERSION=2010.02
 
 export OPENCSGDIR=$PWD/../OpenCSG-1.3.0
-export CGALDIR=$PWD/../install
+export CGALDIR=$PWD/../install/CGAL-3.6
 export EIGEN2DIR=/opt/local/include/eigen2
 
 `dirname $0`/release-common.sh -v $VERSION
--- setenv_mjau.sh
+++ setenv_mjau.sh
@@ -1,3 +1,3 @@
 export OPENCSGDIR=$PWD/../OpenCSG-1.3.0
-export CGALDIR=$PWD/../install
+export CGALDIR=$PWD/../install/CGAL-3.6
 export DYLD_LIBRARY_PATH=$OPENCSGDIR/lib:$QCODEEDITDIR/lib
