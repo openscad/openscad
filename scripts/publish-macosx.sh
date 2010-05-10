203858f6b508f10743960efa88221e65deff9e91
kintel <kintel@b57f626f-c46c-0410-a088-ec61d464b74c>
2010-05-10 17:42:27 +0000

    Autorun sanity check before publising
    git-svn-id: http://svn.clifford.at/openscad/trunk@541 b57f626f-c46c-0410-a088-ec61d464b74c

--- scripts/publish-macosx.sh
+++ scripts/publish-macosx.sh
@@ -9,6 +9,10 @@ export MACOSX_DEPLOY_DIR=$PWD/../libraries/deploy
 if [[ $? != 0 ]]; then
   exit 1
 fi
+`dirname $0`/macosx-sanity-check.py OpenSCAD.app/Contents/MacOS/OpenSCAD
+if [[ $? != 0 ]]; then
+  exit 1
+fi
 cp OpenSCAD-$VERSION.dmg ~/Documents/Dropbox/Public
 ln -sf OpenSCAD-$VERSION.dmg ~/Documents/Dropbox/Public/OpenSCAD-latest.dmg
 
