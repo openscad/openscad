1edad39205480dde6a5e1809a68b053ef8ddee99
clifford <clifford@b57f626f-c46c-0410-a088-ec61d464b74c>
2010-05-27 07:53:35 +0000

    Clifford Wolf: 	Post release stuff
    git-svn-id: http://svn.clifford.at/openscad/trunk@548 b57f626f-c46c-0410-a088-ec61d464b74c

--- scripts/release-linux.sh
+++ scripts/release-linux.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # WARNING: This script might only work with the authors setup...
 
-#VERSION=`date "+%Y.%m.%d"`
-VERSION=2010.05
+VERSION=`date "+%Y.%m.%d"`
+#VERSION=2010.05
 
 set -ex
 
