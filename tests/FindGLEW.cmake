2c4b2e5723d06cc9ad64d298fec5064ba9f36b49
Marius Kintel <marius@kintel.net>
2010-11-17 01:26:22 -0500

    Prefer our glew over macports

--- tests/FindGLEW.cmake
+++ tests/FindGLEW.cmake
@@ -20,26 +20,23 @@ IF (WIN32)
 		${PROJECT_SOURCE_DIR}/src/nvgl/glew/lib
 		DOC "The GLEW library")
 ELSE (WIN32)
+     message(${GLEW_DIR})
 	FIND_PATH( GLEW_INCLUDE_PATH GL/glew.h
-		/usr/include
-		/usr/local/include
-		/sw/include
-		/opt/local/include
+                PATHS
+                ${GLEW_DIR}/include
+                NO_DEFAULT_PATH
 		DOC "The directory where GL/glew.h resides")
 	FIND_LIBRARY( GLEW_LIBRARY
 		NAMES GLEW glew
 		PATHS
-		/usr/lib64
-		/usr/lib
-		/usr/local/lib64
-		/usr/local/lib
-		/sw/lib
-		/opt/local/lib
+                ${GLEW_DIR}/lib
+                NO_DEFAULT_PATH
 		DOC "The GLEW library")
 ENDIF (WIN32)
 
 IF (GLEW_INCLUDE_PATH)
 	SET( GLEW_FOUND 1 CACHE STRING "Set to 1 if GLEW is found, 0 otherwise")
+        MESSAGE(STATUS "GLEW found in " ${GLEW_INCLUDE_PATH} " " ${GLEW_LIBRARY})
 ELSE (GLEW_INCLUDE_PATH)
 	SET( GLEW_FOUND 0 CACHE STRING "Set to 1 if GLEW is found, 0 otherwise")
 ENDIF (GLEW_INCLUDE_PATH)
