438d5cfbc80b128f49c31e57749f937fecfe01b2
meta <s@w23.at>
2011-01-18 00:52:45 +0100

    add some more missing C99 math.h functions

--- src/mathc99.h
+++ src/mathc99.h
@@ -6,6 +6,8 @@
 #include <cmath>
 //for native win32 builds we need to provide C99 math functions by ourselves
 double round(double a);
+float fmin(float a, float b);
+float fmax(float a, float b);
 
 #else
 
