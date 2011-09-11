d25e197ac7d72214bbdc2753ef84104932c625dd
Marius Kintel <marius@kintel.net>
2011-09-11 11:07:18 +0200

    Forgot to commit CGALCache class

---
+++ src/CGALCache.cc
@@ -0,0 +1,17 @@
+#include "CGALCache.h"
+#include "printutils.h"
+#include "CGAL_Nef_polyhedron.h"
+
+CGALCache *CGALCache::inst = NULL;
+
+void CGALCache::insert(const std::string &id, const CGAL_Nef_polyhedron &N)
+{
+	this->cache.insert(id, new CGAL_Nef_polyhedron(N), N.weight());
+	PRINTF("CGAL Cache insert: %s (%d verts)", id.substr(0, 40).c_str(), N.weight());
+}
+
+void CGALCache::print()
+{
+	PRINTF("CGAL Polyhedrons in cache: %d", this->cache.size());
+	PRINTF("Vertices in cache: %d", this->cache.totalCost());
+}
