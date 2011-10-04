46ccaf371609cf8a32753c75db5dd3eb35ae27c0
Marius Kintel <marius@kintel.net>
2011-10-04 21:06:57 +0200

    merged

--- src/linalg.h
+++ src/linalg.h
@@ -1,6 +1,11 @@
 #ifndef LINALG_H_
 #define LINALG_H_
 
+#ifndef __APPLE__
+#define EIGEN_DONT_VECTORIZE 1
+#define EIGEN_DISABLE_UNALIGNED_ARRAY_ASSERT 1
+#endif
+
 #include <Eigen/Core>
 #include <Eigen/Geometry>
 
