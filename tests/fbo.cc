872fc643218ef642544612954272e89629e145c5
Marius Kintel <marius@kintel.net>
2011-10-06 01:51:54 +0200

    We don't need to use the EXT version of FBO calls

--- tests/fbo.cc
+++ tests/fbo.cc
@@ -23,32 +23,32 @@ bool fbo_init(fbo_t *fbo, size_t width, size_t height)
   }
 
   // Generate and bind FBO
-  glGenFramebuffersEXT(1, &fbo->fbo_id);
-  glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, fbo->fbo_id);
+  glGenFramebuffers(1, &fbo->fbo_id);
+  glBindFramebuffer(GL_FRAMEBUFFER, fbo->fbo_id);
   REPORTGLERROR("binding framebuffer");
 
   // Generate depth and render buffers
-  glGenRenderbuffersEXT(1, &fbo->depthbuf_id);
-  glGenRenderbuffersEXT(1, &fbo->renderbuf_id);
+  glGenRenderbuffers(1, &fbo->depthbuf_id);
+  glGenRenderbuffers(1, &fbo->renderbuf_id);
 
   // Create buffers with correct size
   if (!fbo_resize(fbo, width, height)) return false;
 
   // Attach render and depth buffers
-  glFramebufferRenderbufferEXT(GL_FRAMEBUFFER_EXT, GL_COLOR_ATTACHMENT0_EXT, 
-                               GL_RENDERBUFFER_EXT, fbo->renderbuf_id);
+  glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, 
+                               GL_RENDERBUFFER, fbo->renderbuf_id);
   REPORTGLERROR("specifying color render buffer");
 
-  if (glCheckFramebufferStatusEXT(GL_FRAMEBUFFER_EXT) != GL_FRAMEBUFFER_COMPLETE_EXT) {
+  if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
     fprintf(stderr, "Problem with OpenGL framebuffer after specifying color render buffer.\n");
     return false;
   }
 
-  glFramebufferRenderbufferEXT(GL_FRAMEBUFFER_EXT, GL_DEPTH_ATTACHMENT_EXT, 
-                               GL_RENDERBUFFER_EXT, fbo->depthbuf_id);
+  glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, 
+                               GL_RENDERBUFFER, fbo->depthbuf_id);
   REPORTGLERROR("specifying depth render buffer");
 
-  if (glCheckFramebufferStatusEXT(GL_FRAMEBUFFER_EXT) != GL_FRAMEBUFFER_COMPLETE_EXT) {
+  if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
     fprintf(stderr, "Problem with OpenGL framebuffer after specifying depth render buffer.\n");
     return false;
   }
@@ -58,12 +58,12 @@ bool fbo_init(fbo_t *fbo, size_t width, size_t height)
 
 bool fbo_resize(fbo_t *fbo, size_t width, size_t height)
 {
-  glBindRenderbufferEXT(GL_RENDERBUFFER_EXT, fbo->depthbuf_id);
-  glRenderbufferStorageEXT(GL_RENDERBUFFER_EXT, GL_DEPTH_COMPONENT24, width, height);
+  glBindRenderbuffer(GL_RENDERBUFFER, fbo->depthbuf_id);
+  glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT24, width, height);
   REPORTGLERROR("creating depth render buffer");
 
-  glBindRenderbufferEXT(GL_RENDERBUFFER_EXT, fbo->renderbuf_id);
-  glRenderbufferStorageEXT(GL_RENDERBUFFER_EXT, GL_RGBA8, width, height);
+  glBindRenderbuffer(GL_RENDERBUFFER, fbo->renderbuf_id);
+  glRenderbufferStorage(GL_RENDERBUFFER, GL_RGBA8, width, height);
   REPORTGLERROR("creating color render buffer");
 
   return true;
@@ -77,11 +77,11 @@ void fbo_delete(fbo_t *fbo)
 GLuint fbo_bind(fbo_t *fbo)
 {
   glGetIntegerv(GL_FRAMEBUFFER_BINDING, (GLint *)&fbo->old_fbo_id);
-  glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, fbo->fbo_id);
+  glBindFramebuffer(GL_FRAMEBUFFER, fbo->fbo_id);
   return fbo->old_fbo_id;
 }
 
 void fbo_unbind(fbo_t *fbo)
 {
-  glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, fbo->old_fbo_id);
+  glBindFramebuffer(GL_FRAMEBUFFER, fbo->old_fbo_id);
 }
