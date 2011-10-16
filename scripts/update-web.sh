6acf0b5019d8b68d81f6c1facd6f889c1f468b53
Brad Pitcher <bradpitcher@gmail.com>
2011-10-16 00:39:17 -0700

    support mingw win32 builds

--- scripts/update-web.sh
+++ scripts/update-web.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
-dmgfile=$1
-if [ -z "$dmgfile" ]; then
-  echo "Usage: $0 <dmgfile>"
+# Works with Mac OS X and Linux cross-compiling for windows using
+# mingw-cross-env (use like: OS=LINXWIN update-web.sh file1.zip file2.exe).
+
+file1=$1
+if [ -z "$file1" ]; then
+  echo "Usage: $0 <file1> [<file2>]"
   exit 1
 fi
+
+if [[ $OSTYPE =~ "darwin" ]]; then
+  OS=MACOSX
+fi
+
 indexfile=../openscad.github.com/index.html
 if [ -f $indexfile ]; then
-  sed -i .backup -e "s/^\(.*mac-snapshot.*\)\(OpenSCAD-.*\.dmg\)\(.*\)\(OpenSCAD-.*dmg\)\(.*$\)/\\1$dmgfile\\3$dmgfile\\5/" $indexfile
+  if [ $OS == MACOSX ]; then
+    sed -i .backup -e "s/^\(.*mac-snapshot.*\)\(OpenSCAD-.*\.dmg\)\(.*\)\(OpenSCAD-.*dmg\)\(.*$\)/\\1$file1\\3$file1\\5/" $indexfile
+  elif [ $OS == LINXWIN ]; then
+    file2=$2
+    sed -i .backup -e "s/^\(.*win-snapshot-zip.*\)\(OpenSCAD-.*\.zip\)\(.*\)\(OpenSCAD-.*zip\)\(.*$\)/\\1$file1\\3$file1\\5/" $indexfile
+    sed -i .backup -e "s/^\(.*win-snapshot-exe.*\)\(OpenSCAD-.*-Installer\.exe\)\(.*\)\(OpenSCAD-.*-Installer.exe\)\(.*$\)/\\1$file2\\3$file2\\5/" $indexfile
+  fi
   echo "Web page updated. Remember to commit and push openscad.github.com"
 else
   echo "Web page not found at $indexfile"
