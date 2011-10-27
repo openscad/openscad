6df123f80e5872e5a3d01e68f497b3360dee6ad5
Marius Kintel <marius@kintel.net>
2011-10-27 15:31:12 +0200

    Better GL error reporting

--- tests/system-gl.cc
+++ tests/system-gl.cc
@@ -32,22 +32,11 @@ void glew_dump() {
        << endl;
 };
 
-const char * gl_errors[] = {
-  "GL_INVALID_ENUM", // 0x0500
-  "GL_INVALID_VALUE", // 0x0501
-  "GL_INVALID_OPERATION", // 0x0502
-  "GL_OUT_OF_MEMORY" // 0x0503
-};
-
 bool report_glerror(const char * function)
 {
   GLenum tGLErr = glGetError();
   if (tGLErr != GL_NO_ERROR) {
-    if ( (tGLErr-0x500)<=3 && (tGLErr-0x500)>=0 )
-      cerr << "OpenGL error 0x" << hex << tGLErr << " ("
-           << gl_errors[tGLErr-0x500] << ") after " << function << endl;
-    else
-      cerr << "OpenGL error 0x" << hex << tGLErr << " after " << function << endl;
+    cerr << "OpenGL error 0x" << hex << tGLErr << ": " << gluErrorString(tGLErr) << " after " << function << endl;
     return true;
   }
   return false;
