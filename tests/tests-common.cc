bd714c75a8f03257271dc8cba90d22b3730775cd
Marius Kintel <marius@kintel.net>
2012-05-02 12:07:56 -0400

    Ported away from Qt

--- tests/tests-common.cc
+++ tests/tests-common.cc
@@ -2,31 +2,25 @@
 #include "openscad.h"
 #include "module.h"
 #include "handle_dep.h"
+#include "boosty.h"
 
-#include <QFile>
-#include <QFileInfo>
 #include <sstream>
+#include <fstream>
 
 Module *parsefile(const char *filename)
 {
 	Module *root_module = NULL;
 
-	QFileInfo fileInfo(filename);
 	handle_dep(filename);
-	FILE *fp = fopen(filename, "rt");
-	if (!fp) {
+	std::ifstream ifs(filename);
+	if (!ifs.is_open()) {
 		fprintf(stderr, "Can't open input file `%s'!\n", filename);
-	} else {
-		std::stringstream text;
-		char buffer[513];
-		int ret;
-		while ((ret = fread(buffer, 1, 512, fp)) > 0) {
-			buffer[ret] = 0;
-			text << buffer;
-		}
-		fclose(fp);
-		text << "\n" << commandline_commands;
-		root_module = parse(text.str().c_str(), fileInfo.absolutePath().toLocal8Bit(), false);
+	}
+	else {
+		std::string text((std::istreambuf_iterator<char>(ifs)), std::istreambuf_iterator<char>());
+		text += "\n" + commandline_commands;
+		std::string pathname = boosty::stringy(fs::path(filename).parent_path());
+		root_module = parse(text.c_str(), pathname.c_str(), false);
 		if (root_module) {
 			root_module->handleDependencies();
 		}
