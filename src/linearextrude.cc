deaeafea8d950814081bdf77097ac3170ce4660a
Marius Kintel <marius@kintel.net>
2012-07-07 16:30:04 -0400

    coding style

--- src/linearextrude.cc
+++ src/linearextrude.cc
@@ -77,11 +77,12 @@ AbstractNode *LinearExtrudeModule::evaluate(const Context *ctx, const ModuleInst
 
 	// if height not given, and first argument is a number,
 	// then assume it should be the height.
-	if ( c.lookup_variable("height").type == Value::UNDEFINED )
-		if ( inst->argnames.size()>0 )
-			if ( inst->argnames[0] == "" )
-				if ( inst->argvalues[0].type == Value::NUMBER )
-					height = Value(inst->argvalues[0]);
+	if (c.lookup_variable("height").type == Value::UNDEFINED &&
+			inst->argnames.size() > 0 && 
+			inst->argnames[0] == "" &&
+			inst->argvalues[0].type == Value::NUMBER) {
+		height = Value(inst->argvalues[0]);
+	}
 
 	node->layername = layer.text;
 	node->height = height.num;
