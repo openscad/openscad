631ac4606dd78f5a51c298fb64a3616eef25337d
Don Bright <hugh.m.bright@gmail.com>
2012-07-10 22:52:16 -0500

    script to auto-download/install dependency packages in OpenSUSE

---
+++ scripts/getpackages-opensuse.sh
@@ -0,0 +1,6 @@
+sudo zypper install libeigen2-devel mpfr-devel gmp-devel boost-devel \
+ libqt4-devel glew-devel cmake git 
+
+echo "now copy/paste the following to install CGAL and OpenCSG from source:"
+echo "sudo BASEDIR=/usr/local ./scripts/linux-build-dependencies.sh cgal-use-sys-libs"
+echo "sudo BASEDIR=/usr/local ./scripts/linux-build-dependencies.sh opencsg"
