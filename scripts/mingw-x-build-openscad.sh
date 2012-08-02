e9e97af0a104b6dd2b4ef6bac95a5ea3e6ebeddb
don bright <hugh.m.bright@gmail.com>
2012-08-01 23:06:19 -0500

    add script to do qmake line for mingw. enable fully auto build

---
+++ scripts/mingw-x-build-openscad.sh
@@ -0,0 +1,42 @@
+#!/bin/sh -e
+#
+# This script builds Openscad.exe for windows using the MXE cross build system.
+#
+# This script must be run from the OpenSCAD source root directory
+#
+# You must have already run the mingw-x-build-dependencies.sh script
+#
+# Usage:
+#
+# ./scripts/mingw-x-build-openscad.sh
+#
+
+OPENSCADDIR=$PWD
+if [ ! -f $OPENSCADDIR/openscad.pro ]; then
+  echo "Must be run from the OpenSCAD source root directory"
+  exit 0
+fi
+echo OPENSCADDIR: $OPENSCADDIR
+
+. ./scripts/setenv-mingw-xbuild.sh
+
+if [ ! -e $BASEDIR/lib ]; then
+	echo "please run the mingw-x-build-dependencies.sh script first"
+fi
+
+echo cd $DEPLOYDIR
+echo "i686-pc-mingw32-qmake CONFIG+=mingw-cross-env ../openscad.pro"
+#echo "make -j$NUMCPU" # causes parser_yacc.hpp errors
+echo "make"
+
+OPENSCAD_EXE=$DEPLOYDIR/release/openscad.exe
+
+if [ -e $OPENSCAD_EXE ] ; then
+	echo
+	echo $OPENSCAD_EXE built in $DEPLOYDIR/release
+	echo
+else
+	echo Cannot find $OPENSCAD_EXE. The build appears to have had an error.
+fi
+
+echo cd $OPENSCADDIR
