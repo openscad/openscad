db8d8919433cc68ee777fcfc3199448419733c92
Marius Kintel <marius@kintel.net>
2013-01-13 18:28:13 -0500

    bugfix: Wrong slot names for OpenCSG warning dialog

--- src/OpenCSGWarningDialog.cc
+++ src/OpenCSGWarningDialog.cc
@@ -8,12 +8,12 @@ OpenCSGWarningDialog::OpenCSGWarningDialog(QWidget*)
 	connect(this->showBox, SIGNAL(toggled(bool)),
 					Preferences::inst()->openCSGWarningBox, SLOT(setChecked(bool)));
 	connect(this->showBox, SIGNAL(toggled(bool)),
-					Preferences::inst(), SLOT(openCSGWarningChanged(bool)));
+					Preferences::inst(), SLOT(on_openCSGWarningBox_toggled(bool)));
 
 	connect(this->enableOpenCSGBox, SIGNAL(toggled(bool)),
 					Preferences::inst()->enableOpenCSGBox, SLOT(setChecked(bool)));
 	connect(this->enableOpenCSGBox, SIGNAL(toggled(bool)),
-					Preferences::inst(), SLOT(enableOpenCSGChanged(bool)));
+					Preferences::inst(), SLOT(on_enableOpenCSGBox_toggled(bool)));
 }
 
 void OpenCSGWarningDialog::setText(const QString &text)
