3eb13949975fe30c2bd5f66384ae8b3ad597900d
don bright <hugh.m.bright@gmail.com>
2013-01-22 03:57:27 +0100

    documentation fixup

--- src/winconsole.c
+++ src/winconsole.c
@@ -1,14 +1,27 @@
 /*
- enable easy piping under windows command line, using the 'devenv' method
+ Enable easy piping under Windows(TM) command line.
+
+ We use the 'devenv'(TM) method, which means we have two binary files:
+
+  openscad.com, with IMAGE_SUBSYSTEM_WINDOWS_CUI flag set
+  openscad.exe, with IMAGE_SUBSYSTEM_WINDOWS_GUI flag set
+
+ The .com version is a 'wrapper' for the .exe version. If you call
+ 'openscad' with no extension from a script or shell, the .com version
+ is prioritized by the OS and feeds the GUI stdout to the console. We use
+ pure C to minimize binary size when cross-compiling (~10kbytes). See Also:
+
  http://stackoverflow.com/questions/493536/can-one-executable-be-both-a-console-and-gui-app
  http://blogs.msdn.com/b/oldnewthing/archive/2009/01/01/9259142.aspx
  http://blogs.msdn.com/b/junfeng/archive/2004/02/06/68531.aspx
  http://msdn.microsoft.com/en-us/library/aa298534%28v=vs.60%29.aspx
- http://www.i18nguy.com/unicode/c-unicode.html
  http://cournape.wordpress.com/2008/07/29/redirecting-stderrstdout-in-cmdexe/
  Open Group popen() documentation
- See Also: inkscapec by Jos Hirth work at http://kaioa.com
- and Nop Head's OpenSCAD_cl at github.com
+ inkscapec by Jos Hirth work at http://kaioa.com
+ Nop Head's OpenSCAD_cl at github.com
+
+ TODO:
+ Work with unicode: http://www.i18nguy.com/unicode/c-unicode.html
 */
 
 
