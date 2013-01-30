2ce2eef74a019199503fecc380a8aec03daeae4b
Marius Kintel <marius@kintel.net>
2013-01-29 21:02:13 -0500

    Better message for setting test configuration

--- tests/EnforceConfig.cmake
+++ tests/EnforceConfig.cmake
@@ -1,4 +1,4 @@
-message("Enforcing config")
 if(NOT CTEST_CONFIGURATION_TYPE)
+  message("Enforcing Default test configuration. Use ctest -C <config> to override")
   set(CTEST_CONFIGURATION_TYPE Default)
 endif()
