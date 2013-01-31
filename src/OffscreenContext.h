85378e44367a62116e8067105f0fe862a932ba8f
don bright <hugh.m.bright@gmail.com>
2013-01-31 04:18:08 +0100

    documentation note

--- src/OffscreenContext.h
+++ src/OffscreenContext.h
@@ -2,7 +2,8 @@
 #define OFFSCREENCONTEXT_H_
 
 // Here we implement a 'portability' pattern but since we are mixing
-// Objective-C with C++, it is a bit different.
+// Objective-C with C++, it is a bit different. The main struct
+// isn't defined in the header, but instead inside the source code files
 
 #include <iostream>
 #include <fstream>
