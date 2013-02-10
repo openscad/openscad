41c2574930f653cb9b2bf15f8d3636eb650d285b
don bright <hugh.m.bright@gmail.com>
2013-02-09 19:39:07 -0600

    clarify documentation, simplify helper script

--- scripts/translation-update.sh
+++ scripts/translation-update.sh
@@ -2,24 +2,16 @@
 
 # see doc/translation.txt for more info
 
-help()
-{
- echo 'translation.sh [updateall|updatepot|updatepo|updatemo]'
- echo ' updateall - run updatepot, updatepo, and updatemo in that order'
- echo ' updatepot - xgettext: update ./po/openscad.pot from source code files'
- echo ' updatepo - msgmerge: update old .po files with strings in openscad.pot'
- echo ' updatemo - msgfmt: create po/xx/LC_MESSAGES/openscad.mo files'
-}
-
 updatepot()
 {
+ VER=`date +"%Y.%m.%d"`
  OPTS=
  OPTS=$OPTS'--package-name=OpenSCAD'
- VER=`date +"%Y.%m.%d"`
  OPTS=$OPTS' --package-version='$VER
  OPTS=$OPTS' --default-domain=openscad'
  OPTS=$OPTS' --keyword=_'
- cmd='xgettext '$OPTS' --files-from=./po/POTFILES.in -o ./po/openscad.pot'
+ OPTS=$OPTS' --files-from=./po/POTFILES.in'
+ cmd='xgettext '$OPTS' -o ./po/openscad.pot'
  echo $cmd
  $cmd
  if [ ! $? = 0 ]; then
@@ -32,7 +24,6 @@ updatepot()
 updatepo()
 {
  for LANGCODE in `cat ./po/LINGUAS | grep -v "#"`; do
-	OPTS=
   OPTS='--update --backup=t'
   cmd='msgmerge '$OPTS' ./po/'$LANGCODE'.po ./po/openscad.pot'
   echo $cmd
@@ -48,7 +39,8 @@ updatemo()
 {
  for LANGCODE in `cat po/LINGUAS | grep -v "#"`; do
   mkdir -p ./po/$LANGCODE/LC_MESSAGES
-  cmd='msgfmt -c -v -o ./po/'$LANGCODE'/LC_MESSAGES/openscad.mo ./po/'$LANGCODE'.po'
+  OPTS='-c -v'
+  cmd='msgfmt '$OPTS' -o ./po/'$LANGCODE'/LC_MESSAGES/openscad.mo ./po/'$LANGCODE'.po'
   echo $cmd
   $cmd
   if [ ! $? = 0 ]; then
@@ -58,18 +50,5 @@ updatemo()
  done
 }
 
-if [ ! $1 ] ; then
- help
- exit 0
-fi
-
-if [ $1 = updatepot ]; then
- updatepot
-elif [ $1 = updatepo ]; then
- updatepo
-elif [ $1 = updatemo ]; then
- updatemo
-elif [ $1 = updateall ]; then
- updatepot && updatepo && updatemo
-fi
+updatepot && updatepo && updatemo
 
