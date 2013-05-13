d37cb95e2ede85193e3f3f5004095964cedc0d35
Marius Kintel <marius@kintel.net>
2013-05-13 19:32:54 -0400

    Detect errors mid-way into test run

--- scripts/travis-ci.sh
+++ scripts/travis-ci.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
-qmake
-make
+qmake && make
+if [[ $? != 0 ]]; then
+  echo "Error building OpenSCAD executable"
+  exit 1
+fi
 cd tests
-cmake .
+cmake . 
+if [[ $? != 0 ]]; then
+  echo "Error configuring test suite"
+  exit 1
+fi
 make
+if [[ $? != 0 ]]; then
+  echo "Error building test suite"
+  exit 1
+fi
 ctest
+if [[ $? != 0 ]]; then
+  echo "Test failure"
+  exit 1
+fi
