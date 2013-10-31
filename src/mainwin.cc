be4c72d9e9e03a2f70eb55d8aa70ca43d084045f
Torsten Paul <Torsten.Paul@gmx.de>
2013-10-31 22:54:05 +0100

    Add menu entry "Reset View" to reset viewport to initial settings (fixes Issue #434).

--- src/mainwin.cc
+++ src/mainwin.cc
@@ -334,6 +334,7 @@ MainWindow::MainWindow(const QString &filename)
 	connect(this->viewActionBack, SIGNAL(triggered()), this, SLOT(viewAngleBack()));
 	connect(this->viewActionDiagonal, SIGNAL(triggered()), this, SLOT(viewAngleDiagonal()));
 	connect(this->viewActionCenter, SIGNAL(triggered()), this, SLOT(viewCenter()));
+	connect(this->viewActionResetView, SIGNAL(triggered()), this, SLOT(viewResetView()));
 	connect(this->viewActionPerspective, SIGNAL(triggered()), this, SLOT(viewPerspective()));
 	connect(this->viewActionOrthogonal, SIGNAL(triggered()), this, SLOT(viewOrthogonal()));
 	connect(this->viewActionHide, SIGNAL(triggered()), this, SLOT(hideConsole()));
@@ -1769,6 +1770,12 @@ void MainWindow::viewOrthogonal()
 	viewActionPerspective->setChecked(false);
 	viewActionOrthogonal->setChecked(true);
 	this->qglview->setOrthoMode(true);
+	this->qglview->updateGL();
+}
+
+void MainWindow::viewResetView()
+{
+	this->qglview->resetView();
 	this->qglview->updateGL();
 }
 
