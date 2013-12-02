c507db461dbf00c8b28ff14608a3e69a3c709ca1
Marius Kintel <marius@kintel.net>
2013-12-01 22:53:14 -0500

    hull 2D and 3D implementation

--- src/GeometryCache.cc
+++ src/GeometryCache.cc
@@ -1,6 +1,9 @@
 #include "GeometryCache.h"
 #include "printutils.h"
 #include "geometry.h"
+#ifdef DEBUG
+  #include "CGAL_Nef_polyhedron.h"
+#endif
 
 GeometryCache *GeometryCache::inst = NULL;
 
@@ -8,6 +11,7 @@ bool GeometryCache::insert(const std::string &id, const shared_ptr<const Geometr
 {
 	bool inserted = this->cache.insert(id, new cache_entry(geom), geom ? geom->memsize() : 0);
 #ifdef DEBUG
+	assert(!dynamic_cast<const CGAL_Nef_polyhedron*>(geom.get()));
 	if (inserted) PRINTB("Geometry Cache insert: %s (%d bytes)", 
                          id.substr(0, 40) % (geom ? geom->memsize() : 0));
 	else PRINTB("Geometry Cache insert failed: %s (%d bytes)",
