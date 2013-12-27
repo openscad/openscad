3e464d01a76a271790f7fc73eb1a8e3ef270fd6f
Marius Kintel <marius@kintel.net>
2013-12-27 18:04:52 -0500

    Use namespace instead of class method

--- src/calc.h
+++ src/calc.h
@@ -1,9 +1,8 @@
 #ifndef CALC_H_
 #define CALC_H_
 
-class Calc {
-public:
-        static int get_fragments_from_r(double r, double fn, double fs, double fa);
-};
+namespace Calc {
+	int get_fragments_from_r(double r, double fn, double fs, double fa);
+}
 
 #endif
