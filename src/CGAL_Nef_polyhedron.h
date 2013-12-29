bc4fae0d85c92dcc6b060cc40d86b919c7a08a92
Marius Kintel <marius@kintel.net>
2013-12-29 00:37:49 -0500

    small cleanup - removed redundant dim field

--- src/CGAL_Nef_polyhedron.h
+++ src/CGAL_Nef_polyhedron.h
@@ -10,19 +10,18 @@
 class CGAL_Nef_polyhedron : public Geometry
 {
 public:
-	CGAL_Nef_polyhedron(int dim = 0) : dim(dim) {}
-	CGAL_Nef_polyhedron(CGAL_Nef_polyhedron3 *p);
+	CGAL_Nef_polyhedron(CGAL_Nef_polyhedron3 *p = NULL);
 	~CGAL_Nef_polyhedron() {}
 
 	virtual size_t memsize() const;
 	// FIXME: Implement, but we probably want a high-resolution BBox..
 	virtual BoundingBox getBoundingBox() const { assert(false && "not implemented"); }
 	virtual std::string dump() const;
-	virtual unsigned int getDimension() const { return this->dim; }
+	virtual unsigned int getDimension() const { return 3; }
   // Empty means it is a geometric node which has zero area/volume
 	virtual bool isEmpty() const { return !p3; }
 
-	void reset() { dim=0; p3.reset(); }
+	void reset() { p3.reset(); }
 	CGAL_Nef_polyhedron &operator+=(const CGAL_Nef_polyhedron &other);
 	CGAL_Nef_polyhedron &operator*=(const CGAL_Nef_polyhedron &other);
 	CGAL_Nef_polyhedron &operator-=(const CGAL_Nef_polyhedron &other);
@@ -31,8 +30,6 @@ public:
 	class PolySet *convertToPolyset() const;
 	void transform( const Transform3d &matrix );
 	shared_ptr<CGAL_Nef_polyhedron3> p3;
-protected:
-	int dim;
 };
 
 #endif
