6b1079b7b7469bd2ea73bf5f87e09614a222d846
Don Bright <hugh.m.bright@gmail.com>
2014-03-23 21:12:01 -0500

    better glib2 detection

--- tests/FindGLIB2.cmake
+++ tests/FindGLIB2.cmake
@@ -1,6 +1,22 @@
+message(STATUS "running openscad/tests/FindGLIB2.cmake")
+
+# GLIB2 requires pkg-config to build. 
+# If we are did an OPENSCAD_LIBRARIES dependency build of glib2, we need to 
+# tell pkg-config to look under OPENSCAD_LIBRARIES dir.
+# Otherwise, we need to use the system's pkg-config to find system's glib2
+if (NOT $ENV{OPENSCAD_LIBRARIES} STREQUAL "")
+  if (EXISTS "$ENV{OPENSCAD_LIBRARIES}/include/glib.h")
+    set(SAVED_PKG_CONFIG_PATH "${PKG_CONFIG_PATH}")
+    set(SAVED_PKG_CONFIG_LIBDIR "${PKG_CONFIG_LIBDIR}")
+    set(ENV{PKG_CONFIG_PATH} "$ENV{OPENSCAD_LIBRARIES}/lib/pkgconfig")
+    set(ENV{PKG_CONFIG_LIBDIR} "$ENV{OPENSCAD_LIBRARIES}/lib/pkgconfig")
+  endif()
+endif()
+
 find_package(PkgConfig REQUIRED)
 
 pkg_search_module(GLIB2 REQUIRED glib-2.0)
+message(STATUS "GLIB2_VERSION ${GLIB2_VERSION}")
 #message(STATUS "GLIB2_LIBRARIES ${GLIB2_LIBRARIES}")
 message(STATUS "GLIB2_LIBRARY_DIRS ${GLIB2_LIBRARY_DIRS}")
 #message(STATUS "GLIB2_LDFLAGS ${GLIB2_LDFLAGS}")
@@ -26,3 +42,10 @@ foreach(GLIB2_LIB ${GLIB2_LIBRARY_NAMES})
   list(APPEND GLIB2_LIBRARIES "${TMP}")
 endforeach()
 message(STATUS "GLIB2_LIBRARIES: ${GLIB2_LIBRARIES}")
+
+if (EXISTS "$ENV{OPENSCAD_LIBRARIES}/include/glib.h")
+  # message(STATUS "resetting pkgconfig as it was")
+else()
+  set(ENV{PKG_CONFIG_PATH} "${SAVED_PKG_CONFIG_PATH}")
+  set(ENV{PKG_CONFIG_LIBDIR} "${SAVED_PKG_CONFIG_LIBDIR}")
+endif()
