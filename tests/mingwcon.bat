b7cbe6297c41801a30d84347318dd4d8e101ac97
Don Bright <hugh.m.bright@gmail.com>
2014-04-18 20:05:14 -0500

    better ctest description for windows(TM) users of tests

--- tests/mingwcon.bat
+++ tests/mingwcon.bat
@@ -1,9 +1,11 @@
 @echo off
 @echo Welcome to the OpenSCAD test console. Basic commands:
 @echo.
-@echo    ctest                       :: run standard set of tests
-@echo    ctest -C All                :: run all tests
-@echo    ctest -R cgal.*circle       :: run all tests named 'cgal ... circle'
+@echo    ctest                   :: run standard set of tests
+@echo    ctest -R circle         :: run standard tests with 'circle' in name
+@echo    ctest -R cgal.*circle   :: run standard tests matching 'cgal.*circle' 
+@echo    ctest -C All            :: run all tests
+@echo    ctest -C All -R circle  :: run all tests with 'circle' in name
 @echo.
 @echo on
 
