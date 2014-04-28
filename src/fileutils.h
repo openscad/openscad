b5337ad338bc0047016e01c2ff7afd89e64eacb3
Ben Gamari <bgamari.foss@gmail.com>
2014-04-28 13:25:04 -0400

    Various whitespace fixes

--- src/fileutils.h
+++ src/fileutils.h
@@ -3,4 +3,4 @@
 #include <string>
 
 std::string lookup_file(const std::string &filename, 
-												const std::string &path, const std::string &fallbackpath);
+                        const std::string &path, const std::string &fallbackpath);
