19d3ba0d966195e5ae5f4e76c73a209769a8c3d2
don bright <hugh.m.bright@gmail.com>
2014-06-02 16:14:13 -0700

    change gettext-tarball-existance test to use .gz not .xz, since the download/unpack use gz (and gz is more portable than xz)

--- scripts/common-build-dependencies.sh
+++ scripts/common-build-dependencies.sh
@@ -110,7 +110,7 @@ build_gettext()
   echo "Building gettext $version..."
   cd "$BASEDIR"/src
   rm -rf "gettext-$version"
-  if [ ! -f "gettext-$version.tar.xz" ]; then
+  if [ ! -f "gettext-$version.tar.gz" ]; then
     curl --insecure -LO "http://ftpmirror.gnu.org/gettext/gettext-$version.tar.gz"
   fi
   tar xzf "gettext-$version.tar.gz"
