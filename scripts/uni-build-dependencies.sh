f415ee239f89b21ed443846062bb4b818130a156
Torsten Paul <Torsten.Paul@gmx.de>
2014-08-24 02:44:11 +0200

    Remove dependency to libpng when building freetype.

--- scripts/uni-build-dependencies.sh
+++ scripts/uni-build-dependencies.sh
@@ -808,7 +808,7 @@ build_gettext 0.18.3.1
 build_glib2 2.38.2
 
 # the following are only needed for text()
-build_freetype 2.5.0.1
+build_freetype 2.5.0.1 --without-png
 build_libxml2 2.9.1
 build_fontconfig 2.11.0
 build_ragel 6.8
