0b5dc265110c00cdcfef69cae388615c54edd1ff
Marius Kintel <marius@kintel.net>
2014-08-24 17:10:19 -0400

    Added Xcode project file for debugging aid on OS X

---
+++ xcode/dummy.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  openscad_nogui-dummy
+//
+//  Created by Marius Kintel on 24/8/2014.
+//  Copyright (c) 2014 OpenSCAD. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
