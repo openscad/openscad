55aa1c9736b5a32fde47300d03a2862564a49737
Torsten Paul <Torsten.Paul@gmx.de>
2014-09-16 23:27:54 +0200

    Move render color schemes to separate files.

--- src/colormap.h
+++ src/colormap.h
@@ -6,6 +6,13 @@
 #include "linalg.h"
 #include <boost/unordered/unordered_map.hpp>
 
+#include <boost/shared_ptr.hpp>
+#include <boost/filesystem.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+
+namespace fs = boost::filesystem;
+
 enum RenderColor {
 	BACKGROUND_COLOR,
 	OPENCSG_FACE_FRONT_COLOR,
@@ -21,8 +28,37 @@ enum RenderColor {
 
 typedef std::map<RenderColor, Color4f> ColorScheme;
 
+class RenderColorScheme
+{
+private:
+        const fs::path path;
+        
+        boost::property_tree::ptree pt;
+        std::string _name;
+        int _index;
+        bool _show_in_gui;
+        
+        ColorScheme _color_scheme;
+        
+public:
+        RenderColorScheme(const fs::path path);
+        virtual ~RenderColorScheme();
+        
+        const std::string & name() const;
+        int index() const;
+        bool valid() const;
+        bool showInGui() const;
+        ColorScheme & colorScheme();
+        const boost::property_tree::ptree & propertyTree() const;
+
+private:
+        void addColor(RenderColor colorKey, std::string key);
+};
+
 class ColorMap
 {
+        typedef std::multimap<int, boost::shared_ptr<RenderColorScheme>, std::less<int> > colorscheme_set_t;
+    
 public:
 	static ColorMap *inst(bool erase = false);
 
@@ -30,14 +66,15 @@ public:
 
 	const boost::unordered_map<std::string, Color4f> &webColors() const { return webcolors; }
 	const ColorScheme *findColorScheme(const std::string &name) const;
-	std::list<std::string> colorSchemeNames() const;
+	std::list<std::string> colorSchemeNames(bool guiOnly = false) const;
 
 	static Color4f getColor(const ColorScheme &cs, const RenderColor rc);
 	
 private:
 	ColorMap();
 	~ColorMap() {}
+        colorscheme_set_t enumerateColorSchemes();
 
 	boost::unordered_map<std::string, Color4f> webcolors;
-	boost::unordered_map<std::string, ColorScheme> colorschemes;
+        colorscheme_set_t colorschemes;
 };
