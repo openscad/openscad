1941a7fc0d4099f90cdf339d6d7dfcc642842c98
Marius Kintel <marius@kintel.net>
2015-01-06 22:51:44 -0500

    #1105 Workaround for CGAL problem: Don't trust normal vectors from Nef polyhedrons, normalize calculated normal vectors

--- src/cgalutils-tess.cc
+++ src/cgalutils-tess.cc
@@ -107,6 +107,8 @@ namespace CGALUtils {
 		else {
 			// Calculate best guess at face normal using Newell's method
 			CGAL::normal_vector_newell_3(polygons.front().begin(), polygons.front().end(), normalvec);
+			double sqrl = normalvec.squared_length();
+			if (sqrl > 0.0) normalvec = normalvec / sqrt(sqrl);
 		}
 
 		// Pass the normal vector to the (undocumented)
