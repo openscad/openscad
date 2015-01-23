5a0c4b0ba87da0b3f672281eaedf1d3843ca516c
Marius Kintel <marius@kintel.net>
2015-01-23 01:31:41 -0500

    Updated copyright year

--- src/mainwin.cc
+++ src/mainwin.cc
@@ -128,7 +128,7 @@ QSet<MainWindow*> *MainWindow::getWindows()
 unsigned int GuiLocker::gui_locked = 0;
 
 static char copyrighttext[] =
-	"Copyright (C) 2009-2014 The OpenSCAD Developers\n"
+	"Copyright (C) 2009-2015 The OpenSCAD Developers\n"
 	"\n"
 	"This program is free software; you can redistribute it and/or modify "
 	"it under the terms of the GNU General Public License as published by "
