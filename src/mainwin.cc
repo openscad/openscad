0edd05760ee1c2e6aa498c27332b6ea4f94a2100
Marius Kintel <marius@kintel.net>
2015-10-16 12:44:35 -0400

    #1425 For backwards compatibility, revert  calculation so that  never reaches 1.0

--- src/mainwin.cc
+++ src/mainwin.cc
@@ -848,7 +848,7 @@ void MainWindow::updatedAnimFps()
 	double fps = this->e_fps->text().toDouble(&fps_ok);
 	animate_timer->stop();
 	if (fps_ok && fps > 0) {
-		this->anim_step = this->anim_tval * (this->anim_numsteps - 1);
+		this->anim_step = int(this->anim_tval * this->anim_numsteps) % this->anim_numsteps;
 		animate_timer->setSingleShot(false);
 		animate_timer->setInterval(int(1000 / fps));
 		animate_timer->start();
@@ -880,7 +880,7 @@ void MainWindow::updateTVal()
 
 	if (this->anim_numsteps > 1) {
 		this->anim_step = (this->anim_step + 1) % this->anim_numsteps;
-		this->anim_tval = 1.0 * this->anim_step / (this->anim_numsteps - 1);
+		this->anim_tval = 1.0 * this->anim_step / this->anim_numsteps;
 	}
 	else if (this->anim_numsteps > 0) {
 		this->anim_step = 0;
