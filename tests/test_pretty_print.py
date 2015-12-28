e72b6c412724153a86ca28bd9956fb3636cc0edc
Torsten Paul <Torsten.Paul@gmx.de>
2015-12-28 22:16:16 +0100

    Fix duplicate header/footer for STL export (fixes #1532).

--- tests/test_pretty_print.py
+++ tests/test_pretty_print.py
@@ -344,7 +344,7 @@ def to_html(project_name, startdate, tests, enddate, sysinfo, sysid, imgcomparer
 
     templates = Templates()
     for test in report_tests:
-        if test.type in ('txt', 'ast', 'csg', 'term', 'echo'):
+        if test.type in ('txt', 'ast', 'csg', 'term', 'echo', 'stl'):
             text_test_count += 1
             templates.add('text_template', 'text_tests',
                           test_name=test.fullname,
