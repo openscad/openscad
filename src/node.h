5f0e716d6e0566c6938fe41ce19260fce2158cb4
Torsten Paul <Torsten.Paul@gmx.de>
2016-02-11 21:51:05 +0100

    Change LeafNode::createGeometry() to return a const geometry pointer.

--- src/node.h
+++ src/node.h
@@ -109,7 +109,7 @@ public:
 	LeafNode(const ModuleInstantiation *mi) : AbstractPolyNode(mi) { };
 	virtual ~LeafNode() { };
   virtual Response accept(class State &state, class Visitor &visitor) const;
-	virtual class Geometry *createGeometry() const = 0;
+	virtual const class Geometry *createGeometry() const = 0;
 };
 
 std::ostream &operator<<(std::ostream &stream, const AbstractNode &node);
