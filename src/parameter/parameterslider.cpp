17e107988b285179fe03f8719139933d0093163a
amarjeetkapoor1 <amarjeet.kapoor1@gmail.com>
2016-07-28 19:18:15 +0530

    slider support decimals

--- src/parameter/parameterslider.cpp
+++ src/parameter/parameterslider.cpp
@@ -2,10 +2,12 @@
 
 ParameterSlider::ParameterSlider(ParameterObject *parameterobject, bool showDescription)
 {
+    count =0;
     object=parameterobject;
     setName(QString::fromStdString(object->name));
     setValue();
     connect(slider,SIGNAL(sliderReleased()),this,SLOT(on_Changed()));
+    connect(slider,SIGNAL(valueChanged(int)),this,SLOT(onMoved(int)));
     if(showDescription==true){
         setDescription(object->description);
     }
@@ -14,20 +16,33 @@ ParameterSlider::ParameterSlider(ParameterObject *parameterobject, bool showDesc
     }
 }
 
+void ParameterSlider::onMoved(int)
+{
+    double v = slider->value()/pow(10,count);
+    this->labelSliderValue->setText(QString::number(v, 'f', count));
+}
+
 void ParameterSlider::on_Changed()
 {
-    double v = slider->value();
+    double v = slider->value()/pow(10,count);
     object->value = ValuePtr(v);
     //to be corrected
-    this->labelSliderValue->setText(QString::number(v, 'f', 0));
+    this->labelSliderValue->setText(QString::number(v, 'f',count));
     emit changed();
 }
 
 void ParameterSlider::setValue()
 {
+    count =0;
+    double no =object->values->toRange().step_value();
+    while(no!=((int)no))
+    {
+        count++;
+        no=no*10;
+    }
     this->stackedWidget->setCurrentWidget(this->pageSlider);
-    this->slider->setRange(object->values->toRange().begin_value(),object->values->toRange().end_value());
-    this->slider->setValue(object->value->toDouble());
-    this->slider->setSingleStep(object->values->toRange().step_value());
-    this->labelSliderValue->setText(QString::number(object->value->toDouble(), 'f', 0));
+    this->slider->setRange(object->values->toRange().begin_value()*pow(10,count),object->values->toRange().end_value()*pow(10,count));
+    this->slider->setValue(object->value->toDouble()*pow(10,count));
+    this->slider->setSingleStep(object->values->toRange().step_value()*pow(10,count));
+    this->labelSliderValue->setText(QString::number(object->value->toDouble(), 'f',count));
 }
--- src/parameter/parameterslider.h
+++ src/parameter/parameterslider.h
@@ -9,9 +9,10 @@ class ParameterSlider : public ParameterVirtualWidget
 public:
     ParameterSlider(ParameterObject *parameterobject,bool showDescription);
     void setValue();
-
+        int count;
 protected slots:
     void on_Changed();
+    void onMoved(int);
 };
 
 #endif // PARAMETERSLIDER_H
