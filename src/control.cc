d3e2b69d5a409506a8a61872f7db03d8466fc3ec
Kirill <bulgakovk@users.noreply.github.com>
2016-09-30 17:17:36 +0300

    Update control.cc
    Fixed regex initialization

--- src/control.cc
+++ src/control.cc
@@ -263,23 +263,23 @@ AbstractNode *ControlModule::instantiate(const Context* /*ctx*/, const ModuleIns
 		node = new GroupNode(inst);
 		std::stringstream msg;
 		msg << "ECHO: ";
+		static boost::regex tagRegEx(".*<[^>]+>.*");
 		for (size_t i = 0; i < inst->arguments.size(); i++) {
 			if (i > 0) msg << ", ";
 			if (!evalctx->getArgName(i).empty()) msg << evalctx->getArgName(i) << " = ";
 			ValuePtr val = evalctx->getArgValue(i);
 
-            /*
-            * Using regexp to determine whether string contain HTML
-            * If so - we need to replace escape symbols
-            * Because escape symbols (\t, \n etc.) doesn't render well with HTML tags
-            */
+		        /*
+		        * Using regexp to determine whether string contain HTML
+		        * If so - we need to replace escape symbols
+		        * Because escape symbols (\t, \n etc.) doesn't render well with HTML tags
+		        */
 
-            static boost::regex tagRegEx(".*<[^>]+>.*");
-            if (boost::regex_match(val->toString(), tagRegEx)) {
-                boost::regex escapeSymbRegEx("(\t)|(\n)|(\r)"); // using boost extended format string
-                std::string formatString = "(?1&nbsp;&nbsp;&nbsp;&nbsp;)(?2<br/>)(?3<br/>)";
-                val = boost::regex_replace(val->toString(), escapeSymbRegEx, formatString, boost::format_all);
-            }
+		        if (boost::regex_match(val->toString(), tagRegEx)) {
+				boost::regex escapeSymbRegEx("(\t)|(\n)|(\r)"); // using boost extended format string
+				std::string formatString = "(?1&nbsp;&nbsp;&nbsp;&nbsp;)(?2<br/>)(?3<br/>)";
+				val = boost::regex_replace(val->toString(), escapeSymbRegEx, formatString, boost::format_all);
+		        }
 
 			if (val->type() == Value::STRING) {
 				msg << '"' << val->toString() << '"';
