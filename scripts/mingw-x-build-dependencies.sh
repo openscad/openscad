569918a8152ed38bd39e1013f2da83f6a03eae06
Don Bright <hmbright@fastmail.com>
2016-10-08 08:41:11 -0500

    implify build deps for mingw

--- scripts/mingw-x-build-dependencies.sh
+++ scripts/mingw-x-build-dependencies.sh
@@ -1,15 +1,18 @@
 #!/bin/sh -e
 #
 # This script builds all library dependencies of OpenSCAD for cross-compilation
-# from linux to mingw32/64 for windows, using the MXE cross build system.
-#
-# This script must be run from the OpenSCAD source root directory
+# from linux to mingw32/64 for windows(TM), using the MXE cross build system.
 #
 # Usage:
-#        ./scripts/mingw-x-build-dependencies.sh [64|32] [download]
+#   . ./scripts/setenv-mingw-xbuild.sh [64|32] [static|shared]
+#   ./scripts/mingw-x-build-dependencies.sh
 #
-# 64 or 32 choose the bits of the target operating system. Default is 64
-# download will perform only the download, not the build.
+# As of 2016 MXE allows these targets with the MXE_TARGETS environment variable.
+#
+# 64 bit static linked libraries MXE_TARGETS=x86_64-w64-mingw32.static
+# 32 bit static linked libraries MXE_TARGETS=i686-w64-mingw32.static
+# 64 bit shared libraries        MXE_TARGETS=x86_64-w64-mingw32.shared
+# 32 bit shared libraries        MXE_TARGETS=i686-w64-mingw32.shared
 #
 # Prerequisites:
 #
@@ -17,22 +20,11 @@
 #
 # Also see http://en.wikibooks.org/wiki/OpenSCAD_User_Manual/Cross-compiling_for_Windows_on_Linux_or_Mac_OS_X
 #
-# Targets:
-#
-# MXE allows separate targets with the MXE_TARGETS environment variable.
-#
-# As of 2016:
-# 64 bit static linked libraries MXE_TARGETS=x86_64-w64-mingw32.static
-# 32 bit static linked libraries MXE_TARGETS=i686-w64-mingw32.static
-# 64 bit shared libraries        MXE_TARGETS=x86_64-w64-mingw32.shared
-# 32 bit shared libraries        MXE_TARGETS=i686-w64-mingw32.shared
 #
 
-if [ ! $OPENSCADDIR ]; then
-	echo please run '. scripts/setenv-mingw-xbuild.sh'
-fi
 if [ ! $MXE_TARGET ]; then
 	echo please run '. scripts/setenv-mingw-xbuild.sh'
+	exit
 fi
 
 if [ ! $NUMCPU ]; then
@@ -55,10 +47,14 @@ if [ ! -e $MXEDIR ]; then
 	git clone git://github.com/openscad/mxe.git $MXEDIR
 fi
 
-echo "entering" $MXEDIR
-cd $MXEDIR
-echo 'checkout openscad-snapshot-build branch'
-git checkout openscad-snapshot-build
+docmd()
+{
+	echo $*
+	$*
+}
+
+docmd cd $MXEDIR
+docmd git checkout openscad-snapshot-build
 
 PACKAGES='qtbase qscintilla2 mpfr eigen opencsg cgal'
 PACKAGES=$PACKAGES' glib freetype fontconfig harfbuzz'
@@ -66,11 +62,5 @@ if [ "`echo $MXE_TARGET|grep i686`" ]; then
   PACKAGES=$PACKAGES' nsis'
 fi
 
-echo make $PACKAGES MXE_TARGETS=$MXE_TARGET -j $NUMCPU JOBS=$NUMJOBS
-make $PACKAGES MXE_TARGETS=$MXE_TARGET -j $NUMCPU JOBS=$NUMJOBS
-
-echo "leaving" $MXEDIR
-
-echo "entering $OPENSCADDIR"
-cd $OPENSCADDIR
-
+docmd echo make $PACKAGES MXE_TARGETS=$MXE_TARGET -j $NUMCPU JOBS=$NUMJOBS
+docmd cd $OPENSCADDIR
