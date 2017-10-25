0037f282973e7979886231c94d5845f10307f6aa
Justin Buchanan <justbuchanan@gmail.com>
2017-10-24 23:46:38 -0700

    Add clang-format script at scripts/beautify.sh

---
+++ scripts/beautify.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Reformat C++ code using clang-format
+
+# note: with the -style=file option, clang-format reads the config from ./.clang-format
+# See here for the full list of supported options: https://clang.llvm.org/docs/ClangFormatStyleOptions.html
+FORMAT_CMD="clang-format -i -style=file"
+
+function reformat_all() {
+    find . -name "*.h" -o -name "*.cc" | xargs $FORMAT_CMD
+}
+
+# reformat files that differ from master.
+DIFFBASE="origin/master"
+function reformat_changed() {
+    ANCESTOR=$(git merge-base HEAD "$DIFFBASE")
+    FILES=$(git --no-pager diff --name-only $ANCESTOR | grep -E "\.cc|\.h")
+    if [ $? -ne 0 ]; then
+        echo "No files to format, exiting..."
+    else
+        echo -e "Reformatting files:\n$FILES"
+        echo $FILES | xargs $FORMAT_CMD
+    fi
+}
+
+# main
+if [[ "$#" -eq 1 && "$1" == "--all" ]]; then
+    echo "Reformatting all files..."
+    reformat_all
+else
+    echo "Reformatting files that differ from $DIFFBASE..."
+    reformat_changed
+fi
