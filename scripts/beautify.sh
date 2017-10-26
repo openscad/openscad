aad1aec61376075e1b4ce97acc7cd47c5cca5b80
Justin Buchanan <justbuchanan@gmail.com>
2017-10-25 20:20:03 -0700

    Add list of files to exclude from auto-formatting

--- scripts/beautify.sh
+++ scripts/beautify.sh
@@ -6,15 +6,26 @@
 # See here for the full list of supported options: https://clang.llvm.org/docs/ClangFormatStyleOptions.html
 FORMAT_CMD="clang-format -i -style=file"
 
+# Filter out any files that shouldn't be auto-formatted.
+# note: -v flag inverts selection - this tells grep to *filter out* anything
+#       that matches the pattern. For testing, you can remove the -v to ssee
+#       which files would have been excluded.
+FILTER_CMD="grep -v -E objects|src/polyclipping|src/CGAL_Nef3_workaround.h|src/CGAL_workaround_Mark_bounded_volumes.h|src/convex_hull_3_bugfix.h|src/OGL_helper.h"
+
+# C++ file extensions
+SRC_PATTERN=".*\.(h|hpp|cc|cpp)"
+
 function reformat_all() {
-    find . -name "*.h" -o -name "*.hpp" -o -name "*.cc" -o -name "*.cpp" | xargs $FORMAT_CMD
+    find . -regextype posix-extended -regex "$SRC_PATTERN" \
+        | $FILTER_CMD \
+        | xargs $FORMAT_CMD
 }
 
 # reformat files that differ from master.
 DIFFBASE="origin/master"
 function reformat_changed() {
     ANCESTOR=$(git merge-base HEAD "$DIFFBASE")
-    FILES=$(git --no-pager diff --name-only $ANCESTOR | grep -E "\.h|\.hpp|\.cc|\.cpp")
+    FILES=$(git --no-pager diff --name-only $ANCESTOR | grep -E "$SRC_PATTERN" | $FILTER_CMD)
     if [ $? -ne 0 ]; then
         echo "No files to format, exiting..."
     else
