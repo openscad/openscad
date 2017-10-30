524652199d39d701839e33cbc6b2ef931dcb11d0
Marius Kintel <marius@kintel.net>
2017-10-29 22:07:50 -0400

    Experiment with beautify

--- scripts/beautify.sh
+++ scripts/beautify.sh
@@ -4,13 +4,15 @@
 
 # note: with the -style=file option, clang-format reads the config from ./.clang-format
 # See here for the full list of supported options: https://clang.llvm.org/docs/ClangFormatStyleOptions.html
-FORMAT_CMD="clang-format -i -style=file"
+FORMAT_CMD_CLANG_FORMAT="clang-format -i -style=file"
+FORMAT_CMD_UNCRUSTIFY="uncrustify -c .uncrustify.cfg --no-backup"
+FORMAT_CMD=$FORMAT_CMD_CLANG_FORMAT
 
 # Filter out any files that shouldn't be auto-formatted.
 # note: -v flag inverts selection - this tells grep to *filter out* anything
 #       that matches the pattern. For testing, you can remove the -v to ssee
 #       which files would have been excluded.
-FILTER_CMD="grep -v -E objects|src/polyclipping|src/CGAL_Nef3_workaround.h|src/CGAL_workaround_Mark_bounded_volumes.h|src/convex_hull_3_bugfix.h|src/OGL_helper.h"
+FILTER_CMD="grep -v -E objects|src/libsvg|src/libtess2|src/polyclipping|src/CGAL_Nef3_workaround.h|src/CGAL_workaround_Mark_bounded_volumes.h|src/convex_hull_3_bugfix.h|src/OGL_helper.h"
 
 function reformat_all() {
     find src -name "*.h" -o -name "*.hpp" -o -name "*.cc" -o -name "*.cpp" \
@@ -32,7 +34,10 @@ function reformat_changed() {
 }
 
 # main
-if [[ "$#" -eq 1 && "$1" == "--all" ]]; then
+if [ "`echo $* | grep uncrust`" ]; then
+    FORMAT_CMD=$FORMAT_CMD_UNCRUSTIFY
+fi
+if [ "`echo $* | grep -- --all`" ]; then
     echo "Reformatting all files..."
     reformat_all
 else
