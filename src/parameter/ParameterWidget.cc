403c6382b8ccb6206d88ec8ea2ae0d5c79848603
Michael Frey <michael.frey@gmx.ch>
2018-02-11 20:29:46 +0100

    add helper variable "layout" to improve readability

--- src/parameter/ParameterWidget.cc
+++ src/parameter/ParameterWidget.cc
@@ -173,11 +173,12 @@ void ParameterWidget::onPreviewTimerElapsed()
 
 void ParameterWidget::cleanScrollArea()
 {
-	this->scrollAreaWidgetContents->layout()->setAlignment(Qt::AlignTop);
+	auto layout = this->scrollAreaWidgetContents->layout();
+	layout->setAlignment(Qt::AlignTop);
 	QLayoutItem *child;
-	while ((child = this->scrollAreaWidgetContents->layout()->takeAt(0)) != nullptr) {
+	while ((child = layout->takeAt(0)) != nullptr) {
 		QWidget *w = child->widget();
-		this->scrollAreaWidgetContents->layout()->removeWidget(w);
+		layout->removeWidget(w);
 		delete w;
 	}
 }
