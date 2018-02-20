1f26fb4427cd1ea0d945559f3fd6ba4ab3303646
Marius Kintel <marius@kintel.net>
2018-02-20 00:35:56 -0500

    Cleanup, modernization

--- src/builtin.h
+++ src/builtin.h
@@ -9,22 +9,22 @@
 class Builtins
 {
 public:
-	typedef std::unordered_map<std::string, class AbstractFunction*> FunctionContainer;
-	typedef std::unordered_map<std::string, class AbstractModule*> ModuleContainer;
+	using FunctionContainer = std::unordered_map<std::string, class AbstractFunction*>;
+	using ModuleContainer = std::unordered_map<std::string, class AbstractModule*>;
 
 	static Builtins *instance(bool erase = false);
-	static void init(const char *name, class AbstractModule *module);
-	static void init(const char *name, class AbstractFunction *function);
+	static void init(const std::string &name, class AbstractModule *module);
+	static void init(const std::string &name, class AbstractFunction *function);
 	void initialize();
-	std::string isDeprecated(const std::string &name);
+	std::string isDeprecated(const std::string &name) const;
 
-	const AssignmentList &getAssignments() { return this->assignments; }
-	const FunctionContainer &getFunctions() { return this->functions; }
-	const ModuleContainer &getModules() { return modules; }
+	const AssignmentList &getAssignments() const { return this->assignments; }
+	const FunctionContainer &getFunctions() const { return this->functions; }
+	const ModuleContainer &getModules() const { return modules; }
 	
 private:
 	Builtins();
-	~Builtins();
+	~Builtins() = default;
 
 	AssignmentList assignments;
 	FunctionContainer functions;
