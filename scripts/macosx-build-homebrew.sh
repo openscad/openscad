a24ecaec107206e1523a97910db4dab152bb578b
Torsten Paul <Torsten.Paul@gmx.de>
2018-08-19 19:48:14 +0200

    Revert switch to CGAL 4.11, trying to re-run boost.
    This reverts commit 73227f6099f94fb7a524c55c7a0f5c54c05416c9.

--- scripts/macosx-build-homebrew.sh
+++ scripts/macosx-build-homebrew.sh
@@ -44,7 +44,7 @@ $TAP tap openscad/homebrew-tap
 # FIXME: We used to require unlinking boost, but doing so also causes us to lose boost.
 # Disabling until we can figure out why we unlinked in the first place
 # brew unlink boost
-for formula in eigen boost cgal@4.11 glew glib opencsg freetype libzip libxml2 fontconfig harfbuzz qt5 qscintilla2 imagemagick ccache; do
+for formula in eigen boost cgal glew glib opencsg freetype libzip libxml2 fontconfig harfbuzz qt5 qscintilla2 imagemagick ccache; do
   log "Installing formula $formula"
   brew ls --versions $formula
   time brew install $formula
