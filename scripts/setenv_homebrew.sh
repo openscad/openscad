03bedaccfc9b5d5728821664dc146bc14e97679b
don bright <hmbright@fastmail.com>
2018-08-22 21:46:20 -0500

    environmet variables for homebrew build

---
+++ scripts/setenv_homebrew.sh
@@ -0,0 +1,5 @@
+export OPENSCAD_LIBRARIES=/usr/local
+export DYLD_LIBRARY_PATH=
+export DYLD_FRAMEWORK_PATH=
+export PKG_CONFIG_PATH="/usr/local/opt/libxml2/lib/pkgconfig:$PKG_CONFIG_PATH"
+export OPENSCAD_HOMEBREW=1
