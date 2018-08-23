e6de06b2cab499ad39738cb4662d48cceb5353a1
don bright <hmbright@fastmail.com>
2018-08-22 21:48:26 -0500

    simplify

--- scripts/setenv_homebrew.sh
+++ scripts/setenv_homebrew.sh
@@ -1,5 +1,3 @@
 export OPENSCAD_LIBRARIES=/usr/local
-export DYLD_LIBRARY_PATH=
-export DYLD_FRAMEWORK_PATH=
 export PKG_CONFIG_PATH="/usr/local/opt/libxml2/lib/pkgconfig:$PKG_CONFIG_PATH"
 export OPENSCAD_HOMEBREW=1
