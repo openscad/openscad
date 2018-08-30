bc27ba049bd83dcfbfe5e89560823591eb9ced59
Chris Palmer <nop.head@gmail.com>
2018-08-30 12:18:40 +0100

    Fixed OPENSCAD_FONT_PATH in both test_cmdline_tool.py and export_pngtest.py. Neither matched the comment at the top of the file.
    Made the import() file name relative in export_import_pngtest.py because under
    Windows an absolute path needs to start with a drive letter.

--- tests/test_cmdline_tool.py
+++ tests/test_cmdline_tool.py
@@ -234,7 +234,7 @@ def run_test(testname, cmd, args):
         cmdline = [cmd] + args + [outputname]
         sys.stderr.flush()
         print('run_test() cmdline:',cmdline)
-        fontdir =  os.path.join(os.path.dirname(cmd), "testdata")
+        fontdir =  os.path.join(os.path.dirname(__file__), "..", "testdata/ttf");
         fontenv = os.environ.copy()
         fontenv["OPENSCAD_FONT_PATH"] = fontdir
         print('using font directory:', fontdir)
