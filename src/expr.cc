5101d7d5dd2bd9bcaa24495745140bf93cb6ea90
Torsten Paul <Torsten.Paul@gmx.de>
2018-10-07 05:51:33 +0200

    Remove experimental flag of assert() (fixes #1860).

--- src/expr.cc
+++ src/expr.cc
@@ -443,7 +443,7 @@ void FunctionCall::print(std::ostream &stream, const std::string &indent) const
 
 Expression * FunctionCall::create(const std::string &funcname, const AssignmentList &arglist, Expression *expr, const Location &loc)
 {
-	if (funcname == "assert" && Feature::ExperimentalAssertExpression.is_enabled()) {
+	if (funcname == "assert") {
 		return new Assert(arglist, expr, loc);
 	} else if (funcname == "echo") {
 		return new Echo(arglist, expr, loc);
@@ -711,8 +711,6 @@ void LcLet::print(std::ostream &stream, const std::string &indent) const
 
 void evaluate_assert(const Context &context, const class EvalContext *evalctx, const Location &loc)
 {
-	ExperimentalFeatureException::check(Feature::ExperimentalAssertExpression);
-
 	AssignmentList args;
 	args += Assignment("condition"), Assignment("message");
 
