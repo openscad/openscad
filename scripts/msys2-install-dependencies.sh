88fd7ef1ffa4cd32d2700da4f70320ef59a2ead5
Torsten Paul <Torsten.Paul@gmx.de>
2018-10-29 22:31:59 +0100

    Enable Mesa software renderer for running the test cases.

--- scripts/msys2-install-dependencies.sh
+++ scripts/msys2-install-dependencies.sh
@@ -5,11 +5,7 @@
 
 pacman --query --explicit
 
-for pkg in mingw-w64-x86_64-gdb mingw-w64-x86_64-boost mingw-w64-x86_64-cgal mingw-w64-x86_64-eigen3 mingw-w64-x86_64-glew mingw-w64-x86_64-qscintilla mingw-w64-x86_64-opencsg mingw-w64-x86_64-pkg-config mingw-w64-x86_64-libzip mingw-w64-x86_64-cmake bison git
+for pkg in mingw-w64-x86_64-gdb mingw-w64-x86_64-boost mingw-w64-x86_64-cgal mingw-w64-x86_64-eigen3 mingw-w64-x86_64-glew mingw-w64-x86_64-qscintilla mingw-w64-x86_64-opencsg mingw-w64-x86_64-pkg-config mingw-w64-x86_64-libzip mingw-w64-x86_64-cmake mingw-w64-x86_64-mesa bison git
 do
 	pacman --noconfirm --ask 20 --sync  --needed ${pkg}
 done
-
-# overwrite minizip which seems to come in via a dependency
-#pacman --noconfirm --ask 20 --force --sync \
-#	mingw-w64-x86_64-libzip
