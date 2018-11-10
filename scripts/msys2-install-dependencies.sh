c7f8efef510ec729bc99b5c1f759700c67792f6e
Torsten Paul <Torsten.Paul@gmx.de>
2018-11-11 00:44:18 +0100

    Add time stamps for AppVeyor build.

--- scripts/msys2-install-dependencies.sh
+++ scripts/msys2-install-dependencies.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+date "+### %Y-%m-%d %T: msys2-install-dependencies started"
+
 #pacman --noconfirm --ask 20 -Sy
 #pacman --noconfirm --ask 20 -Su
 
@@ -7,5 +9,8 @@ pacman --query --explicit
 
 for pkg in mingw-w64-x86_64-gdb mingw-w64-x86_64-boost mingw-w64-x86_64-cgal mingw-w64-x86_64-eigen3 mingw-w64-x86_64-glew mingw-w64-x86_64-qscintilla mingw-w64-x86_64-opencsg mingw-w64-x86_64-pkg-config mingw-w64-x86_64-libzip mingw-w64-x86_64-cmake mingw-w64-x86_64-mesa bison git
 do
-	pacman --noconfirm --ask 20 --sync  --needed ${pkg}
+	date "+### %Y-%m-%d %T: install ${pkg}"
+	pacman --noconfirm --ask 20 --sync --needed ${pkg}
 done
+
+date "+### %Y-%m-%d %T: msys2-install-dependencies finished"
