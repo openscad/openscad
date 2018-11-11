2a7c8cff40cb0a33aea53d6186b42f3d9c65a003
Torsten Paul <Torsten.Paul@gmx.de>
2018-11-11 18:20:13 +0100

    Coverity: CID 192883: Error handling issues (CHECKED_RETURN).
    "read(int, void *, size_t)" returns the number of bytes read, but it
    is ignored.

--- src/input/JoystickInputDriver.cc
+++ src/input/JoystickInputDriver.cc
@@ -35,19 +35,26 @@
 
 void JoystickInputDriver::run()
 {
-    struct js_event js;
+	struct js_event js;
 
-    while (read(fd, &js, sizeof(struct js_event)) > 0 && !stopRequest) {
-        switch (js.type & ~JS_EVENT_INIT) {
-        case JS_EVENT_BUTTON:
-            InputDriverManager::instance()->sendEvent(new InputEventButtonChanged(js.number, js.value != 0));
-            break;
-        case JS_EVENT_AXIS:
-            InputDriverManager::instance()->sendEvent(new InputEventAxisChanged(js.number, js.value / 32767.0));
-            break;
-        }
-    }
-    ::close(fd);
+	while (!stopRequest) {
+		ssize_t len = read(fd, &js, sizeof(struct js_event));
+		if (len < 0) {
+			break;
+		}
+		if (len != sizeof(struct js_event)) {
+			continue;
+		}
+		switch (js.type & ~JS_EVENT_INIT) {
+		case JS_EVENT_BUTTON:
+			InputDriverManager::instance()->sendEvent(new InputEventButtonChanged(js.number, js.value != 0));
+			break;
+		case JS_EVENT_AXIS:
+			InputDriverManager::instance()->sendEvent(new InputEventAxisChanged(js.number, js.value / 32767.0));
+			break;
+		}
+	}
+	::close(fd);
 }
 
 JoystickInputDriver::JoystickInputDriver() : fd(-1)
