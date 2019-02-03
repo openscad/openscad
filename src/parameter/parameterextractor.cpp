d14f7ebc2a74dd2c7347b454d246e2bcdfa25b0a
Michael Frey <michael.frey@gmx.ch>
2019-02-03 22:11:39 +0100

    typo (doen't -> doesn't)

--- src/parameter/parameterextractor.cpp
+++ src/parameter/parameterextractor.cpp
@@ -43,7 +43,7 @@ void ParameterExtractor::setParameters(const FileModule* module,entry_map_t& ent
 
     //check whether object exist or not previously
     if (entries.find(assignment.name) == entries.end()) {
-      //if object doen't exist, add new entry
+      //if object doesn't exist, add new entry
       entries[assignment.name] = entryObject;
       rebuildParameterWidget = true;
     } else {
