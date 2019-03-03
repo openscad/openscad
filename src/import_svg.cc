d5b3ae2c158fc3b1dd5f9ba16ca61993b4f7b8fb
Torsten Paul <Torsten.Paul@gmx.de>
2019-03-03 02:17:53 +0100

    Handle scaling based on viewBox and preserveAspectRatio settings.

--- src/import_svg.cc
+++ src/import_svg.cc
@@ -5,72 +5,134 @@
 #include "clipper-utils.h"
 #include "AST.h"
 
+namespace {
+
+constexpr double INCH_TO_MM = 25.4;
+
+struct dim_t {
+	double x;
+	double y;
+};
+
+double to_mm(const libsvg::length_t& length, const double viewbox, const bool viewbox_valid, const double dpi)
+{
+	switch (length.unit) {
+	case libsvg::unit_t::NONE:
+		return INCH_TO_MM * length.number / dpi;
+	case libsvg::unit_t::PX:
+		return INCH_TO_MM * length.number / 96.0;
+	case libsvg::unit_t::PT:
+		return INCH_TO_MM * length.number / 72.0;
+	case libsvg::unit_t::PC:
+		return INCH_TO_MM * length.number / 6.0;
+	case libsvg::unit_t::IN:
+		return INCH_TO_MM * length.number;
+	case libsvg::unit_t::CM:
+		return 10 * length.number;
+	case libsvg::unit_t::MM:
+		return length.number;
+	case libsvg::unit_t::UNDEFINED:
+		// If no width/height given, but viewbox is set, then rely on
+		// the DPI value (e.g. Adobe Illustrator does that in older
+		// versions)
+		return viewbox_valid ? INCH_TO_MM * viewbox / dpi : 0.0;
+	default:
+		return viewbox_valid ? viewbox : 0.0;
+	}
+}
+
+}
+
 Polygon2d *import_svg(const std::string &filename, const double dpi, const bool center, const Location &loc)
 {
 	try {
 		const libsvg::shapes_list_t *shapes = libsvg::libsvg_read_file(filename.c_str());
 
-		double scale = 1.0;
+		double width_mm = 0.0;
+		double height_mm = 0.0;
 
-		double x_min = 1.0/0.0;
-		double x_max = -1.0/0.0;
-		double y_min = 1.0/0.0;
-		double y_max = -1.0/0.0;
-		double height = 0;
+		dim_t min{1.0/0.0, 1.0/0.0};
+		dim_t max{-1.0/0.0, -1.0/0.0};
+		dim_t scale{0.0, 0.0};
+		dim_t align{0.0, 0.0};
+		dim_t viewbox{0.0, 0.0};
+
 		for (const auto& shape_ptr : *shapes) {
-			PRINTD("SVG shape");
 			const auto page = dynamic_cast<libsvg::svgpage *>(shape_ptr.get());
-			double unit_scale = 1.0;
 			if (page) {
+				const libsvg::length_t w = page->get_width();
 				const libsvg::length_t h = page->get_height();
-				switch (h.unit) {
-				case libsvg::unit_t::UNDEFINED:
-				case libsvg::unit_t::NONE:
-					scale = 25.4 / dpi;
-					height = scale * h.number;
-					break;
-				case libsvg::unit_t::PT:
-					height = 25.4 / 72.0 * h.number;
-					break;
-				case libsvg::unit_t::PC:
-					height = 25.4 / 6.0 * h.number;
-					break;
-				case libsvg::unit_t::IN:
-					height = 25.4 * h.number;
-					break;
-				case libsvg::unit_t::CM:
-					height = 10.0 * h.number;
-					break;
-				case libsvg::unit_t::MM:
-					height = h.number;
-					break;
+				const libsvg::alignment_t alignment = page->get_alignment();
+
+				const bool viewbox_valid = page->get_viewbox().is_valid;
+				width_mm = to_mm(w, page->get_viewbox().width, viewbox_valid, dpi);
+				height_mm = to_mm(h, page->get_viewbox().height, viewbox_valid, dpi);
+
+				scale.x = viewbox_valid ? width_mm / page->get_viewbox().width : 1.0;
+				scale.y = viewbox_valid ? height_mm / page->get_viewbox().height : 1.0;
+
+				if (viewbox_valid && (alignment.x != libsvg::align_t::NONE)) {
+					if (alignment.meet) {
+						// preserve aspect ratio and fit into viewport, so
+						// select the smaller of the 2 scale factors
+						scale.x = scale.x < scale.y ? scale.x : scale.y;
+					} else {
+						// preserve aspect ratio and fill viewport, so select
+						// the bigger of the 2 scale factors
+						scale.x = scale.x > scale.y ? scale.x : scale.y;
+					}
+					scale.y = scale.x;
+
+					switch (alignment.x) {
+					case libsvg::align_t::MID:
+						align.x = width_mm / 2.0 - scale.x * page->get_viewbox().width / 2.0;
+						break;
+					case libsvg::align_t::MAX:
+						align.x = width_mm - scale.x * page->get_viewbox().width;
+						break;
+					default:
+						break;
+					}
+
+					switch (alignment.y) {
+					case libsvg::align_t::MID:
+						align.y = height_mm / 2.0 - scale.y * page->get_viewbox().height / 2.0;
+						break;
+					case libsvg::align_t::MAX:
+						align.y = height_mm - scale.y * page->get_viewbox().height;
+						break;
+					default:
+						break;
+					}
 				}
+
+				viewbox.x = viewbox_valid ? page->get_viewbox().x : 0.0;
+				viewbox.y = viewbox_valid ? page->get_viewbox().y : 0.0;
 			}
 
 			const auto& s = *shape_ptr;
 			for (const auto& p : s.get_path_list()) {
-				PRINTD("SVG path");
 				for (const auto& v : p) {
-					double x = scale * v.x();
-					double y = scale * v.y();
-					if (x < x_min) {
-						x_min = x;
+					double x = scale.x * v.x();
+					double y = scale.y * v.y();
+					if (x < min.x) {
+						min.x = x;
 					}
-					if (x > x_max) {
-						x_max = x;
+					if (x > max.x) {
+						max.x = x;
 					}
-					if (y < y_min) {
-						y_min = y;
+					if (y < min.y) {
+						min.y = y;
 					}
-					if (y > y_max) {
-						y_max = y;
+					if (y > max.y) {
+						max.y = y;
 					}
 				}
 			}
 		}
 
-		double cx = center ? (x_min + x_max) / 2 : 0;
-		double cy = center ? (y_min + y_max) / 2 : height;
+		double cx = center ? (min.x + max.x) / 2 : -align.x;
+		double cy = center ? (min.y + max.y) / 2 : height_mm - align.y;
 
 		std::vector<const Polygon2d*> polygons;
 		for (const auto& shape_ptr : *shapes) {
@@ -79,10 +141,10 @@ Polygon2d *import_svg(const std::string &filename, const double dpi, const bool 
 			for (const auto& p : s.get_path_list()) {
 				Outline2d outline;
 				for (const auto& v : p) {
-					double x = scale * v.x() - cx;
-					double y = -scale * v.y() + cy;
+					double x = scale.x * (-viewbox.x + v.x()) - cx;
+					double y = scale.y * (-viewbox.y - v.y()) + cy;
 					outline.vertices.push_back(Vector2d(x, y));
-					outline.positive=true;
+					outline.positive = true;
 				}
 				poly->addOutline(outline);
 			}
@@ -94,4 +156,3 @@ Polygon2d *import_svg(const std::string &filename, const double dpi, const bool 
 		return new Polygon2d();
 	}
 }
-	
