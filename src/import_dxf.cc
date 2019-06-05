5397d3b18a74772dd3f1f680f9bcc65df39d9266
leonplust <myemail>
2019-06-05 17:36:57 +0800

    Modify the code to output a simple circle

--- src/import_dxf.cc
+++ src/import_dxf.cc
@@ -3,6 +3,7 @@
 #include "printutils.h"
 #include "calc.h"
 #include "Polygon2d.h"
+#include "degree_trig.h"
 
 Polygon2d *import_dxf(const std::string &filename)
 {
@@ -13,8 +14,8 @@ Polygon2d *import_dxf(const std::string &filename)
     // brlcad::entities_list *entities = brlcad::read_dxf_file(filename.c_str());
     // Assume we have receive a data of square.
     
-    // To make it simple, I will ignore other enitities. Basically just use four
-    // lines to form the square
+    // To make it simple, I will ignore other enitities. Basically just use necessary
+    // component to form the geometry
 
     // These are other expected values that we should get from just the line entity
     // std::string curr_layer_name
@@ -22,18 +23,39 @@ Polygon2d *import_dxf(const std::string &filename)
     // double scale;
     
     // Last extract data like coord from the entity
+    
+    //suqare
     auto poly = new Polygon2d();
 	Grid2d<std::vector<int>> grid(GRID_COARSE);
-    std::vector<double> xCoord = {-20, -20, 20, 20};
-	std::vector<double> yCorrd = {-20, 20, 20, -20};
-    int vertexNum = 4;
+    std::vector<double> xverts = {-20, -20, 20, 20};
+	std::vector<double> yverts = {-20, 20, 20, -20};
     Outline2d outline;
-    for(int i = 0; i < vertexNum; i++){
-        grid.align(xCoord.at(i), yCorrd.at(i));
-        //what does the grid.align do? Just align the vertex on the grid?
-        outline.vertices.push_back(Vector2d(xCoord.at(i),yCorrd.at(i)));
-        outline.positive = true;
+    // int vertexNum = 4;
+    // for(int i = 0; i < vertexNum; i++){
+    //     grid.align(xverts.at(i), yverts.at(i));
+    //     //what does the grid.align do? Just align the vertex on the grid?
+    //     outline.vertices.push_back(Vector2d(xverts.at(i),yverts.at(i)));
+    //     outline.positive = true;
+    // }
+
+    //Now assuming we got the data of a circle
+    //We should get raidus, center of the circle and special values $fn, $fa, $fs
+    
+    double radius = 10;
+    double fn = 0, fs = 2, fa = 12;
+    xverts.at(0) = 0;
+    yverts.at(0) = 0;
+    int n = Calc::get_fragments_from_r(radius, fn, fs, fa);
+    Vector2d center(xverts.at(0), yverts.at(0));
+    for (int i = 0; i < n; i++) {
+        double a1 = (360.0 * i) / n;
+        double a2 = (360.0 *(i + 1)) / n;
+        outline.vertices.push_back(Vector2d(cos_degrees(a1)*radius + center[0], 
+                                            sin_degrees(a1)*radius + center[1]));
+        outline.vertices.push_back(Vector2d(cos_degrees(a2)*radius + center[0], 
+                                            sin_degrees(a2)*radius + center[1]));
     }
+    
     poly->addOutline(outline);
     return poly;
 }
