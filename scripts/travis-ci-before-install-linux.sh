e0c67f204ef61299fcb47b4e5b8b105fb7fc97cb
Torsten Paul <Torsten.Paul@gmx.de>
2019-07-29 22:55:18 +0200

    Fix quotes.

--- scripts/travis-ci-before-install-linux.sh
+++ scripts/travis-ci-before-install-linux.sh
@@ -6,7 +6,7 @@ PACKAGES1="build-essential bison cmake curl flex git-core imagemagick"
 PACKAGES2="libboost-all-dev libboost-dev libeigen3-dev libzip-dev"
 PACKAGES3="libxi-dev libxmu-dev qtbase5-dev qtmultimedia5-dev libqt5opengl5-dev libqt5scintilla2-dev"
 PACKAGES4="libcgal-dev libcgal-qt5-dev libglew-dev libgmp3-dev libgmp-dev libmpfr-dev"
-PACKAGES5=libdouble-conversion-dev libfontconfig-dev libharfbuzz-dev libopencsg-dev lib3mf-dev
+PACKAGES5="libdouble-conversion-dev libfontconfig-dev libharfbuzz-dev libopencsg-dev lib3mf-dev"
 
 if [[ "$DIST" == "trusty" ]]; then
 
