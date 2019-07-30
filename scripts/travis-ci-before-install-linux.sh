5f62d67a12246921c7e7a30899e6e390a7058bfc
Torsten Paul <Torsten.Paul@gmx.de>
2019-07-31 01:03:59 +0200

    Travis updates (#3018)
    * Disable automatic updates for homebrew build.
    * Add brew cleanup before cache store.
    * Extract before install to separate script and add bionic.
    * Enable IRC join again as we need to leave +n on for spam protection.

---
+++ scripts/travis-ci-before-install-linux.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+DIST="$1"
+
+PACKAGES1="build-essential bison cmake curl flex git-core imagemagick"
+PACKAGES2="libboost-all-dev libboost-dev libeigen3-dev libzip-dev"
+PACKAGES3="libxi-dev libxmu-dev qtbase5-dev qtmultimedia5-dev libqt5opengl5-dev libqt5scintilla2-dev"
+PACKAGES4="libcgal-dev libcgal-qt5-dev libglew-dev libgmp3-dev libgmp-dev libmpfr-dev"
+PACKAGES5="libdouble-conversion-dev libfontconfig-dev libharfbuzz-dev libopencsg-dev lib3mf-dev"
+
+if [[ "$DIST" == "trusty" ]]; then
+
+    LIB3MF_REPO="http://download.opensuse.org/repositories/home:/t-paul:/lib3mf/xUbuntu_14.04/"
+
+elif [[ "$DIST" == "xenial" ]]; then
+
+    LIB3MF_REPO="http://download.opensuse.org/repositories/home:/t-paul:/lib3mf/xUbuntu_16.04/"
+
+elif [[ "$DIST" == "bionic" ]]; then
+
+    LIB3MF_REPO="https://download.opensuse.org/repositories/home:/t-paul:/lib3mf/xUbuntu_18.04/"
+
+else
+
+    echo "ERROR: unhandled DIST: $DIST"
+    exit 1
+
+fi
+
+echo "Selected distribution: $DIST"
+
+wget -qO - http://files.openscad.org/OBS-Repository-Key.pub | sudo apt-key add -
+echo yes | sudo add-apt-repository "deb $LIB3MF_REPO ./"
+sudo apt-get update -qq
+sudo apt-get purge -qq fglrx || true
+sudo apt-get install -qq $PACKAGES1 $PACKAGES2 $PACKAGES3 $PACKAGES4 $PACKAGES5
+
