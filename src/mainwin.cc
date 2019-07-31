8e2d08742bb174a9f3cedd17dbe3f254748eb2ec
Romit Kumar <romitkumar1705@gmail.com>
2019-07-31 22:52:17 +0530

    Don't clear autoreload when selecting No in the reload dialog

--- src/mainwin.cc
+++ src/mainwin.cc
@@ -1968,7 +1968,6 @@ bool MainWindow::checkEditorModified()
 				"Do you really want to reload the file?"),
 				QMessageBox::Yes | QMessageBox::No);
 		if (ret != QMessageBox::Yes) {
-			designActionAutoReload->setChecked(false);
 			return false;
 		}
 	}
