bb53b4eb3744ef2546e9cdbe27861703f6106379
Torsten Paul <Torsten.Paul@gmx.de>
2019-10-19 19:30:09 +0200

    Make ContextHandle move-only.

--- src/context.h
+++ src/context.h
@@ -17,14 +17,18 @@
 template<typename T>
 struct ContextHandle
 {
-    ContextHandle(std::shared_ptr<T>&& sp) : ctx(std::forward<std::shared_ptr<T>>(sp)) {
+    ContextHandle(std::shared_ptr<T>&& sp) : ctx(std::move(sp)) {
         ctx->init();
 		ctx->push(ctx);
     }
-
     ~ContextHandle() {
         ctx->pop();
     }
+
+	ContextHandle(const ContextHandle&) = delete;
+	ContextHandle& operator=(const ContextHandle&) = delete;
+	ContextHandle(ContextHandle&&) = default;
+	ContextHandle& operator=(ContextHandle&&) = default;
 
 	const T* operator->() const { return ctx.get(); }
     T* operator->() { return ctx.get(); }
