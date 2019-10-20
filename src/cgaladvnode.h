db8eb01c7820fe42c9202b4d6812e8dcc19feee5
Torsten Paul <Torsten.Paul@gmx.de>
2019-10-20 05:53:38 +0200

    Remove path member from CgalAdvNode.

--- src/cgaladvnode.h
+++ src/cgaladvnode.h
@@ -21,7 +21,6 @@ public:
 	std::string toString() const override;
 	std::string name() const override;
 
-	Value path;
 	unsigned int convexity;
 	Vector3d newsize;
 	Eigen::Matrix<bool,3,1> autosize;
