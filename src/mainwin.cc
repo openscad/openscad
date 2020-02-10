e509ba832f17055ed6dc23361cb9682d135f6d52
Torsten Paul <Torsten.Paul@gmx.de>
2020-02-10 22:19:54 +0100

    Update year in copyright messages.

--- src/mainwin.cc
+++ src/mainwin.cc
@@ -147,7 +147,7 @@
 unsigned int GuiLocker::gui_locked = 0;
 
 static char copyrighttext[] =
-	"Copyright (C) 2009-2019 The OpenSCAD Developers\n\n"
+	"Copyright (C) 2009-2020 The OpenSCAD Developers\n\n"
 	"This program is free software; you can redistribute it and/or modify "
 	"it under the terms of the GNU General Public License as published by "
 	"the Free Software Foundation; either version 2 of the License, or "
