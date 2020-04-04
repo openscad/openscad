e91a47279da0ec3a6600302a85e75baf2bb16608
tbharathchandra <t.bharathchandra@gmail.com>
2020-04-04 14:05:09 +0530

    Added mm to points scale and name

--- src/export.h
+++ src/export.h
@@ -28,7 +28,7 @@ enum class FileFormat {
 };
 
 void exportFileByName(const shared_ptr<const class Geometry> &root_geom, FileFormat format,
-											const char *name2open, const char *name2display);
+                                            const char *name2open, const char *name2display);
 
 void export_stl(const shared_ptr<const Geometry> &geom, std::ostream &output);
 void export_3mf(const shared_ptr<const Geometry> &geom, std::ostream &output);
@@ -39,7 +39,7 @@ void export_svg(const shared_ptr<const Geometry> &geom, std::ostream &output);
 void export_nefdbg(const shared_ptr<const Geometry> &geom, std::ostream &output);
 void export_nef3(const shared_ptr<const Geometry> &geom, std::ostream &output);
 
-void export_pdf(const shared_ptr<const Geometry> &geom, const char *name2open);
+void export_pdf(const shared_ptr<const Geometry> &geom, const char *name2open, const char *name2display, bool &onerror);
 
 // void exportFile(const class Geometry *root_geom, std::ostream &output, FileFormat format);
 
--- src/export_pdf.cc
+++ src/export_pdf.cc
@@ -6,6 +6,17 @@
 #include "polyset.h"
 #include "polyset-utils.h"
 
+void draw_name(const char *name2display, cairo_t *cr){
+
+    cairo_set_font_size(cr, 20.);
+    cairo_move_to(cr, 0., 0.);
+    cairo_show_text(cr,name2display);
+
+}
+
+double mm_to_points(double mm){
+    return mm*2.8346;
+}
 
 void draw_geom(const Polygon2d &poly, cairo_t *cr){
     for(const auto &o : poly.outlines()){
@@ -13,12 +24,12 @@ void draw_geom(const Polygon2d &poly, cairo_t *cr){
             continue;
         }
         const Eigen::Vector2d& p0 = o.vertices[0];
-        cairo_move_to(cr,p0.x(),p0.y());
+        cairo_move_to(cr, mm_to_points(p0.x()), mm_to_points(p0.y()));
         for (unsigned int idx = 1;idx < o.vertices.size();idx++) {
             const Eigen::Vector2d& p = o.vertices[idx];
-            cairo_line_to(cr,p.x(),p.y());
+            cairo_line_to(cr, mm_to_points(p.x()), mm_to_points(p.y()));
         }
-        cairo_line_to(cr,p0.x(),p0.y());
+        cairo_line_to(cr, mm_to_points(p0.x()), mm_to_points(p0.y()));
 
     }
 }
@@ -39,15 +50,21 @@ void draw_geom(const shared_ptr<const Geometry> &geom, cairo_t *cr){
     }
 }
 
-void export_pdf(const shared_ptr<const Geometry> &geom, const char *name2open){
+void export_pdf(const shared_ptr<const Geometry> &geom, const char *name2open, const char *name2display, bool &onerror){
     //A4 Size in points
-    const double WPOINTS = 595;
-    const double HPOINTS = 842;
+    const double WPOINTS = 595.;
+    const double HPOINTS = 842.;
+    const double MARGIN = 10.;
 
     cairo_surface_t *surface = cairo_pdf_surface_create(name2open, WPOINTS, HPOINTS);
+    if(cairo_surface_status(surface)==cairo_status_t::CAIRO_STATUS_NULL_POINTER){
+        onerror=true;
+        cairo_surface_destroy(surface);
+        return;
+    }
     cairo_t *cr = cairo_create(surface);
 
-    cairo_set_source_rgb(cr, 0, 0, 0);
+    cairo_set_source_rgb(cr, 0., 0., 0.);
     cairo_set_line_width(cr, 0.9);
 
     BoundingBox bbox = geom->getBoundingBox();
@@ -55,19 +72,28 @@ void export_pdf(const shared_ptr<const Geometry> &geom, const char *name2open){
     int maxy = (int)floor(bbox.max().y());
     int maxx = (int)ceil(bbox.max().x());
     int miny = (int)ceil(bbox.min().y());
-    //int width = maxx - minx;
+    //int width = maxx - min
     //int height = maxy - miny;
 
+
     if(minx>=0 && miny>=0 && maxx>=0 && maxy>=0){
-        cairo_translate(cr, 10., HPOINTS-10.);
+
+        cairo_translate(cr, MARGIN, HPOINTS-MARGIN);
+        cairo_rotate(cr,270.*(M_PI/180.));
+        draw_geom(geom,cr);
+        cairo_translate(cr, 0., HPOINTS-(2.*MARGIN));
+
     }else {
-        cairo_translate(cr, WPOINTS/2, HPOINTS/2);
+
+        cairo_translate(cr, WPOINTS/2., HPOINTS/2.);
+        cairo_rotate(cr,270.*(M_PI/180.));
+        draw_geom(geom,cr);
+        cairo_translate(cr,-((WPOINTS/2.)-MARGIN),(HPOINTS/2.)-MARGIN);
     }
-    cairo_rotate(cr,270.*(M_PI/180.));
-
-    draw_geom(geom,cr);
-
     cairo_stroke(cr);
+
+    draw_name(name2display,cr);
+
     cairo_show_page(cr);
     cairo_surface_destroy(surface);
     cairo_destroy(cr);
