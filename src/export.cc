3806c0bc820358fe42471c21cb2f33be15918446
tbharathchandra <t.bharathchandra@gmail.com>
2020-04-04 14:05:09 +0530

    Added mm to points scale and name

--- src/export.cc
+++ src/export.cc
@@ -69,7 +69,11 @@ void exportFileByName(const shared_ptr<const Geometry> &root_geom, FileFormat fo
 	const char *name2open, const char *name2display)
 {
     if (format == FileFormat::PDF){
-        export_pdf(root_geom,name2open);
+        bool onerror = false;
+        export_pdf(root_geom,name2open,name2display,onerror);
+        if (onerror) {
+            PRINTB(_("ERROR: \"%s\" write error. (Disk full?)"), name2display);
+        }
         return;
     }
 	std::ios::openmode mode = std::ios::out | std::ios::trunc;
