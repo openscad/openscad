e0e22379f2aa76995c62b705671b1e2b3440225e
Torsten Paul <Torsten.Paul@gmx.de>
2020-06-13 00:39:30 +0200

    Fix DXF export with lazy union enabled.

--- src/export_dxf.cc
+++ src/export_dxf.cc
@@ -29,12 +29,8 @@
 #include "polyset-utils.h"
 #include "dxfdata.h"
 
-/*!
-	Saves the current Polygon2d as DXF to the given absolute filename.
- */
-void export_dxf(const Polygon2d &poly, std::ostream &output)
+static void export_dxf_header(std::ostream &output)
 {
-	setlocale(LC_NUMERIC, "C"); // Ensure radix is . (not ,) in output
 	// Some importers (e.g. Inkscape) needs a BLOCKS section to be present
 	output << "  0\n"
 				 <<	"SECTION\n"
@@ -46,33 +42,10 @@ void export_dxf(const Polygon2d &poly, std::ostream &output)
 				 << "SECTION\n"
 				 << "  2\n"
 				 << "ENTITIES\n";
+}
 
-	for(const auto &o : poly.outlines()) {
-		for (unsigned int i=0;i<o.vertices.size();i++) {
-			const Vector2d &p1 = o.vertices[i];
-			const Vector2d &p2 = o.vertices[(i+1)%o.vertices.size()];
-			double x1 = p1[0];
-			double y1 = p1[1];
-			double x2 = p2[0];
-			double y2 = p2[1];
-			output << "  0\n"
-						 << "LINE\n";
-			// Some importers (e.g. Inkscape) needs a layer to be specified
-      // The [X1 Y1 X2 Y2] order is the most common and can be parsed linearly.
-			// Some libraries, like the python libraries dxfgrabber and ezdxf, cannot open [X1 X2 Y1 Y2] order.
-			output << "  8\n"
-						 << "0\n"
-						 << " 10\n"
-						 << x1 << "\n"
-						 << " 20\n"
-						 << y1 << "\n"
-						 << " 11\n"
-						 << x2 << "\n"
-						 << " 21\n"
-						 << y2 << "\n";
-		}
-	}
-
+static void export_dxf_footer(std::ostream &output)
+{
 	output << "  0\n"
 				 << "ENDSEC\n";
 
@@ -88,22 +61,64 @@ void export_dxf(const Polygon2d &poly, std::ostream &output)
 
 	output << "  0\n"
 				 <<"EOF\n";
-
-	setlocale(LC_NUMERIC, "");      // Set default locale
 }
 
-void export_dxf(const shared_ptr<const Geometry> &geom, std::ostream &output)
+/*!
+	Saves the current Polygon2d as DXF to the given output stream.
+ */
+static void append_dxf_geometry(const Polygon2d &poly, std::ostream &output)
+{
+	for (const auto &o : poly.outlines()) {
+		for (unsigned int i=0;i<o.vertices.size();i++) {
+			const Vector2d &p1 = o.vertices[i];
+			const Vector2d &p2 = o.vertices[(i+1)%o.vertices.size()];
+			double x1 = p1[0];
+			double y1 = p1[1];
+			double x2 = p2[0];
+			double y2 = p2[1];
+			output << "  0\n"
+						 << "LINE\n";
+			// Some importers (e.g. Inkscape) needs a layer to be specified
+			// The [X1 Y1 X2 Y2] order is the most common and can be parsed linearly.
+			// Some libraries, like the python libraries dxfgrabber and ezdxf, cannot open [X1 X2 Y1 Y2] order.
+			output << "  8\n"
+						 << "0\n"
+						 << " 10\n"
+						 << x1 << "\n"
+						 << " 20\n"
+						 << y1 << "\n"
+						 << " 11\n"
+						 << x2 << "\n"
+						 << " 21\n"
+						 << y2 << "\n";
+		}
+	}
+}
+
+static void export_dxf_geometry(const shared_ptr<const Geometry> &geom, std::ostream &output)
 {
 	if (const auto geomlist = dynamic_pointer_cast<const GeometryList>(geom)) {
 		for(const auto &item : geomlist->getChildren()) {
-			export_dxf(item.second, output);
+			export_dxf_geometry(item.second, output);
 		}
 	} else if (dynamic_pointer_cast<const PolySet>(geom)) {
 		assert(false && "Unsupported file format");
 	}
 	else if (const auto poly = dynamic_pointer_cast<const Polygon2d>(geom)) {
-		export_dxf(*poly, output);
+		append_dxf_geometry(*poly, output);
 	} else {
 		assert(false && "Export as DXF for this geometry type is not supported");
 	}
+}
+
+void export_dxf(const shared_ptr<const Geometry> &geom, std::ostream &output)
+{
+	// Ensure radix is . (not ,) in output
+	setlocale(LC_NUMERIC, "C");
+	// Set back to default locale when leaving the scope
+	const std::shared_ptr<int> localeGuard(nullptr, [](int *) { setlocale(LC_NUMERIC, ""); });
+
+	export_dxf_header(output);
+	export_dxf_geometry(geom, output);
+	export_dxf_footer(output);
 }
