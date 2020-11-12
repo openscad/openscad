da28123251584bb83ff682a0951e472aa6b81586
Kevin Gravier <kevin@mrkmg.com>
2020-11-12 10:19:20 -0500

    Changed spaces to tab Fixed issue with M_PI not being available for windows builds

--- src/offsetextrudenode.h
+++ src/offsetextrudenode.h
@@ -7,22 +7,22 @@
 class OffsetExtrudeNode : public AbstractPolyNode
 {
 public:
-    VISITABLE();
-    OffsetExtrudeNode(const ModuleInstantiation *mi, const std::shared_ptr<EvalContext> &ctx) : AbstractPolyNode(mi, ctx) {
-        convexity = 0;
-        fn = fs = fa = 0;
-        delta = height = 1;
-        slices = 1;
-        center = chamfer = false;
-        miter_limit = 1000000.0;
-        join_type = ClipperLib::jtRound;
-    }
-    std::string toString() const override;
-    std::string name() const override { return "offset_extrude"; }
+  VISITABLE();
+  OffsetExtrudeNode(const ModuleInstantiation *mi, const std::shared_ptr<EvalContext> &ctx) : AbstractPolyNode(mi, ctx) {
+    convexity = 0;
+    fn = fs = fa = 0;
+    delta = height = 1;
+    slices = 1;
+    center = chamfer = false;
+    miter_limit = 1000000.0;
+    join_type = ClipperLib::jtRound;
+  }
+  std::string toString() const override;
+  std::string name() const override { return "offset_extrude"; }
 
-    bool chamfer, center;
-    double delta, height, fn, fs, fa;
-    int slices, convexity;
-    double miter_limit; // currently fixed high value to disable chamfers with jtMiter
-    ClipperLib::JoinType join_type;
+  bool chamfer, center;
+  double delta, height, fn, fs, fa;
+  int slices, convexity;
+  double miter_limit; // currently fixed high value to disable chamfers with jtMiter
+  ClipperLib::JoinType join_type;
 };
