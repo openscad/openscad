8bf6470c61f428ecbe0ba07b916e311165d3e10b
Don Calderwood <dec@asgardsw.com>
2021-01-03 16:09:49 -0500

    don't show autocomplete in strings. removed extraneous global variable. (fixes #3563)

--- src/scadapi.cc
+++ src/scadapi.cc
@@ -19,6 +19,26 @@ ScadApi::~ScadApi()
 
 void ScadApi::updateAutoCompletionList(const QStringList &context, QStringList &list)
 {
+    //first see if we are in a string literal. if so, don't allow auto comlete
+    bool lastWasEscape=false;
+    bool inString=false;
+    int line, col;
+    qsci->getCursorPosition(&line, &col);
+    auto sLine = qsci->text(line);  //get the current line of text
+    QByteArray ba = sLine.toLocal8Bit();
+    const char *c_str = ba.data();
+    const char *p = c_str;
+    while (col-- > 0) {
+        if (*p == '\\')
+            lastWasEscape = true;   //next character will be literal handle \"
+        else if (lastWasEscape)
+            lastWasEscape = false;
+        else if (*p == '"')        //string toggle
+            inString = !inString;
+        p++;
+    }
+    if (inString) return;  //we are in string literal, don't return autocomplete list
+    //not in string literal, proceed as normal
 	const QString c = context.last();
 	for (int a = 0; a < funcs.size(); ++a) {
 		const ApiFunc &func = funcs.at(a);
