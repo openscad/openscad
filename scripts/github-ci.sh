959035c72a5a9ae2a740a31361fe46f13bc32da6
Olivier Chafik <ochafik@google.com>
2021-02-13 22:58:11 +0000

    [fast-csg] fast-csg with CGAL 4.x on github ci w/ cmake (+ avoid weird lazy-union interaction)

--- scripts/github-ci.sh
+++ scripts/github-ci.sh
@@ -19,7 +19,13 @@ do_build() {
 	mkdir "$BUILDDIR"
 	(
 		cd "$BUILDDIR"
-		cmake -DCMAKE_BUILD_TYPE=Release -DEXPERIMENTAL=ON -DPROFILE=ON .. && make $PARALLEL_MAKE
+		cmake \
+			-DCMAKE_BUILD_TYPE=Release \
+			-DEXPERIMENTAL=ON \
+			-DPROFILE=ON \
+			-DFAST_CSG_AVAILABLE=${FAST_CSG_AVAILABLE:-1} \
+			.. && \
+		make $PARALLEL_MAKE
 	)
 	if [[ $? != 0 ]]; then
 		echo "Build failure"
