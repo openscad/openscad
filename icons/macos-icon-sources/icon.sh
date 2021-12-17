1b9e2b5b0dadf28a9b34c81c6bf009520c28b058
Johannes Kreutz <jkm@jkm-marburg.de>
2021-12-18 00:32:06 +0100

    Update macOS document icon to new style

--- icons/macos-icon-sources/icon.sh
+++ icons/macos-icon-sources/icon.sh
@@ -25,3 +25,17 @@ sips -z 512 512   Icon1024Nightly.png --out icon-nightly.iconset/icon_512x512.pn
 cp Icon1024Nightly.png icon-nightly.iconset/icon_512x512@2x.png
 iconutil -c icns icon-nightly.iconset
 rm -R icon-nightly.iconset
+
+mkdir SCAD.iconset
+sips -z 16 16     SCAD.png --out SCAD.iconset/icon_16x16.png
+sips -z 32 32     SCAD.png --out SCAD.iconset/icon_16x16@2x.png
+sips -z 32 32     SCAD.png --out SCAD.iconset/icon_32x32.png
+sips -z 64 64     SCAD.png --out SCAD.iconset/icon_32x32@2x.png
+sips -z 128 128   SCAD.png --out SCAD.iconset/icon_128x128.png
+sips -z 256 256   SCAD.png --out SCAD.iconset/icon_128x128@2x.png
+sips -z 256 256   SCAD.png --out SCAD.iconset/icon_256x256.png
+sips -z 512 512   SCAD.png --out SCAD.iconset/icon_256x256@2x.png
+sips -z 512 512   SCAD.png --out SCAD.iconset/icon_512x512.png
+cp SCAD.png SCAD.iconset/icon_512x512@2x.png
+iconutil -c icns SCAD.iconset
+rm -R SCAD.iconset
