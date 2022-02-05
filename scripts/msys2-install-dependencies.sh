943cab75169319dfe835f21115c874a16049e533
Torsten Paul <Torsten.Paul@gmx.de>
2022-02-05 04:56:55 +0100

    Pin cgal version 5.2-3.

--- scripts/msys2-install-dependencies.sh
+++ scripts/msys2-install-dependencies.sh
@@ -10,7 +10,7 @@ pacman --query --explicit
 for pkg in \
     mingw-w64-x86_64-toolchain \
     mingw-w64-x86_64-boost \
-    mingw-w64-x86_64-cgal \
+    mingw-w64-x86_64-cgal=5.2-3 \
     mingw-w64-x86_64-eigen3 \
     mingw-w64-x86_64-glew \
     mingw-w64-x86_64-qscintilla \
