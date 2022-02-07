7d1da1994857a868b35639edb22d3a1c2968f859
Torsten Paul <Torsten.Paul@gmx.de>
2022-02-07 01:22:43 +0100

    Update formatting.

--- src/context.cc
+++ src/context.cc
@@ -49,8 +49,8 @@ Context::Context(const std::shared_ptr<const Context>& parent) :
 Context::~Context()
 {
   clear();
-    if (accountingAdded) // avoiding bad accounting where exception threw in constructor  issue #3871
-  session()->contextMemoryManager().releaseContext();
+  if (accountingAdded)   // avoiding bad accounting where exception threw in constructor  issue #3871
+    session()->contextMemoryManager().releaseContext();
 }
 
 const Children *Context::user_module_children() const
