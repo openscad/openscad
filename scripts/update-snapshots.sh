caa47480e677414557992babe64ed33f1559e2ab
Torsten Paul <Torsten.Paul@gmx.de>
2022-03-05 19:10:18 +0100

    Update Circle-CI download script.

--- scripts/update-snapshots.sh
+++ scripts/update-snapshots.sh
@@ -1,8 +1,12 @@
 #!/bin/bash -e
 
-NAME=( "OpenSCAD-*.AppImage" "OpenSCAD-*.dmg"     "OpenSCAD-*-x86-32.zip" "OpenSCAD-*-x86-32-Installer.exe" "OpenSCAD-*-x86-64.zip" "OpenSCAD-*-x86-64-Installer.exe" )
-KEY=(  "LIN64_SNAPSHOT"      "MAC_SNAPSHOT"       "WIN32_SNAPSHOT_ZIP"    "WIN32_SNAPSHOT_INSTALLER"        "WIN64_SNAPSHOT_ZIP"    "WIN64_SNAPSHOT_INSTALLER"        )
-OUT=(  ".snapshot_linux.js"  ".snapshot_macos.js" ".snapshot_win32.js"    ".snapshot_win32.js"              ".snapshot_win64.js"    ".snapshot_win64.js"              )
+NAME=( "OpenSCAD-*-x86_64.AppImage" "OpenSCAD-*-aarch64.AppImage" "OpenSCAD-WASM-*.zip" "OpenSCAD-*.dmg"     "OpenSCAD-*-x86-32.zip" "OpenSCAD-*-x86-32-Installer.exe" "OpenSCAD-*-x86-64.zip" "OpenSCAD-*-x86-64-Installer.exe" )
+KEY=(  "LIN64_SNAPSHOT"             "LIN_AARCH64_SNAPSHOT"        "WASM_SNAPSHOT"       "MAC_SNAPSHOT"       "WIN32_SNAPSHOT_ZIP"    "WIN32_SNAPSHOT_INSTALLER"        "WIN64_SNAPSHOT_ZIP"    "WIN64_SNAPSHOT_INSTALLER"        )
+OUT=(  ".snapshot_linux_x86_64.js"  ".snapshot_linux_aarch64.js"  ".snapshot_wasm.js"   ".snapshot_macos.js" ".snapshot_win32.js"    ".snapshot_win32.js"              ".snapshot_win64.js"    ".snapshot_win64.js"              )
+
+#NAME=( "OpenSCAD-*-x86_64.AppImage" "OpenSCAD-*-aarch64.AppImage" "OpenSCAD-*.dmg"     )
+#KEY=(  "LIN64_SNAPSHOT"             "LIN_AARCH64_SNAPSHOT"        "MAC_SNAPSHOT"       )
+#OUT=(  ".snapshot_linux_x86_64.js"  ".snapshot_linux_aarch64.js"  ".snapshot_macos.js" )
 
 for o in ${OUT[*]}
 do
@@ -12,10 +16,14 @@ done
 
 for n in $(seq 0 $((${#NAME[@]} - 1)))
 do
-	FILE="$(ls -t ${NAME[$n]} | head -n 1)"
-        DATE="$(echo "$FILE" | cut -b 1-19)"
-	SIZE="$((($(stat --format=%s "$FILE") / 1024 + 512) / 1024)) MB"
-	echo "setSnapshotFileInfo('${KEY[$n]}', '$DATE', '$SIZE', 'https://files.openscad.org/snapshots/$FILE');" >> "${OUT[$n]}".tmp
+	FILE="$(ls -t ${NAME[$n]} 2>/dev/null | head -n 1)"
+	if [ -f "$FILE" ]; then
+        	DATE="$(echo "$FILE" | cut -b 1-19)"
+		SIZE="$((($(stat --format=%s "$FILE") / 1024 + 512) / 1024)) MB"
+		echo "setSnapshotFileInfo('${KEY[$n]}', '$DATE', '$SIZE', 'https://files.openscad.org/snapshots/$FILE');" >> "${OUT[$n]}".tmp
+	else
+		echo "File not found ${NAME[$n]}!"
+	fi
 done
 
 for o in ${OUT[*]}
