6e669f0573bdb8e2db23e606b58f8550f1813148
Torsten Paul <Torsten.Paul@gmx.de>
2022-03-05 21:50:42 +0100

    Fix filename to match website expectation.

--- scripts/update-snapshots.sh
+++ scripts/update-snapshots.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
-NAME=( "OpenSCAD-*-x86_64.AppImage" "OpenSCAD-*-aarch64.AppImage" "OpenSCAD-WASM-*.zip" "OpenSCAD-*.dmg"     "OpenSCAD-*-x86-32.zip" "OpenSCAD-*-x86-32-Installer.exe" "OpenSCAD-*-x86-64.zip" "OpenSCAD-*-x86-64-Installer.exe" )
-KEY=(  "LIN64_SNAPSHOT"             "LIN_AARCH64_SNAPSHOT"        "WASM_SNAPSHOT"       "MAC_SNAPSHOT"       "WIN32_SNAPSHOT_ZIP"    "WIN32_SNAPSHOT_INSTALLER"        "WIN64_SNAPSHOT_ZIP"    "WIN64_SNAPSHOT_INSTALLER"        )
-OUT=(  ".snapshot_linux_x86_64.js"  ".snapshot_linux_aarch64.js"  ".snapshot_wasm.js"   ".snapshot_macos.js" ".snapshot_win32.js"    ".snapshot_win32.js"              ".snapshot_win64.js"    ".snapshot_win64.js"              )
+NAME=( "OpenSCAD-*-x86_64.AppImage" "OpenSCAD-*-aarch64.AppImage" "OpenSCAD-*-WebAssembly.zip" "OpenSCAD-*.dmg"     "OpenSCAD-*-x86-32.zip" "OpenSCAD-*-x86-32-Installer.exe" "OpenSCAD-*-x86-64.zip" "OpenSCAD-*-x86-64-Installer.exe" )
+KEY=(  "LIN64_SNAPSHOT"             "LIN_AARCH64_SNAPSHOT"        "WASM_SNAPSHOT"              "MAC_SNAPSHOT"       "WIN32_SNAPSHOT_ZIP"    "WIN32_SNAPSHOT_INSTALLER"        "WIN64_SNAPSHOT_ZIP"    "WIN64_SNAPSHOT_INSTALLER"        )
+OUT=(  ".snapshot_linux_x86_64.js"  ".snapshot_linux_aarch64.js"  ".snapshot_wasm.js"          ".snapshot_macos.js" ".snapshot_win32.js"    ".snapshot_win32.js"              ".snapshot_win64.js"    ".snapshot_win64.js"              )
 
 #NAME=( "OpenSCAD-*-x86_64.AppImage" "OpenSCAD-*-aarch64.AppImage" "OpenSCAD-*.dmg"     )
 #KEY=(  "LIN64_SNAPSHOT"             "LIN_AARCH64_SNAPSHOT"        "MAC_SNAPSHOT"       )
