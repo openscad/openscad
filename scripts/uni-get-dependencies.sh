4d630cb4647aa86c6bfc921bc81da32ed9b597a9
lukas <lukas.heiligenbrunner@gmail.com>
2022-03-19 14:44:50 +0100

    remove wrong/not existing qscintilla-devel build dependency for fedora

--- scripts/uni-get-dependencies.sh
+++ scripts/uni-get-dependencies.sh
@@ -10,7 +10,7 @@ get_fedora_deps_yum()
   fontconfig-devel freetype-devel \
   boost-devel mpfr-devel gmp-devel glew-devel CGAL-devel gcc gcc-c++ pkgconfig \
   opencsg-devel git libXmu-devel curl imagemagick ImageMagick glib2-devel make \
-  xorg-x11-server-Xvfb gettext qscintilla-devel qscintilla-qt5-devel \
+  xorg-x11-server-Xvfb gettext qscintilla-qt5-devel \
   mesa-dri-drivers double-conversion-devel
 }
 
@@ -20,7 +20,7 @@ get_fedora_deps_dnf()
   fontconfig-devel freetype-devel \
   boost-devel mpfr-devel gmp-devel glew-devel CGAL-devel gcc gcc-c++ pkgconfig \
   opencsg-devel git libXmu-devel curl ImageMagick glib2-devel make \
-  xorg-x11-server-Xvfb gettext qscintilla-devel qscintilla-qt5-devel \
+  xorg-x11-server-Xvfb gettext qscintilla-qt5-devel \
   mesa-dri-drivers libzip-devel ccache qt5-qtmultimedia-devel qt5-qtsvg-devel \
   double-conversion-devel
  dnf -y install libxml2-devel
