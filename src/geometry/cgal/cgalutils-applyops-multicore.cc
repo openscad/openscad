d8c970dfab29a7a663bdffbb9aee8d8cc00249a1
parvit (Vittorio Parrella) <vittorioparrella+1@gmail.com>
2022-07-01 16:01:18 -0400

    single separated thread execution

--- src/geometry/cgal/cgalutils-applyops-multicore.cc
+++ src/geometry/cgal/cgalutils-applyops-multicore.cc
@@ -13,40 +13,98 @@
 #include "node.h"
 #include "progress.h"
 
-void applyUnion3DMulticoreWorker(Geometry::Geometries::iterator chbegin,
-																 Geometry::Geometries::iterator chend,
-																 shared_ptr<const Geometry>* partialResult );
+#include <list>
+#include <thread>
 
 namespace CGALUtils {
+
+void applyUnion3DMulticoreWorker(Geometry::Geometries::iterator& chbegin,
+																 Geometry::Geometries::iterator& chend,
+																 shared_ptr<const Geometry>& partialResult );
+
+void applyOperator3DMulticoreWorker(const Geometry::Geometries& children,
+																		OpenSCADOperator op, shared_ptr<const Geometry> partialResult );
+
+void applyHullMulticoreWorker(const Geometry::Geometries& childrens, PolySet& result, bool& continueExec );
 
 shared_ptr<const Geometry> applyUnion3DMulticore(
   Geometry::Geometries::iterator chbegin,
   Geometry::Geometries::iterator chend)
 {
-	applyUnion3DMulticoreWorker(chbegin, chend, NULL);
-  return nullptr;
+//	int numElements = std::distance( chbegin, chend );
+//	unsigned int numCores = std::thread::hardware_concurrency();
+//
+//	if( numElements < numCores * 2 ) {
+//		// needs at least two elements per core, if  not then fallback basic
+//		return applyBasicUnion3D( chbegin, chend );
+//	}
+
+	shared_ptr<const Geometry> partialResult;
+	std::thread t1( applyUnion3DMulticoreWorker, std::ref(chbegin), std::ref(chend), std::ref(partialResult) );
+
+	t1.join();
+
+  return partialResult;
 }
 
 shared_ptr<const Geometry> applyOperator3DMulticore(
   const Geometry::Geometries& children, 
   OpenSCADOperator op)
 {
-  return nullptr;
+	//	int numElements = std::distance( chbegin, chend );
+	//	unsigned int numCores = std::thread::hardware_concurrency();
+	//
+	//	if( numElements < numCores * 2 ) {
+	//		// needs at least two elements per core, if  not then fallback basic
+	//		return applyBasicUnion3D( chbegin, chend );
+	//	}
+
+	shared_ptr<const Geometry> partialResult;
+	std::thread t1( applyOperator3DMulticoreWorker, std::ref(children), op, std::ref(partialResult) );
+
+	t1.join();
+
+	return partialResult;
 }
 
 bool applyHullMulticore(const Geometry::Geometries& children, PolySet& result)
 {
-  return false;
+	//	int numElements = std::distance( chbegin, chend );
+	//	unsigned int numCores = std::thread::hardware_concurrency();
+	//
+	//	if( numElements < numCores * 2 ) {
+	//		// needs at least two elements per core, if  not then fallback basic
+	//		return applyBasicUnion3D( chbegin, chend );
+	//	}
+
+	bool resultFlag = false;
+	std::thread t1( applyHullMulticoreWorker, std::ref(children), std::ref(result), std::ref(resultFlag) );
+
+	t1.join();
+
+	return resultFlag;
+}
+
+void applyUnion3DMulticoreWorker(Geometry::Geometries::iterator& chbegin,
+																 Geometry::Geometries::iterator& chend,
+																 shared_ptr<const Geometry>& partialResult )
+{
+	shared_ptr<const Geometry> tmpResult = applyBasicUnion3D(chbegin, chend);
+	partialResult.swap( tmpResult );
+}
+
+void applyOperator3DMulticoreWorker(const Geometry::Geometries& children,
+																		OpenSCADOperator op, shared_ptr<const Geometry> partialResult )
+{
+	shared_ptr<const Geometry> tmpResult = applyBasicOperator3D(children, op);
+	partialResult.swap( tmpResult );
+}
+
+void applyHullMulticoreWorker(const Geometry::Geometries& children, PolySet& result, bool& continueExec )
+{
+	continueExec = applyBasicHull(children, result);
 }
 
 }  // namespace CGALUtils
-
-void applyUnion3DMulticoreWorker(Geometry::Geometries::iterator chbegin,
-																 Geometry::Geometries::iterator chend,
-																 shared_ptr<const Geometry>* partialResult )
-{
-
-}
-
 
 #endif // ENABLE_CGAL
