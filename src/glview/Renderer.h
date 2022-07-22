121d59314f079410ced3772a3e12af6d72f7e4ac
Joseph Black <17530522+jbinvnt@users.noreply.github.com>
2022-07-21 19:06:52 -0500

    Fix deadlock after shader change and allow instant update without re-render

--- src/glview/Renderer.h
+++ src/glview/Renderer.h
@@ -43,6 +43,7 @@ public:
   [[nodiscard]] virtual inline const Renderer::shaderinfo_t& getShader() const { return renderer_shader; }
 
   static std::string loadShaderSource(const std::string& name, const std::string* location = nullptr);
+  virtual void setShader(const std::string* shaderDirectoryPath);
   virtual void prepare(bool showfaces, bool showedges, const shaderinfo_t *shaderinfo = nullptr) {}
   virtual void draw(bool showfaces, bool showedges, const shaderinfo_t *shaderinfo = nullptr) const = 0;
   [[nodiscard]] virtual BoundingBox getBoundingBox() const = 0;
