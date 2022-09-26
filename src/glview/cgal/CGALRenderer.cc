e5c73c4246efbde8ecb13df5395f40e675f997cc
Joseph Black <17530522+jbinvnt@users.noreply.github.com>
2022-09-25 19:36:04 -0500

    Stop using transparency to control when faces are shown, and not render them instead

--- src/glview/cgal/CGALRenderer.cc
+++ src/glview/cgal/CGALRenderer.cc
@@ -226,13 +226,13 @@ void CGALRenderer::createPolySets(bool showfaces)
         shaderinfo_t shader_info = this->getShader();
         glUniform1i(shader_info.data.csg_rendering.draw_edges, 0);
         std::shared_ptr<VertexState> color_state = std::make_shared<VBOShaderVertexState>(0, 0, vertex_array.verticesVBO(), vertex_array.elementsVBO());
-        color_state->glBegin().emplace_back([shader_info, last_color, marked_color, showfaces]() {
-          GL_TRACE("glUniform4f(%d, %f, %f, %f, %f)", shader_info.data.csg_rendering.color_area % last_color[0] % last_color[1] % last_color[2] % showfaces ? last_color[3] : 0.0);
-          glUniform4f(shader_info.data.csg_rendering.color_area, last_color[0], last_color[1], last_color[2], showfaces ? last_color[3] : 0.0); GL_ERROR_CHECK();
+        color_state->glBegin().emplace_back([shader_info, last_color, marked_color]() {
+          GL_TRACE("glUniform4f(%d, %f, %f, %f, %f)", shader_info.data.csg_rendering.color_area % last_color[0] % last_color[1] % last_color[2] % last_color[3]);
+          glUniform4f(shader_info.data.csg_rendering.color_area, last_color[0], last_color[1], last_color[2], last_color[3]); GL_ERROR_CHECK();
           GL_TRACE("glUniform4f(%d, %f, %f, %f, 1.0)", shader_info.data.csg_rendering.color_edge % ((last_color[0] + 1) / 2) % ((last_color[1] + 1) / 2) % ((last_color[2] + 1) / 2));
           glUniform4f(shader_info.data.csg_rendering.color_edge, (last_color[0] + 1) / 2, (last_color[1] + 1) / 2, (last_color[2] + 1) / 2, 1.0); GL_ERROR_CHECK();
-          GL_TRACE("glUniform4f(%d, %f, %f, %f, %f)", shader_info.data.csg_rendering.color_marked % marked_color[0] % marked_color[1] % marked_color[2] % showfaces ? marked_color[3] : 0.0);
-          glUniform4f(shader_info.data.csg_rendering.color_marked, marked_color[0], marked_color[1], marked_color[2], showfaces ? marked_color[3] : 0.0); GL_ERROR_CHECK();
+          GL_TRACE("glUniform4f(%d, %f, %f, %f, %f)", shader_info.data.csg_rendering.color_marked % marked_color[0] % marked_color[1] % marked_color[2] % marked_color[3]);
+          glUniform4f(shader_info.data.csg_rendering.color_marked, marked_color[0], marked_color[1], marked_color[2], marked_color[3]); GL_ERROR_CHECK();
         });
         polyset_states.emplace_back(std::move(color_state));
       }
@@ -306,7 +306,7 @@ void CGALRenderer::draw(bool showfaces, bool showedges, const shaderinfo_t * sha
       }
     }
     #endif //DISABLE_FIXEDFUNCTION_GL
-  } else {
+  } else if(showfaces) {
     GLint prev_id;
     glGetIntegerv(GL_CURRENT_PROGRAM, &prev_id);
     PRINTDB("Previously, was using shader ID: %d\n", prev_id);
