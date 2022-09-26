d51e5fbd9ebe5a42e77ccba9571388b00b5be3aa
Joseph Black <17530522+jbinvnt@users.noreply.github.com>
2022-09-25 23:37:07 -0500

    Adjust the shader data location to be based on polysets size

--- src/glview/cgal/CGALRenderer.cc
+++ src/glview/cgal/CGALRenderer.cc
@@ -144,7 +144,7 @@ void CGALRenderer::createPolySets(bool showfaces)
   vertex_array.addEdgeData();
   vertex_array.addSurfaceData();
   vertex_array.writeSurface();
-  if(Feature::ExperimentalLazyUnion.is_enabled()) {
+  if(this->polysets.size() > 1) {
     add_shader_data(vertex_array);
   }
 
@@ -229,7 +229,7 @@ void CGALRenderer::createPolySets(bool showfaces)
       was_2d = true;
     } else {
       PRINTD("3d polysets");
-      if(!Feature::ExperimentalLazyUnion.is_enabled()) {
+      if(this->polysets.size() == 1) {
         add_shader_data(vertex_array);
       }
       vertex_array.writeSurface();
@@ -347,14 +347,12 @@ void CGALRenderer::draw(bool showfaces, bool showedges, const shaderinfo_t * sha
     size_t i{0};
     for (const auto& polyset : polyset_states) {
       if (polyset) {
-        if (!Feature::ExperimentalLazyUnion.is_enabled()) {
-          if(polyset_2d_locations[i++]) {
-            glUseProgram(prev_id);
-            GL_ERROR_CHECK();
-          } else {
-            glUseProgram(new_id);
-            GL_ERROR_CHECK();
-          }
+        if(polyset_2d_locations[i++]) {
+          glUseProgram(prev_id);
+          GL_ERROR_CHECK();
+        } else {
+          glUseProgram(new_id);
+          GL_ERROR_CHECK();
         }
         polyset->draw();
       }
