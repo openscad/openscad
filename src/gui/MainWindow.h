71cc0312712f8ca537ad6d1c271c449e5c17fc9b
Joseph Black <17530522+jbinvnt@users.noreply.github.com>
2022-10-01 16:59:07 -0500

    Add an option to revert to the default shader

--- src/gui/MainWindow.h
+++ src/gui/MainWindow.h
@@ -336,6 +336,8 @@ public slots:
   void viewTogglePerspective();
   void viewResetView();
   void setShader();
+  void useDefaultShader();
+  void useShader(const std::string& shaderLocation);
   void viewAll();
   void editorContentChanged();
   void selectObject(QPoint coordinate);
