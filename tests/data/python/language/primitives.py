f38aa55404fb11cd1e287e96fac35a23e46262ae
Guenther Sohler <guenther.sohler@photeon.com>
2023-02-09 17:23:48 +0100

    added primitives test with basic functions

--- tests/data/python/language/primitives.py
+++ tests/data/python/language/primitives.py
@@ -1,2 +1,21 @@
-cube().output()
+# Sample python script to check *most* available python bindings and their
+# parameters
+
+fn=20
+
+cube1 = cube([3,2,1],center=True)
+cube2 = cube(1.5).rotate([10,20,30])
+cube3 = cube([5,1,1],center=True)
+sphere1 = sphere(r=2,fn=6)
+sphere2 = sphere(d=4,fn=20)
+cylinder1 = cylinder(r=2,h=4)
+cylinder2 = cylinder(r=1,h=8,center=True)
+
+output([
+        cube1.translate([0,1,0]),
+        cube2.translate([4,0,0]),
+        sphere1.translate([0,5,0]),
+        sphere2.translate([4,5,0]),
+        (cylinder1 + cube3 -cylinder2).translate([0,10,0]),
+    ])
 
