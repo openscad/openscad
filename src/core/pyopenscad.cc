7e39b167aed20112a29d7b5ed922e53c67508624
Guenther Sohler <guenther.sohler@photeon.com>
2023-02-12 17:20:00 +0100

    simplified structure initialisation syntax

--- src/core/pyopenscad.cc
+++ src/core/pyopenscad.cc
@@ -242,17 +242,44 @@ PyMappingMethods PyOpenSCADMapping =
 
 
 PyTypeObject PyOpenSCADType = {
-  PyVarObject_HEAD_INIT(NULL, 0)
-  .tp_name = "PyOpenSCAD",
-  .tp_basicsize = sizeof(PyOpenSCADObject),
-  .tp_dealloc = (destructor) PyOpenSCADObject_dealloc,
-  .tp_as_number = &PyOpenSCADNumbers,
-  .tp_as_mapping = &PyOpenSCADMapping,
-  .tp_flags = Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE,
-  .tp_doc = "PyOpenSCAD objects",
-  .tp_methods = PyOpenSCADMethods,
-  .tp_init = (initproc) PyOpenSCADInit,
-  .tp_new = PyOpenSCADObject_new
+    PyVarObject_HEAD_INIT(NULL, 0)
+    "PyOpenSCAD",             			/* tp_name */
+    sizeof(PyOpenSCADObject), 			/* tp_basicsize */
+    0,                         			/* tp_itemsize */
+    (destructor) PyOpenSCADObject_dealloc,	/* tp_dealloc */
+    0,                         			/* tp_print */
+    0,                         			/* tp_getattr */
+    0,                         			/* tp_setattr */
+    0,                         			/* tp_reserved */
+    0,                         			/* tp_repr */
+    &PyOpenSCADNumbers,        			/* tp_as_number */
+    0,                         			/* tp_as_sequence */
+    &PyOpenSCADMapping,        			/* tp_as_mapping */
+    0,                         			/* tp_hash  */
+    0,                         			/* tp_call */
+    0,                         			/* tp_str */
+    0,                         			/* tp_getattro */
+    0,                         			/* tp_setattro */
+    0,                         			/* tp_as_buffer */
+    Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE,	/* tp_flags */
+    "PyOpenSCAD Object",          		/* tp_doc */
+    0,                         			/* tp_traverse */
+    0,                         			/* tp_clear */
+    0,                         			/* tp_richcompare */
+    0,                         			/* tp_weaklistoffset */
+    0,                         			/* tp_iter */
+    0,                         			/* tp_iternext */
+    PyOpenSCADMethods,             		/* tp_methods */
+    0,             				/* tp_members */
+    0,                         			/* tp_getset */
+    0,                         			/* tp_base */
+    0,                         			/* tp_dict */
+    0,                         			/* tp_descr_get */
+    0,                         			/* tp_descr_set */
+    0,                         			/* tp_dictoffset */
+    (initproc) PyOpenSCADInit,      		/* tp_init */
+    0,                         			/* tp_alloc */
+    PyOpenSCADObject_new,                	/* tp_new */
 };
 
 
