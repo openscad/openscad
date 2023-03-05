196df361672f852b7dc86c3d7f65ec498ae223c8
Robert Schiele <rschiele@gmail.com>
2023-03-05 09:54:28 +0100

    implement new scale and translate parameter to rotate_extrude
    This implements two new vector parameters scale and translate to
    rotate_extrude with similar behavior that is known of twist in
    linear_extrude.
    
    - scale: scale the base polygon along x and y axis in a linear way from
      [1,1] to the specified value from the start of the rotation to the
      end.  Since this parameter name was also used by the deprecated file
      importing function it is only available when not using file importing
      function.
    
    - translate: translate the base polygon along x and y axis in a linear
      way from [0,0] to the specified value from the start of the rotation
      to the end.

--- src/core/RotateExtrudeNode.h
+++ src/core/RotateExtrudeNode.h
@@ -12,13 +12,15 @@ public:
     fn = fs = fa = 0;
     origin_x = origin_y = scale = 0;
     angle = 360;
+    scale_x = scale_y = 1;
+    trans_x = trans_y = 0;
   }
   std::string toString() const override;
   std::string name() const override { return "rotate_extrude"; }
 
   int convexity;
   double fn, fs, fa;
-  double origin_x, origin_y, scale, angle;
+  double origin_x, origin_y, scale, angle, scale_x, scale_y, trans_x, trans_y;
   Filename filename;
   std::string layername;
 };
