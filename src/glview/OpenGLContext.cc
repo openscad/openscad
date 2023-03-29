fcfac559822a2a7c529e7902dec4ff92a96bc0c1
Marius Kintel <marius@kintel.net>
2023-03-29 16:15:19 -0400

    details

--- src/glview/OpenGLContext.cc
+++ src/glview/OpenGLContext.cc
@@ -6,9 +6,9 @@
 
 std::vector<uint8_t> OpenGLContext::getFramebuffer() const
 {
-  int samplesPerPixel = 4; // R, G, B and A
-  int rowBytes = samplesPerPixel * this->width_;
+  const uint8_t samplesPerPixel = 4; // R, G, B and A
+  const size_t rowBytes = samplesPerPixel * this->width_;
   std::vector<uint8_t> buffer(rowBytes * this->height_);
   GL_CHECK(glReadPixels(0, 0, this->width_, this->height_, GL_RGBA, GL_UNSIGNED_BYTE, buffer.data()));
-  return std::move(buffer);
+  return buffer;
 }
