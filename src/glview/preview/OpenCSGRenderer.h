7d6a4248f00da49e2338969de8f132285522572c
Guenther Sohler <guenther.sohler@photeon.com>
2023-11-20 22:26:26 +0100

    Preparation for several different types

--- src/glview/preview/OpenCSGRenderer.h
+++ src/glview/preview/OpenCSGRenderer.h
@@ -104,6 +104,7 @@ private:
   void createCSGVBOProducts(const CSGProducts& products, const Renderer::shaderinfo_t *shaderinfo, bool highlight_mode, bool background_mode);
   void renderCSGVBOProducts(bool showedges, const Renderer::shaderinfo_t *shaderinfo) const;
   std::vector<Vector3d> findModelPoint(Vector3d near, Vector3d far, int mouse_x, int mouse_y, double tolerance) override;
+  std::vector<SelectedObject> findModelObject(Vector3d near, Vector3d far, int mouse_x, int mouse_y, double tolerance) override;
 
   OpenCSGVBOProducts vbo_vertex_products;
   std::vector<GLuint> vertices_vbos_;
