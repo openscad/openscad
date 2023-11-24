4a069b466b889d2761b8fe155d5e54527bf4ae73
Guenther Sohler <guenther.sohler@photeon.com>
2023-11-24 08:04:22 +0100

    fix typo, was masked tue to different 3mf lib

--- src/io/import_3mf.cc
+++ src/io/import_3mf.cc
@@ -164,7 +164,7 @@ Geometry *import_3mf(const std::string& filename, const Location& loc)
         if (lib3mf_meshobject_getvertex(object, triangle.m_nIndices[i], &vertex) != LIB3MF_OK) {
           return import_3mf_error(model, object_it, first_mesh);
         }
-        builder.appendVrtex(builder.vertexIndex(Vector3d(vertex.m_fPosition[0], vertex.m_fPosition[1], vertex.m_fPosition[2])));
+        builder.appendVertex(builder.vertexIndex(Vector3d(vertex.m_fPosition[0], vertex.m_fPosition[1], vertex.m_fPosition[2])));
       }
     }
 
