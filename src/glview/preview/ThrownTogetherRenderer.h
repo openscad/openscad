484feb714450f0d3dc9092fb1dd2fc7fa80cf501
Marius Kintel <marius@kintel.net>
2024-01-26 16:37:17 -0800

    Minor refactoring

--- src/glview/preview/ThrownTogetherRenderer.h
+++ src/glview/preview/ThrownTogetherRenderer.h
@@ -2,7 +2,6 @@
 
 #include "Renderer.h"
 #include "CSGNode.h"
-
 #include "VBORenderer.h"
 
 class CSGProducts;
@@ -11,12 +10,8 @@ class CSGChainObject;
 class TTRVertexState : public VertexState
 {
 public:
-  TTRVertexState()
-    : VertexState(), csg_object_index_(0)
-  {}
-  TTRVertexState(size_t csg_object_index)
-    : VertexState(), csg_object_index_(csg_object_index)
-  {}
+  TTRVertexState(size_t csg_object_index = 0)
+    : csg_object_index_(csg_object_index) {}
   TTRVertexState(GLenum draw_mode, GLsizei draw_size, GLenum draw_type,
                  size_t draw_offset, size_t element_offset, GLuint vertices_vbo, GLuint elements_vbo,
                  size_t csg_object_index = 0)
