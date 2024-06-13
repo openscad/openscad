e45fb5887e19156b7221521ede4b10d8b9964673
Guenther Sohler <guenther.sohler@photeon.com>
2024-06-13 16:28:30 +0200

    calculate Segment-to-Segment distance instead of Line-to-Line distance

--- src/gui/Measurement.h
+++ src/gui/Measurement.h
@@ -7,6 +7,7 @@ enum { MEASURE_IDLE, MEASURE_DIST1, MEASURE_DIST2, MEASURE_ANG1, MEASURE_ANG2, M
 
 extern double calculateLinePointDistance(const Vector3d &l1, const Vector3d &l2, const Vector3d &pt, double & dist_lat) ;
 extern double calculateLineLineDistance(const Vector3d &l1b, const Vector3d &l1e, const Vector3d &l2b, const Vector3d &l2e, double &dist_lat);
+extern double calculateSegSegDistance(const Vector3d &l1b, const Vector3d &l1e, const Vector3d &l2b, const Vector3d &l2e, double &dist_lat);
 
 class Measurement
 {
