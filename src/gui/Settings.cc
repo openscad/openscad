51a971dc5849acd492e4e2455f97b33c400e00ce
Folkert van Heusden <mail@vanheusden.com>
2024-09-22 10:34:43 +0200

    Implemented suggestions from https://github.com/openscad/openscad/pull/5288#pullrequestreview-2301552143

--- src/gui/Settings.cc
+++ src/gui/Settings.cc
@@ -125,7 +125,7 @@ SettingsEntryString Settings::octoPrintSlicerProfile("printing", "octoPrintSlice
 SettingsEntryString Settings::octoPrintSlicerProfileDesc("printing", "octoPrintSlicerProfileDesc", "");
 
 SettingsEntryBool Settings::exportUseAsciiSTL("export", "useAsciiSTL", false);
-SettingsEntryEnum Settings::toolbarExport3D("advanced", "toolbarExport3D", {{"none", "none"}, {"STL", "STL"}, {"OFF", "OFF"}, {"WRL", "WRL"}, {"AMF", "AMF"}, {"3MF", "3MF"}}, "STL");
+SettingsEntryEnum Settings::toolbarExport3D("advanced", "toolbarExport3D", {{"none", "none"}, {"STL", "STL"}, {"OFF", "OFF"}, {"WRL", "WRL"}, {"AMF", "AMF"}, {"3MF", "3MF"}, {"POV", "POV"}}, "STL");
 SettingsEntryEnum Settings::toolbarExport2D("advanced", "toolbarExport2D", {{"none", "none"}, {"DXF", "DXF"}, {"SVG", "SVG"}, {"PDF", "PDF"}}, "none");
 
 SettingsEntryBool Settings::summaryCamera("summary", "camera", false);
