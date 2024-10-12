8f9459d9aecadef42304467d7cfbf32c32a76205
Henner Zeller <h.zeller@acm.org>
2024-10-12 09:20:15 -0700

    Include <QPushButton> in files missing it.

--- src/gui/Animate.cc
+++ src/gui/Animate.cc
@@ -1,6 +1,7 @@
 #include "gui/Animate.h"
 #include "utils/printutils.h"
 #include "gui/MainWindow.h"
+#include <QPushButton>
 #include <QResizeEvent>
 #include <QTimer>
 #include <QWidget>
--- src/gui/Animate.h
+++ src/gui/Animate.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <QPushButton>
 #include <QResizeEvent>
 #include <QTimer>
 #include <QWidget>
