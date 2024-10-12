803965773bebe47eaad90d60efc3c13c5c470bfb
Henner Zeller <h.zeller@acm.org>
2024-10-12 09:20:15 -0700

    Include <QMouseEvent> in files missing it.

--- src/gui/QGLView.h
+++ src/gui/QGLView.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "glview/system-gl.h"
+#include <QMouseEvent>
 #include <QOpenGLContext>
 #include <QPoint>
 #include <QWheelEvent>
--- src/gui/TabWidget.h
+++ src/gui/TabWidget.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <QMouseEvent>
 #include <QString>
 #include <QWidget>
 #include <QTabBar>
