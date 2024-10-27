d5814cf8873cdf528278f834736a72fce5035dd6
Marius Kintel <marius@kintel.net>
2024-10-26 21:35:44 -0400

    Read from files.openscad.org

--- src/gui/PrintService.cc
+++ src/gui/PrintService.cc
@@ -58,7 +58,7 @@ createPrintServices() {
   // TODO: Where to call this, will we need a mutex?
   try {
     auto networkRequest = NetworkRequest<void>{
-        QUrl{"http://localhost:8000/print-service.json"}, {200}, 30};
+        QUrl{"https://files.openscad.org/print-service.json"}, {200}, 30};
     networkRequest.execute(
         [](QNetworkRequest &request) {
           request.setHeader(QNetworkRequest::ContentTypeHeader,
