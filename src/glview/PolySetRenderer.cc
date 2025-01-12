c376ced12e5b70383740a3bc23790e7a336aa142
Marius Kintel <marius@kintel.net>
2025-01-12 18:05:18 -0500

    PolySetRenderer fixes

--- src/glview/PolySetRenderer.cc
+++ src/glview/PolySetRenderer.cc
@@ -117,78 +117,93 @@ void PolySetRenderer::createVertexStates()
     glGenBuffers(1, &elements_vbo_);
   }
 
-  VertexArray vertex_array(std::make_unique<VertexStateFactory>(), 
-                           vertex_states_, vertices_vbo_, elements_vbo_);
+  VBOBuilder vbo_builder(std::make_unique<VertexStateFactory>(), 
+                          vertex_states_, vertices_vbo_, elements_vbo_);
 
-  vertex_array.addEdgeData();
-  vertex_array.addSurfaceData();
-
-
-  size_t num_vertices = 0;
-  for (const auto& polyset : this->polysets_) {
-    num_vertices += getSurfaceBufferSize(*polyset);
-    num_vertices += getEdgeBufferSize(*polyset);
+  if (!polysets_.empty() && !polygons_.empty()) {
+    LOG("Warning: PolySetRenderer has both 2D and 3D geometry. Only 3D geometry will be rendered.");
   }
-  for (const auto &[polygon, polyset] : this->polygons_) {
-    num_vertices += getSurfaceBufferSize(*polyset);
-    num_vertices += getEdgeBufferSize(*polygon);
+  if (!polysets_.empty()) {
+    vbo_builder.addSurfaceData();
+    if (getShader().progid != 0) {
+      vbo_builder.addShaderData();
+    } else {
+      LOG("Warning: Shader not available");
+    }
+
+    size_t num_vertices = 0;
+    for (const auto& polyset : this->polysets_) {
+      num_vertices += getSurfaceBufferSize(*polyset);
+    }
+
+    vbo_builder.allocateBuffers(num_vertices);
+
+    for (const auto& polyset : this->polysets_) {
+      Color4f color;
+
+      PRINTD("3d polysets");
+      vbo_builder.writeSurface();
+
+      // Create 3D polygons
+      getColor(ColorMode::MATERIAL, color);
+      add_color(vbo_builder, color);
+      this->create_surface(*polyset, vbo_builder, RendererUtils::CSGMODE_NORMAL,
+                          Transform3d::Identity(), color);
+    }
+  } else if (!polygons_.empty()) {
+    vbo_builder.addEdgeData();
+//    vbo_builder.addSurfaceData();
+
+    size_t num_vertices = 0;
+    for (const auto &[polygon, polyset] : this->polygons_) {
+      num_vertices += getSurfaceBufferSize(*polyset);
+      num_vertices += getEdgeBufferSize(*polygon);
+    }
+
+    vbo_builder.allocateBuffers(num_vertices);
+
+    for (const auto &[polygon, polyset] : this->polygons_) {
+      Color4f color;
+
+      PRINTD("2d polygons");
+      vbo_builder.writeEdge();
+
+      std::shared_ptr<VertexState> init_state = std::make_shared<VertexState>();
+      init_state->glEnd().emplace_back([]() {
+        GL_TRACE0("glDisable(GL_LIGHTING)");
+        GL_CHECKD(glDisable(GL_LIGHTING));
+      });
+      vertex_states_.emplace_back(std::move(init_state));
+
+      // Create 2D polygons
+      getColor(ColorMode::CGAL_FACE_2D_COLOR, color);
+      this->create_polygons(*polyset, vbo_builder, Transform3d::Identity(),
+                            color);
+
+      std::shared_ptr<VertexState> edge_state = std::make_shared<VertexState>();
+      edge_state->glBegin().emplace_back([]() {
+        GL_TRACE0("glDisable(GL_DEPTH_TEST)");
+        GL_CHECKD(glDisable(GL_DEPTH_TEST));
+      });
+      edge_state->glBegin().emplace_back([]() {
+        GL_TRACE0("glLineWidth(2)");
+        GL_CHECKD(glLineWidth(2));
+      });
+      vertex_states_.emplace_back(std::move(edge_state));
+
+      // Create 2D edges
+      getColor(ColorMode::CGAL_EDGE_2D_COLOR, color);
+      this->create_edges(*polygon, vbo_builder, Transform3d::Identity(), color);
+
+      std::shared_ptr<VertexState> end_state = std::make_shared<VertexState>();
+      end_state->glBegin().emplace_back([]() {
+        GL_TRACE0("glEnable(GL_DEPTH_TEST)");
+        GL_CHECKD(glEnable(GL_DEPTH_TEST));
+      });
+      vertex_states_.emplace_back(std::move(end_state));
+    }
   }
 
-  vertex_array.allocateBuffers(num_vertices);
-
-  for (const auto& polyset : this->polysets_) {
-    Color4f color;
-
-    PRINTD("3d polysets");
-    vertex_array.writeSurface();
-
-    // Create 3D polygons
-    getColor(ColorMode::MATERIAL, color);
-    this->create_surface(*polyset, vertex_array, RendererUtils::CSGMODE_NORMAL,
-                         Transform3d::Identity(), color);
-  }
-
-  for (const auto &[polygon, polyset] : this->polygons_) {
-    Color4f color;
-
-    PRINTD("2d polygons");
-    vertex_array.writeEdge();
-
-    std::shared_ptr<VertexState> init_state = std::make_shared<VertexState>();
-    init_state->glEnd().emplace_back([]() {
-      GL_TRACE0("glDisable(GL_LIGHTING)");
-      GL_CHECKD(glDisable(GL_LIGHTING));
-    });
-    vertex_states_.emplace_back(std::move(init_state));
-
-    // Create 2D polygons
-    getColor(ColorMode::CGAL_FACE_2D_COLOR, color);
-    this->create_polygons(*polyset, vertex_array, Transform3d::Identity(),
-                          color);
-
-    std::shared_ptr<VertexState> edge_state = std::make_shared<VertexState>();
-    edge_state->glBegin().emplace_back([]() {
-      GL_TRACE0("glDisable(GL_DEPTH_TEST)");
-      GL_CHECKD(glDisable(GL_DEPTH_TEST));
-    });
-    edge_state->glBegin().emplace_back([]() {
-      GL_TRACE0("glLineWidth(2)");
-      GL_CHECKD(glLineWidth(2));
-    });
-    vertex_states_.emplace_back(std::move(edge_state));
-
-    // Create 2D edges
-    getColor(ColorMode::CGAL_EDGE_2D_COLOR, color);
-    this->create_edges(*polygon, vertex_array, Transform3d::Identity(), color);
-
-    std::shared_ptr<VertexState> end_state = std::make_shared<VertexState>();
-    end_state->glBegin().emplace_back([]() {
-      GL_TRACE0("glEnable(GL_DEPTH_TEST)");
-      GL_CHECKD(glEnable(GL_DEPTH_TEST));
-    });
-    vertex_states_.emplace_back(std::move(end_state));
-  }
-  
   if (!this->polysets_.empty() || !this->polygons_.empty()) {
     if (Feature::ExperimentalVxORenderersIndexing.is_enabled()) {
       GL_TRACE0("glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0)");
@@ -197,8 +212,9 @@ void PolySetRenderer::createVertexStates()
     GL_TRACE0("glBindBuffer(GL_ARRAY_BUFFER, 0)");
     GL_CHECKD(glBindBuffer(GL_ARRAY_BUFFER, 0));
 
-    vertex_array.createInterleavedVBOs();
+    vbo_builder.createInterleavedVBOs();
   }
+
 }
 
 void PolySetRenderer::prepare(bool /*showfaces*/, bool /*showedges*/, const RendererUtils::ShaderInfo * /*shaderinfo*/)
@@ -210,20 +226,34 @@ void PolySetRenderer::prepare(bool /*showfaces*/, bool /*showedges*/, const Rend
   PRINTD("prepare() end");
 }
 
-void PolySetRenderer::draw(bool showfaces, bool showedges, const RendererUtils::ShaderInfo * /*shaderinfo*/) const
+void PolySetRenderer::draw(bool /*showfaces*/, bool showedges, const RendererUtils::ShaderInfo *shaderinfo) const
 {
   PRINTD("draw()");
+  if (!shaderinfo && showedges) shaderinfo = &getShader();
+
   // grab current state to restore after
   GLfloat current_point_size, current_line_width;
-  const GLboolean origVertexArrayState = glIsEnabled(GL_VERTEX_ARRAY);
+  const GLboolean origVBOBuilderState = glIsEnabled(GL_VERTEX_ARRAY);
   const GLboolean origNormalArrayState = glIsEnabled(GL_NORMAL_ARRAY);
   const GLboolean origColorArrayState = glIsEnabled(GL_COLOR_ARRAY);
 
   GL_CHECKD(glGetFloatv(GL_POINT_SIZE, &current_point_size));
   GL_CHECKD(glGetFloatv(GL_LINE_WIDTH, &current_line_width));
 
-  for (const auto& polyset : vertex_states_) {
-    if (polyset) polyset->draw();
+  if (shaderinfo && shaderinfo->progid) {
+    GL_TRACE("glUseProgram(%d)", shaderinfo->progid);
+    GL_CHECKD(glUseProgram(shaderinfo->progid));
+    if (shaderinfo->type == RendererUtils::ShaderType::EDGE_RENDERING && showedges) {
+      VBOUtils::shader_attribs_enable(*shaderinfo);
+    }
+  }
+  for (const auto& vs : vertex_states_) {
+    if (vs) vs->draw();
+  }
+  if (shaderinfo && shaderinfo->progid) {
+    if (shaderinfo->type == RendererUtils::ShaderType::EDGE_RENDERING && showedges) {
+      VBOUtils::shader_attribs_disable(*shaderinfo);
+    }
   }
 
   // restore states
@@ -232,7 +262,7 @@ void PolySetRenderer::draw(bool showfaces, bool showedges, const RendererUtils::
   GL_TRACE("glLineWidth(%d)", current_line_width);
   GL_CHECKD(glLineWidth(current_line_width));
 
-  if (!origVertexArrayState) glDisableClientState(GL_VERTEX_ARRAY);
+  if (!origVBOBuilderState) glDisableClientState(GL_VERTEX_ARRAY);
   if (!origNormalArrayState) glDisableClientState(GL_NORMAL_ARRAY);
   if (!origColorArrayState) glDisableClientState(GL_COLOR_ARRAY);
  
