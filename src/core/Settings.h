f8fd4a7968a326060df32da4145f6721150e5e94
Marius Kintel <marius@kintel.net>
2025-01-14 23:00:59 -0500

    Add constructors for SettingsEntryEnum::Item to allow existing initializers to work correctly

--- src/core/Settings.h
+++ src/core/Settings.h
@@ -179,6 +179,8 @@ class SettingsEntryEnum : public SettingsEntry<enum_type>
 {
 public:
   struct Item {
+    Item(enum_type value, std::string description) : value(value), description(std::move(description)) {}  
+    Item(enum_type value, std::string name, std::string description) : value(value), name(std::move(name)), description(std::move(description)) {}  
     enum_type value;
     std::string name;
     std::string description;
