b31d42e50eb626895c5e21a5a72559b4def6cf94
Marius Kintel <marius@kintel.net>
2025-02-04 21:55:53 -0500

    cosmetics

--- src/glview/fbo.cc
+++ src/glview/fbo.cc
@@ -1,9 +1,9 @@
-#include "fbo.h"
+#include "glview/fbo.h"
 
-#include "system-gl.h"
-
-#include <iostream>
 #include <memory>
+
+#include "glview/system-gl.h"
+#include "utils/printutils.h"
 
 namespace {
 
@@ -43,10 +43,8 @@ bool checkFBOStatus() {
     break;
   }
 
-  std::cerr << "glCheckFramebufferStatus(): ";
-  if (statusString) std::cerr << statusString;
-  else std::cerr << "Unknown status " << status;
-  std::cerr << std::endl;
+  LOG(message_group::Error, "glCheckFramebufferStatus(): %1$s",
+      statusString ? statusString : "Unknown status " + std::to_string(status));
   return false;
 }
 
@@ -58,101 +56,101 @@ std::unique_ptr<FBO> createFBO(int width, int height) {
   } else if (hasGLExtension(EXT_framebuffer_object)) {
     return std::make_unique<FBO>(width, height, /*useEXT*/ true);
   } else {
-    // TODO: LOG
-    std::cerr << "Framebuffer Objects not supported" << std::endl;
+    LOG(message_group::Error, "Framebuffer Objects not supported");
     return nullptr;
   }
 }
 
-FBO::FBO(int width, int height, bool useEXT) : useEXT(useEXT) {
+FBO::FBO(int width, int height, bool useEXT) : use_ext_(useEXT) {
   // Generate and bind FBO
-  GL_CHECKD(glGenFramebuffers(1, &this->fbo_id));
+  GL_CHECKD(glGenFramebuffers(1, &this->fbo_id_));
   this->bind();
 
   // Generate depth and render buffers
-  GL_CHECKD(glGenRenderbuffers(1, &this->depthbuf_id));
-  GL_CHECKD(glGenRenderbuffers(1, &this->renderbuf_id));
+  GL_CHECKD(glGenRenderbuffers(1, &this->depthbuf_id_));
+  GL_CHECKD(glGenRenderbuffers(1, &this->renderbuf_id_));
 
   // Create buffers with correct size
   if (!this->resize(width, height)) return;
 
   // Attach render and depth buffers
   GL_CHECKD(glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
-				  GL_RENDERBUFFER, this->renderbuf_id));
+				  GL_RENDERBUFFER, this->renderbuf_id_));
 
   if (!checkFBOStatus()) {
-    std::cerr << "Problem with OpenGL framebuffer after specifying color render buffer.\n";
+    LOG(message_group::Error, "Problem with OpenGL framebuffer after specifying color render buffer.");
     return;
   }
 
-  //glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT,
   // to prevent Mesa's software renderer from crashing, do this in two stages.
   // ie. instead of using GL_DEPTH_STENCIL_ATTACHMENT, do DEPTH then STENCIL.
   GL_CHECKD(glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT,
-				     GL_RENDERBUFFER, this->depthbuf_id));
+				     GL_RENDERBUFFER, this->depthbuf_id_));
   GL_CHECKD(glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_STENCIL_ATTACHMENT,
-				     GL_RENDERBUFFER, this->depthbuf_id));
+				     GL_RENDERBUFFER, this->depthbuf_id_));
 
   if (!checkFBOStatus()) {
-    std::cerr << "Problem with OpenGL framebuffer after specifying depth render buffer.\n";
+    LOG(message_group::Error, "Problem with OpenGL framebuffer after specifying depth render buffer.");
     return;
   }
 
-  this->complete = true;
+  this->complete_ = true;
 }
 
 bool FBO::resize(size_t width, size_t height)
 {
-  if (this->useEXT) {
-    GL_CHECKD(glBindRenderbufferEXT(GL_RENDERBUFFER, this->renderbuf_id));
+  if (this->use_ext_) {
+    GL_CHECKD(glBindRenderbufferEXT(GL_RENDERBUFFER, this->renderbuf_id_));
   } else {
-    GL_CHECKD(glBindRenderbuffer(GL_RENDERBUFFER, this->renderbuf_id));
+    GL_CHECKD(glBindRenderbuffer(GL_RENDERBUFFER, this->renderbuf_id_));
   }
   GL_CHECKD(glRenderbufferStorage(GL_RENDERBUFFER, GL_RGBA8, width, height));
-  if (this->useEXT) {
-    GL_CHECKD(glBindRenderbufferEXT(GL_RENDERBUFFER, this->depthbuf_id));
+  if (this->use_ext_) {
+    GL_CHECKD(glBindRenderbufferEXT(GL_RENDERBUFFER, this->depthbuf_id_));
   } else {
-    GL_CHECKD(glBindRenderbuffer(GL_RENDERBUFFER, this->depthbuf_id));
+    GL_CHECKD(glBindRenderbuffer(GL_RENDERBUFFER, this->depthbuf_id_));
   }
   GL_CHECKD(glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, width, height));
 
   return true;
 }
 
+// Bind this VBO. Returs the old FBO id.
 GLuint FBO::bind()
 {
-  glGetIntegerv(GL_FRAMEBUFFER_BINDING, reinterpret_cast<GLint *>(&this->old_fbo_id));
-  if (this->useEXT) {
-    GL_CHECKD(glBindFramebufferEXT(GL_FRAMEBUFFER, this->fbo_id));
+  glGetIntegerv(GL_FRAMEBUFFER_BINDING, reinterpret_cast<GLint *>(&this->old_fbo_id_));
+  if (this->use_ext_) {
+    GL_CHECKD(glBindFramebufferEXT(GL_FRAMEBUFFER, this->fbo_id_));
   } else {
-    GL_CHECKD(glBindFramebuffer(GL_FRAMEBUFFER, this->fbo_id));
+    GL_CHECKD(glBindFramebuffer(GL_FRAMEBUFFER, this->fbo_id_));
   }
-  return this->old_fbo_id;
+  return this->old_fbo_id_;
 }
 
+// Unbind this VBO, and bind the previous FBO id.
 void FBO::unbind()
 {
-  if (this->useEXT) {
-    GL_CHECKD(glBindFramebufferEXT(GL_FRAMEBUFFER, this->old_fbo_id));
+  if (this->use_ext_) {
+    GL_CHECKD(glBindFramebufferEXT(GL_FRAMEBUFFER, this->old_fbo_id_));
   } else {
-    GL_CHECKD(glBindFramebuffer(GL_FRAMEBUFFER, this->old_fbo_id));
+    GL_CHECKD(glBindFramebuffer(GL_FRAMEBUFFER, this->old_fbo_id_));
   }
-  this->old_fbo_id = 0;
+  this->old_fbo_id_ = 0;
 }
 
 void FBO::destroy()
 {
   this->unbind();
-  if (this->depthbuf_id != 0) {
-    GL_CHECKD(glDeleteRenderbuffers(1, &this->depthbuf_id));
-    this->depthbuf_id = 0;
+  if (this->depthbuf_id_ != 0) {
+    GL_CHECKD(glDeleteRenderbuffers(1, &this->depthbuf_id_));
+    this->depthbuf_id_ = 0;
   }
-  if (this->renderbuf_id != 0) {
-    GL_CHECKD(glDeleteRenderbuffers(1, &this->renderbuf_id));
-    this->renderbuf_id = 0;
+  if (this->renderbuf_id_ != 0) {
+    GL_CHECKD(glDeleteRenderbuffers(1, &this->renderbuf_id_));
+    this->renderbuf_id_ = 0;
   }
-  if (this->fbo_id != 0) {
-    GL_CHECKD(glDeleteFramebuffers(1, &this->fbo_id));
-    this->fbo_id = 0;
+  if (this->fbo_id_ != 0) {
+    GL_CHECKD(glDeleteFramebuffers(1, &this->fbo_id_));
+    this->fbo_id_ = 0;
   }
 }
--- src/glview/fbo.h
+++ src/glview/fbo.h
@@ -3,26 +3,27 @@
 #include <cstddef>
 #include <memory>
 
-#include "system-gl.h"
-#include "OpenGLContext.h"
+#include "glview/system-gl.h"
 
 class FBO
 {
-  bool useEXT;
-  GLuint fbo_id = 0;
-  GLuint old_fbo_id = 0;
-  GLuint renderbuf_id = 0;
-  GLuint depthbuf_id = 0;
-  bool complete = false;
-
 public:
   FBO(int width, int height, bool useEXT);
   ~FBO() { destroy(); };
-  bool isComplete() { return this->complete; }
+  bool isComplete() { return this->complete_; }
   bool resize(size_t width, size_t height);
   GLuint bind();
   void unbind();
+
+private:
   void destroy();
+
+  bool use_ext_;
+  GLuint fbo_id_ = 0;
+  GLuint old_fbo_id_ = 0;
+  GLuint renderbuf_id_ = 0;
+  GLuint depthbuf_id_ = 0;
+  bool complete_ = false;
 };
 
 std::unique_ptr<FBO> createFBO(int width, int height);
