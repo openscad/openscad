9fadb8b63282dbc6e21ab5a8ca797c758a85f255
Chris Mayo <aklhfex@gmail.com>
2025-02-05 01:40:29 +0000

    Fix Console output on save error (#5637)
    "File/Save As..." to a read-only directory can cause invalid characters
    to be written to the Console.

--- src/gui/TabManager.cc
+++ src/gui/TabManager.cc
@@ -594,8 +594,8 @@ bool TabManager::shouldClose()
 
 void TabManager::saveError(const QIODevice& file, const std::string& msg, const QString& filepath)
 {
-  const char *fileName = filepath.toLocal8Bit().constData();
-  LOG("%1$s %2$s (%3$s)", msg.c_str(), fileName, file.errorString().toLocal8Bit().constData());
+  const std::string fileName = filepath.toStdString();
+  LOG("%1$s %2$s (%3$s)", msg.c_str(), fileName, file.errorString().toStdString());
 
   const std::string dialogFormatStr = msg + "\n\"%1\"\n(%2)";
   const QString dialogFormat(dialogFormatStr.c_str());
