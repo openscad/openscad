5623229d709f9a88aa2a1cd9310fa28b4c2fc860
Sohler Günther <guenther.sohler@gmail.com>
2025-02-23 02:58:57 +0100

    Add union, difference, intersection and translate (#5691)
    ---------
    Co-authored-by: Guenther Sohler <guenther.sohler@photeon.com>

--- src/python/pyfunctions.cc
+++ src/python/pyfunctions.cc
@@ -26,6 +26,9 @@
 
 #include <Python.h>
 #include "src/python/pyopenscad.h"
+#include "src/core/CsgOpNode.h"
+#include "src/core/TransformNode.h"
+
 #include "src/core/primitives.h"
 
 PyObject *python_cube(PyObject *self, PyObject *args, PyObject *kwargs)
@@ -66,10 +69,39 @@ PyObject *python_cube(PyObject *self, PyObject *args, PyObject *kwargs)
   }
   return PyOpenSCADObjectFromNode(&PyOpenSCADType, node);
 }
+PyObject *python_nb_sub_vec3(PyObject *arg1, PyObject *arg2, int mode);
+PyObject *python_translate_core(PyObject *obj, PyObject *v) 
+{
+  if(v == nullptr) return obj;
+  return  python_nb_sub_vec3(obj, v, 0);
+}	
+
+PyObject *python_translate(PyObject *self, PyObject *args, PyObject *kwargs)
+{
+  char *kwlist[] = {"obj", "v", NULL};
+  PyObject *obj = NULL;
+  PyObject *v = NULL;
+  if (!PyArg_ParseTupleAndKeywords(args, kwargs, "O|O", kwlist, &obj, &v)) {
+    PyErr_SetString(PyExc_TypeError, "Error during parsing translate(object,vec3)");
+    return NULL;
+  }
+  return python_translate_core(obj,v);
+}
+
+PyObject *python_oo_translate(PyObject *obj, PyObject *args, PyObject *kwargs)
+{
+  char *kwlist[] = {"v", NULL};
+  PyObject *v = NULL;
+  if (!PyArg_ParseTupleAndKeywords(args, kwargs, "|O", kwlist, &v)) {
+    PyErr_SetString(PyExc_TypeError, "Error during parsing translate(object,vec3)");
+    return NULL;
+  }
+  return python_translate_core(obj,v);
+}
 
 PyObject *python_show_core(PyObject *obj)
 {
-  std::shared_ptr<AbstractNode> child = PyOpenSCADObjectToNode(obj);
+  std::shared_ptr<AbstractNode> child = PyOpenSCADObjectToNodeMulti(obj);
   if (child == NULL) {
     PyErr_SetString(PyExc_TypeError, "Invalid type for Object in output");
     return NULL;
@@ -93,9 +125,280 @@ PyObject *python_show(PyObject *self, PyObject *args, PyObject *kwargs)
   return python_show_core(obj);
 }
 
+PyObject *python_oo_show(PyObject *obj, PyObject *args, PyObject *kwargs)
+{
+  char *kwlist[] = {NULL};
+  if (!PyArg_ParseTupleAndKeywords(args, kwargs, "", kwlist
+                                   ))  {
+    PyErr_SetString(PyExc_TypeError, "Error during parsing output(object)");
+    return NULL;
+  }
+  return python_show_core(obj);
+}
+
+PyObject *python_csg_sub(PyObject *self, PyObject *args, PyObject *kwargs, OpenSCADOperator mode)
+{
+  DECLARE_INSTANCE
+  int i;
+
+  auto node = std::make_shared<CsgOpNode>(instance, mode);
+  PyObject *obj;
+  std::shared_ptr<AbstractNode> child;
+  for (i = 0; i < PyTuple_Size(args);i++) {
+    obj = PyTuple_GetItem(args, i);
+    if(i == 0) child = PyOpenSCADObjectToNodeMulti(obj);
+    else child = PyOpenSCADObjectToNodeMulti(obj);
+    if(child != NULL) {
+      node->children.push_back(child);
+    } else {
+      switch(mode) {
+        case OpenSCADOperator::UNION:	    
+          PyErr_SetString(PyExc_TypeError, "Error during parsing union. arguments must be solids or arrays.");
+	  return nullptr;
+  	break;
+        case OpenSCADOperator::DIFFERENCE:	    
+          PyErr_SetString(PyExc_TypeError, "Error during parsing difference. arguments must be solids or arrays.");
+	  return nullptr;
+  	break;
+        case OpenSCADOperator::INTERSECTION:	    
+          PyErr_SetString(PyExc_TypeError, "Error during parsing intersection. arguments must be solids or arrays.");
+	  return nullptr;
+	  break;
+        case OpenSCADOperator::MINKOWSKI:	    
+      	  break;
+        case OpenSCADOperator::HULL:	    
+      	  break;
+        case OpenSCADOperator::FILL:	    
+      	  break;
+        case OpenSCADOperator::RESIZE:	    
+      	  break;
+      }
+      return NULL;
+    }
+  }
+
+  PyObject *pyresult =PyOpenSCADObjectFromNode(&PyOpenSCADType, node);
+  return pyresult;
+}
+
+PyObject *python_union(PyObject *self, PyObject *args, PyObject *kwargs)
+{
+  return python_csg_sub(self, args, kwargs, OpenSCADOperator::UNION);
+}
+
+PyObject *python_difference(PyObject *self, PyObject *args, PyObject *kwargs)
+{
+  return python_csg_sub(self, args, kwargs, OpenSCADOperator::DIFFERENCE);
+}
+
+PyObject *python_intersection(PyObject *self, PyObject *args, PyObject *kwargs)
+{
+  return python_csg_sub(self, args, kwargs, OpenSCADOperator::INTERSECTION);
+}
+
+
+PyObject *python_oo_csg_sub(PyObject *self, PyObject *args, PyObject *kwargs, OpenSCADOperator mode)
+{
+  DECLARE_INSTANCE
+  int i;
+
+  auto node = std::make_shared<CsgOpNode>(instance, mode);
+
+
+  PyObject *obj;
+  std::shared_ptr<AbstractNode> child;
+
+  child = PyOpenSCADObjectToNodeMulti(self);
+  if(child != NULL) node->children.push_back(child);
+  
+  for (i = 0; i < PyTuple_Size(args);i++) {
+    obj = PyTuple_GetItem(args, i);
+    if(i == 0) child = PyOpenSCADObjectToNodeMulti(obj);
+    else child = PyOpenSCADObjectToNodeMulti(obj);
+    if(child != NULL) {
+      node->children.push_back(child);
+    } else {
+      switch(mode) {
+        case OpenSCADOperator::UNION:	    
+          PyErr_SetString(PyExc_TypeError, "Error during parsing union. arguments must be solids or arrays.");
+  	break;
+        case OpenSCADOperator::DIFFERENCE:	    
+          PyErr_SetString(PyExc_TypeError, "Error during parsing difference. arguments must be solids or arrays.");
+  	break;
+        case OpenSCADOperator::INTERSECTION:	    
+          PyErr_SetString(PyExc_TypeError, "Error during parsing intersection. arguments must be solids or arrays.");
+	  break;
+        case OpenSCADOperator::MINKOWSKI:	    
+      	  break;
+        case OpenSCADOperator::HULL:	    
+      	  break;
+        case OpenSCADOperator::FILL:	    
+      	  break;
+        case OpenSCADOperator::RESIZE:	    
+      	  break;
+      }
+      return NULL;
+    }
+  }
+
+  PyObject *pyresult =PyOpenSCADObjectFromNode(&PyOpenSCADType, node);
+  return pyresult;
+}
+
+PyObject *python_oo_union(PyObject *self, PyObject *args, PyObject *kwargs)
+{
+  return python_oo_csg_sub(self, args, kwargs, OpenSCADOperator::UNION);
+}
+
+PyObject *python_oo_difference(PyObject *self, PyObject *args, PyObject *kwargs)
+{
+  return python_oo_csg_sub(self, args, kwargs, OpenSCADOperator::DIFFERENCE);
+}
+
+PyObject *python_oo_intersection(PyObject *self, PyObject *args, PyObject *kwargs)
+{
+  return python_oo_csg_sub(self, args, kwargs, OpenSCADOperator::INTERSECTION);
+}
+
+PyObject *python_nb_sub(PyObject *arg1, PyObject *arg2, OpenSCADOperator mode)
+{
+  DECLARE_INSTANCE
+  std::shared_ptr<AbstractNode> child[2];
+
+  if(arg1 == Py_None && mode == OpenSCADOperator::UNION) return arg2;
+  if(arg2 == Py_None && mode == OpenSCADOperator::UNION) return arg1;
+  if(arg2 == Py_None && mode == OpenSCADOperator::DIFFERENCE) return arg1;
+
+
+  child[0] = PyOpenSCADObjectToNodeMulti(arg1);
+  if (child[0] == NULL) {
+    PyErr_SetString(PyExc_TypeError, "invalid argument left to operator");
+    return NULL;
+  }
+  child[1] = PyOpenSCADObjectToNodeMulti(arg2);
+  if (child[1] == NULL) {
+    PyErr_SetString(PyExc_TypeError, "invalid argument right to operator");
+    return NULL;
+  }
+  auto node = std::make_shared<CsgOpNode>(instance, mode);
+  node->children.push_back(child[0]);
+  node->children.push_back(child[1]);
+  PyObject *pyresult = PyOpenSCADObjectFromNode(&PyOpenSCADType, node);
+  return pyresult;
+}
+
+PyObject *python_nb_sub_vec3(PyObject *arg1, PyObject *arg2, int mode) // 0: translate, 1: scale, 2: translateneg, 3=translate-exp
+{
+  DECLARE_INSTANCE
+  std::shared_ptr<AbstractNode> child;
+
+  child = PyOpenSCADObjectToNodeMulti(arg1);
+  std::vector<Vector3d> vecs;
+		
+  vecs = python_vectors(arg2,2,3);
+
+    if (vecs.size() > 0) {
+    if (child == NULL) {
+      PyErr_SetString(PyExc_TypeError, "invalid argument left to operator");
+      return NULL;
+    }
+    std::vector<std::shared_ptr<TransformNode>> nodes;
+    for(size_t j=0;j<vecs.size();j++) {
+      auto node = std::make_shared<TransformNode>(instance, "transform");
+      if(mode == 0 || mode == 3) node->matrix.translate(vecs[j]);
+      if(mode == 1) node->matrix.scale(vecs[j]);
+      if(mode == 2) node->matrix.translate(-vecs[j]);
+      node->children.push_back(child);
+      nodes.push_back(node);
+    }  
+    if(nodes.size() == 1) {
+      PyObject *pyresult = PyOpenSCADObjectFromNode(&PyOpenSCADType, nodes[0]);
+      return pyresult;
+    }
+  }
+  PyErr_SetString(PyExc_TypeError, "invalid argument right to operator");
+  return NULL;
+}
+
+PyObject *python_nb_add(PyObject *arg1, PyObject *arg2) { return python_nb_sub_vec3(arg1, arg2, 0); }  // translate
+PyObject *python_nb_mul(PyObject *arg1, PyObject *arg2) { return python_nb_sub_vec3(arg1, arg2, 1); } // scale
+PyObject *python_nb_or(PyObject *arg1, PyObject *arg2) { return python_nb_sub(arg1, arg2,  OpenSCADOperator::UNION); }
+PyObject *python_nb_subtract(PyObject *arg1, PyObject *arg2)
+{
+  double dmy;	
+  if(PyList_Check(arg2) && PyList_Size(arg2) > 0) {
+    PyObject *sub = PyList_GetItem(arg2, 0);	  
+    if (!python_numberval(sub, &dmy) || PyList_Check(sub)){
+      return python_nb_sub_vec3(arg1, arg2, 2); 
+    }
+  }	  
+  return python_nb_sub(arg1, arg2,  OpenSCADOperator::DIFFERENCE); // if its solid
+}
+PyObject *python_nb_and(PyObject *arg1, PyObject *arg2) { return python_nb_sub(arg1, arg2,  OpenSCADOperator::INTERSECTION); }
+
 PyMethodDef PyOpenSCADFunctions[] = {
   {"cube", (PyCFunction) python_cube, METH_VARARGS | METH_KEYWORDS, "Create Cube."},
+  {"translate", (PyCFunction) python_translate, METH_VARARGS | METH_KEYWORDS, "Move  Object."},
   {"show", (PyCFunction) python_show, METH_VARARGS | METH_KEYWORDS, "Show the result."},
+  {"union", (PyCFunction) python_union, METH_VARARGS | METH_KEYWORDS, "Union Object."},
+  {"difference", (PyCFunction) python_difference, METH_VARARGS | METH_KEYWORDS, "Difference Object."},
+  {"intersection", (PyCFunction) python_intersection, METH_VARARGS | METH_KEYWORDS, "Intersection Object."},
   {NULL, NULL, 0, NULL}
+};
+
+#define	OO_METHOD_ENTRY(name,desc) \
+  {#name, (PyCFunction) python_oo_##name, METH_VARARGS | METH_KEYWORDS, desc},
+
+PyMethodDef PyOpenSCADMethods[] = {
+  OO_METHOD_ENTRY(translate,"Move Object")	
+  OO_METHOD_ENTRY(union,"Union Object")	
+  OO_METHOD_ENTRY(difference,"Difference Object")	
+  OO_METHOD_ENTRY(intersection,"Intersection Object")	
+  OO_METHOD_ENTRY(show,"Show Object")	
+  {NULL, NULL, 0, NULL}
+};
+
+PyNumberMethods PyOpenSCADNumbers =
+{
+     python_nb_add,	//binaryfunc nb_add
+     python_nb_subtract,//binaryfunc nb_subtract
+     python_nb_mul,	//binaryfunc nb_multiply
+     0,			//binaryfunc nb_remainder
+     0,			//binaryfunc nb_divmod
+     0,			//ternaryfunc nb_power
+     0,			//unaryfunc nb_negative
+     0,			//unaryfunc nb_positive
+     0,			//unaryfunc nb_absolute
+     0,			//inquiry nb_bool
+     0,			  //unaryfunc nb_invert
+     0,			//binaryfunc nb_lshift
+     0,			//binaryfunc nb_rshift
+     python_nb_and,	//binaryfunc nb_and 
+     0,			//binaryfunc nb_xor
+     python_nb_or,	//binaryfunc nb_or 
+     0,			//unaryfunc nb_int
+     0,			//void *nb_reserved
+     0,			//unaryfunc nb_float
+
+     0,			//binaryfunc nb_inplace_add
+     0,			//binaryfunc nb_inplace_subtract
+     0,			//binaryfunc nb_inplace_multiply
+     0,			//binaryfunc nb_inplace_remainder
+     0,			//ternaryfunc nb_inplace_power
+     0,			//binaryfunc nb_inplace_lshift
+     0,			//binaryfunc nb_inplace_rshift
+     0,			//binaryfunc nb_inplace_and
+     0,			//binaryfunc nb_inplace_xor
+     0,			//binaryfunc nb_inplace_or
+
+     0,			//binaryfunc nb_floor_divide
+     0,			//binaryfunc nb_true_divide
+     0,			//binaryfunc nb_inplace_floor_divide
+     0,			//binaryfunc nb_inplace_true_divide
+
+     0,			//unaryfunc nb_index
+
+     0,			//binaryfunc nb_matrix_multiply
+     0			//binaryfunc nb_inplace_matrix_multiply
 };
 
--- src/python/pyopenscad.h
+++ src/python/pyopenscad.h
@@ -20,6 +20,9 @@ typedef struct {
   /* Type-specific fields go here. */
 } PyOpenSCADObject;
 
+void PyObjectDeleter (PyObject *pObject);
+using PyObjectUniquePtr = std::unique_ptr<PyObject, const decltype(PyObjectDeleter)&>;
+
 PyMODINIT_FUNC PyInit_PyOpenSCAD(void);
 int python_vectorval(PyObject *vec, int minarg, int maxarg, double *x, double *y, double *z, double *w=NULL);
 PyObject *PyOpenSCADObjectFromNode(PyTypeObject *type, const std::shared_ptr<AbstractNode> &node);
@@ -30,6 +33,13 @@ extern PyTypeObject PyOpenSCADType;
 extern std::shared_ptr<AbstractNode> python_result_node;
 extern std::string trusted_edit_document_name;
 extern std::string untrusted_edit_document_name;
+std::vector<Vector3d> python_vectors(PyObject *vec, int mindim, int maxdim);
+int python_numberval(PyObject *number, double *result);
+std::shared_ptr<AbstractNode> PyOpenSCADObjectToNode(PyObject *object);
+std::shared_ptr<AbstractNode> PyOpenSCADObjectToNodeMulti(PyObject *object);
+extern PyNumberMethods PyOpenSCADNumbers;
 extern PyMethodDef PyOpenSCADFunctions[];
-extern PyObject *pythonInitDict;
-extern PyObject *pythonMainModule;
+extern PyMethodDef PyOpenSCADMethods[];
+
+extern PyObjectUniquePtr pythonInitDict;
+extern PyObjectUniquePtr pythonMainModule;
