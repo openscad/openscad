8fb299296fca90f717fc66e1b1ef2f4cfa1a856f
Guenther Sohler <guenther.sohler@photeon.com>
2025-04-01 21:47:05 +0200

    small bugfix

--- src/python/pyfunctions.cc
+++ src/python/pyfunctions.cc
@@ -3166,7 +3166,7 @@ PyObject *python_csg_sub(PyObject *self, PyObject *args, PyObject *kwargs, OpenS
   for (i = 0; i < PyTuple_Size(args);i++) {
     obj = PyTuple_GetItem(args, i);
     PyObject *dict = nullptr;	  
-    if(i == 0) child = PyOpenSCADObjectToNodeMulti(obj, &dict);
+    child = PyOpenSCADObjectToNodeMulti(obj, &dict);
     child_dict.push_back(dict);
     if(child != NULL) {
       node->children.push_back(child);
