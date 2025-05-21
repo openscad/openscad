d4f00ed79fea81c1fa878c4ca7a6a1dc9e57e5f5
Guenther Sohler <guenther.sohler@photeon.com>
2025-05-21 10:13:09 +0200

    Patch from nomike, thank you!

--- tests/mingw_convert_ctest.py
+++ tests/mingw_convert_ctest.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 #
 # Mingw_x_testfile - convert paths in CTestTestfile.cmake so they work 
--- tests/test_cmdline_tool.py
+++ tests/test_cmdline_tool.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+/usr/bin/env python3
 #
 # Regression test driver for cmd-line tools
 #
