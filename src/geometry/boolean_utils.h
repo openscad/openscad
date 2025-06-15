2a8774dfacea4ccb51a38abb7073450365323508
Marius Kintel <marius@kintel.net>
2025-06-14 23:18:29 -0400

    Use Manifold's hull in Manifold mode

--- src/geometry/boolean_utils.h
+++ src/geometry/boolean_utils.h
@@ -4,5 +4,5 @@
 #include "geometry/PolySet.h"
 #include "geometry/Geometry.h"
 
-std::unique_ptr<PolySet> applyHull(const Geometry::Geometries& children);
+std::unique_ptr<Geometry> applyHull(const Geometry::Geometries& children);
 std::shared_ptr<const Geometry> applyMinkowski(const Geometry::Geometries& children);
