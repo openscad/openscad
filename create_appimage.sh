2aba5b3ac414e7a27de13e4c33b9e56bda0fd90b
Guenther Sohler <guenther.sohler@photeon.com>
2025-06-19 23:34:07 +0200

    AppImage works sgain

--- create_appimage.sh
+++ create_appimage.sh
@@ -1,14 +1,13 @@
 #! /bin/sh
 
 cd build
-cmake  -DCMAKE_INSTALL_PREFIX=/usr -DEXPERIMENTAL=1 -DENABLE_PYTHON=1 -DPYTHON_VERSION=3.13 -DENABLE_LIBFIVE=1 ..
-rm -rf ../pythonscad.appdata.xml # TODO fix here
-cp ../pythonscad.appdata.xml.in2 ../pythonscad.appdata.xml # TODO fix here
+cmake  -DCMAKE_INSTALL_PREFIX=/usr -DEXPERIMENTAL=1 -DENABLE_PYTHON=1 -DPYTHON_VERSION=3.12 -DENABLE_LIBFIVE=1 ..
 make -j3
 if [ ! $? == 0 ] ; then exit 1 ; fi
 rm -rf ../AppDir
 make install DESTDIR=../AppDir
 cd ..
+
 export EXTRA_QT_PLUGINS=svg
 /home/gsohler/git/linuxdeploy-plugin-python/linuxdeploy-plugin-python.sh --appdir AppDir
 export PATH=/appimage/usr/local/bin:$PATH
