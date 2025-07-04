f52de82065f56d421e94374b2234781f2cabed7e
Guenther Sohler <guenther.sohler@photeon.com>
2025-07-04 15:48:01 +0200

    cmopare rendered fillet images

--- tests/data/pythonscad/fillet_csg.py
+++ tests/data/pythonscad/fillet_csg.py
@@ -5,6 +5,6 @@ c=cube(10);
 d = c+ [5,5,5]
 
 union(c,d,r=2,fn=10).show()
-#difference(d,c,r=2,fn=10).right(15).show()
+difference(c,d.front(10),r=2,fn=10).right(15).show()
 intersection(c,d,r=2,fn=10).right(30).show()
 
