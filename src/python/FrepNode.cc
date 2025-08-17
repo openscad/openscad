47cfb873375afd71cf6ab968ce2e2a4fafd6bba8
nomike <nomike@nomike.com>
2025-08-18 00:14:10 +0200

    Fix format specifiers in printf statements to use %lu for size_t types

--- src/python/FrepNode.cc
+++ src/python/FrepNode.cc
@@ -467,12 +467,12 @@ PyObject *ifrep(const std::shared_ptr<const PolySet>& ps)
 
   // TODO fill pointToFaceInd
   //  TODO tsten
-  printf("%d Faces, %d Points\n", ps->indices.size(), ps->vertices.size());
+  printf("%lu Faces, %lu Points\n", ps->indices.size(), ps->vertices.size());
 
   std::vector<CutFace> edgeFaces;
   std::vector<CutFace> normFaces;
   edgeFaces = calculateEdgeFaces(ps->vertices, ps->indices, pointToFaceInds, normFaces);
-  printf("%d EdgeFaces generated\n", edgeFaces.size());
+  printf("%lu EdgeFaces generated\n", edgeFaces.size());
 
   std::vector<int> table;                     // x(0) dimenstion faces y(1) dimenions edgefas
   for (int i = 0; i < edgeFaces.size(); i++)  // create table
