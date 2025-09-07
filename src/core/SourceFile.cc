73f144ab24c6722067493251a9413f5727ba97a5
coryrc <github@corycross.org>
2025-09-07 16:42:44 -0400

    SourceFileCache: Rename and reword away from compile/evaluate to parse/process (#6157)

--- src/core/SourceFile.cc
+++ src/core/SourceFile.cc
@@ -117,8 +117,8 @@ time_t SourceFile::include_modified(const std::string& filename) const
 }
 
 /*!
-   Check if any dependencies have been modified and recompile them.
-   Returns true if anything was recompiled.
+   Check if any dependencies have been modified and reparse them.
+   Returns true if anything was reparsed.
  */
 time_t SourceFile::handleDependencies(bool is_root)
 {
@@ -150,11 +150,11 @@ time_t SourceFile::handleDependencies(bool is_root)
     if (found) {
       auto oldmodule = SourceFileCache::instance()->lookup(filename);
       SourceFile *newmodule;
-      auto mtime = SourceFileCache::instance()->evaluate(this->getFullpath(), filename, newmodule);
+      auto mtime = SourceFileCache::instance()->process(this->getFullpath(), filename, newmodule);
       if (mtime > latest) latest = mtime;
       auto changed = newmodule && newmodule != oldmodule;
       // Detect appearance but not removal of files, and keep old module
-      // on compile errors (FIXME: Is this correct behavior?)
+      // on parse errors (FIXME: Is this correct behavior?)
       if (changed) {
         PRINTDB("  %s: %p -> %p", filename % oldmodule % newmodule);
       } else {
