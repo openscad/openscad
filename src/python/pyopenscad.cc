openscad/openscad

commit c3d173854efafdc95aa9ff962e5a69b69e7f1590

Python: Sphere, Cylinder, Color, Rotation
- Node cloning for safe tree handling in python
- Locking support for python interpreter
---------
Co-authored-by: Guenther Sohler <guenther.sohler@photeon.com>

src/python/pyopenscad.cc
@@ -77,9 +77,23 @@ PyObject *PyOpenSCADObjectFromNode(PyTypeObject *type, const std::shared_ptr<Abs
   }
   return nullptr;
 }
+
+PyThreadState *tstate=nullptr;
+
+void python_lock(void){
+  if(tstate != nullptr && pythonInitDict != nullptr) PyEval_RestoreThread(tstate);
+}
+
+void python_unlock(void) {
+  if(pythonInitDict != nullptr)	tstate = PyEval_SaveThread();
+}
+
 std::shared_ptr<AbstractNode> PyOpenSCADObjectToNode(PyObject *obj)
 {
   std::shared_ptr<AbstractNode> result = ((PyOpenSCADObject *) obj)->node;
+  if(result.use_count() > 2) {
+    result = result->clone();
+  }
   return result;
 }
 
@@ -99,6 +113,9 @@ std::shared_ptr<AbstractNode> PyOpenSCADObjectToNodeMulti(PyObject *objs)
   std::shared_ptr<AbstractNode> result;
   if (Py_TYPE(objs) == &PyOpenSCADType) {
     result = ((PyOpenSCADObject *) objs)->node;
+    if(result.use_count() > 2) {
+	    result = result->clone();
+    }
   } else if (PyList_Check(objs)) {
     DECLARE_INSTANCE
     auto node = std::make_shared<CsgOpNode>(instance, OpenSCADOperator::UNION);
@@ -213,6 +230,39 @@ std::vector<Vector3d> python_vectors(PyObject *vec, int mindim, int maxdim)
 
 /*
  * Helper function to extract actual values for fn, fa and fs
+ */
+
+void get_fnas(double& fn, double& fa, double& fs) {
+  PyObject *mainModule = PyImport_AddModule("__main__");
+  if (mainModule == nullptr) return;
+  fn=0;
+  fa=12;
+  fs=2;
+
+  if(PyObject_HasAttrString(mainModule,"fn")) {
+    PyObjectUniquePtr varFn(PyObject_GetAttrString(mainModule, "fn"),PyObjectDeleter);
+    if (varFn.get() != nullptr){
+      fn = PyFloat_AsDouble(varFn.get());
+    }
+  }  
+
+  if(PyObject_HasAttrString(mainModule,"fa")) {
+    PyObjectUniquePtr varFa(PyObject_GetAttrString(mainModule, "fa"),PyObjectDeleter);
+    if (varFa.get() != nullptr){
+      fa = PyFloat_AsDouble(varFa.get());
+    }
+  }
+
+  PyObjectUniquePtr varFs(PyObject_GetAttrString(mainModule, "fs"),PyObjectDeleter);
+  if(PyObject_HasAttrString(mainModule,"fn")) {
+    if (varFs.get() != nullptr){
+      fs = PyFloat_AsDouble(varFs.get());
+    }
+  }  
+}
+
+/*
+ * Type specific init function. nothing special here
  */
 
 static int PyOpenSCADInit(PyOpenSCADObject *self, PyObject *args, PyObject *kwds)

CMakeLists.txt
@@ -791,6 +791,7 @@ set(CORE_SOURCES
   src/core/ImportNode.cc
   src/core/LinearExtrudeNode.cc
   src/core/LocalScope.cc
+  src/core/node_clone.cc
   src/core/ModuleInstantiation.cc
   src/core/NodeDumper.cc
   src/core/NodeVisitor.cc

src/core/node.h
@@ -65,6 +65,7 @@ public:
   int idx; // Node index (unique per tree)
 
   std::shared_ptr<const AbstractNode> getNodeByID(int idx, std::deque<std::shared_ptr<const AbstractNode>>& path) const;
+  std::shared_ptr<AbstractNode> clone(void);
 };
 
 class AbstractIntersectionNode : public AbstractNode

src/core/node_clone.cc
@@ -1,0 +1,112 @@
+/*
+ *  OpenSCAD (www.openscad.org)
+ *  Copyright (C) 2009-2011 Clifford Wolf <clifford@clifford.at> and
+ *                          Marius Kintel <marius@kintel.net>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  As a special exception, you have permission to link this program
+ *  with the CGAL library and distribute executables, as long as you
+ *  follow the requirements of the GNU GPL in regard to all of the
+ *  software in the executable aside from CGAL.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#include "src/geometry/linalg.h"
+#include "src/geometry/GeometryUtils.h"
+#include "src/core/primitives.h"
+#include "src/core/TransformNode.h"
+#include "src/core/RotateExtrudeNode.h"
+#include "src/core/LinearExtrudeNode.h"
+#include "src/core/CgalAdvNode.h"
+#include "src/core/CsgOpNode.h"
+#include "src/core/ColorNode.h"
+#include "src/core/RoofNode.h"
+#include "src/core/RenderNode.h"
+#include "src/core/SurfaceNode.h"
+#include "src/core/TextNode.h"
+#include "src/core/OffsetNode.h"
+#include "src/core/ProjectionNode.h"
+#include "src/core/ImportNode.h"
+
+std::vector<ModuleInstantiation *> modinsts_list;
+
+#define NodeCloneFunc(T) std::shared_ptr<T> clone_what(const T *node) {\
+       	ModuleInstantiation *inst = new ModuleInstantiation(node->modinst->name() ,\
+	node->modinst->arguments, node->modinst->location());\
+	modinsts_list.push_back(inst); \
+       	auto clone = std::make_shared<T>(*node);\
+       	clone->modinst = inst; \
+	return clone;\
+}
+
+#define NodeCloneUse(T) { const T *node = dynamic_cast<const T *>(this); if((node) != nullptr) clone=clone_what(node); }
+NodeCloneFunc(CubeNode)
+NodeCloneFunc(SphereNode)
+NodeCloneFunc(CylinderNode)
+NodeCloneFunc(PolyhedronNode)
+NodeCloneFunc(SquareNode)
+NodeCloneFunc(CircleNode)
+NodeCloneFunc(PolygonNode)
+NodeCloneFunc(TransformNode)
+NodeCloneFunc(ColorNode)
+NodeCloneFunc(RotateExtrudeNode)
+NodeCloneFunc(LinearExtrudeNode)
+NodeCloneFunc(CsgOpNode)
+NodeCloneFunc(CgalAdvNode)
+NodeCloneFunc(RoofNode)
+NodeCloneFunc(RenderNode)
+NodeCloneFunc(SurfaceNode)
+NodeCloneFunc(TextNode)
+NodeCloneFunc(OffsetNode)
+NodeCloneFunc(ProjectionNode)
+NodeCloneFunc(GroupNode)
+NodeCloneFunc(ImportNode)
+
+std::shared_ptr<AbstractNode> AbstractNode::clone(void)
+{
+	std::shared_ptr<AbstractNode> clone=nullptr;
+	NodeCloneUse(CubeNode)
+	NodeCloneUse(SphereNode)
+	NodeCloneUse(CylinderNode)
+	NodeCloneUse(PolyhedronNode)
+	NodeCloneUse(SquareNode)
+	NodeCloneUse(CircleNode)
+	NodeCloneUse(PolygonNode)
+	NodeCloneUse(TransformNode)
+	NodeCloneUse(ColorNode)
+	NodeCloneUse(RotateExtrudeNode)
+	NodeCloneUse(LinearExtrudeNode)
+	NodeCloneUse(CsgOpNode)
+	NodeCloneUse(CgalAdvNode)
+	NodeCloneUse(RoofNode)
+	NodeCloneUse(RenderNode)
+	NodeCloneUse(SurfaceNode)
+	NodeCloneUse(TextNode)
+	NodeCloneUse(OffsetNode)
+	NodeCloneUse(ProjectionNode)
+	NodeCloneUse(GroupNode)
+	NodeCloneUse(ImportNode)
+	if(clone != nullptr) {
+		clone->idx = idx_counter++;
+		clone->children.clear();
+		for(const auto &child: this->children) {
+			clone->children.push_back(child->clone());
+		}
+		return clone;
+	}
+	std::cout << "Type not defined for clone :" << typeid(this).name() << "\n\r";
+	return std::shared_ptr<AbstractNode>(this);
+}

src/gui/CGALWorker.cc
@@ -13,6 +13,10 @@
 #include "utils/printutils.h"
 #include "utils/exceptions.h"
 
+#ifdef ENABLE_PYTHON
+#include "python/python_public.h"
+#endif
+
 CGALWorker::CGALWorker()
 {
   this->tree = nullptr;
@@ -29,6 +33,9 @@ CGALWorker::~CGALWorker()
 
 void CGALWorker::start(const Tree& tree)
 {
+#ifdef ENABLE_PYTHON
+  python_unlock();
+#endif
   this->tree = &tree;
   this->thread->start();
 }
@@ -36,6 +43,9 @@ void CGALWorker::start(const Tree& tree)
 void CGALWorker::work()
 {
   // this is a worker thread: we don't want any exceptions escaping and crashing the app.
+#ifdef ENABLE_PYTHON
+  python_lock();
+#endif
   std::shared_ptr<const Geometry> root_geom;
   try {
     GeometryEvaluator evaluator(*this->tree);
@@ -59,7 +69,9 @@ void CGALWorker::work()
   } catch (...) {
     LOG(message_group::Error, "Rendering cancelled by unknown exception.");
   }
-
+ #ifdef ENABLE_PYTHON
+  python_unlock();
+ #endif
   emit done(root_geom);
   thread->quit();
 }

src/gui/MainWindow.cc
@@ -1505,7 +1505,11 @@ void MainWindow::saveBackup()
   }
 
   if (!this->tempFile) {
-    this->tempFile = new QTemporaryFile(backupPath.append(basename + "-backup-XXXXXXXX.scad"));
+    QString suffix="scad";
+#ifdef ENABLE_PYTHON
+    if(this->python_active) suffix="py";
+#endif
+    this->tempFile = new QTemporaryFile(backupPath.append(basename + "-backup-XXXXXXXX." + suffix));
   }
 
   if ((!this->tempFile->isOpen()) && (!this->tempFile->open())) {
@@ -1823,6 +1827,10 @@ bool MainWindow::trust_python_file(const std::string& file,  const std::string& 
 
   if (content.size() <= 1) { // 1st character already typed
     this->trusted_edit_document_name = file;
+    return true;
+  }
+  if(content.rfind("from openscad import",0) == 0) { // 1st character already typed
+    this->trusted_edit_document_name=file;
     return true;
   }
 
@@ -2155,6 +2163,9 @@ void MainWindow::cgalRender()
 
 void MainWindow::actionRenderDone(const std::shared_ptr<const Geometry>& root_geom)
 {
+#ifdef ENABLE_PYTHON
+  python_lock();	
+#endif
   progress_report_fin();
   if (root_geom) {
     std::vector<std::string> options;

src/gui/TabManager.cc
@@ -470,6 +470,11 @@ void TabManager::setTabModified(EditorInterface *edt)
 void TabManager::openTabFile(const QString& filename)
 {
   par->setCurrentOutput();
+#ifdef ENABLE_PYTHON
+  if(boost::algorithm::ends_with(filename, ".py")) {
+    std::string templ="from openscad import *\n";	  
+  } else
+#endif  
   editor->setPlainText("");
 
   QFileInfo fileinfo(filename);

src/python/pyfunctions.cc
@@ -27,9 +27,14 @@
 #include <Python.h>
 #include "src/python/pyopenscad.h"
 #include "src/core/CsgOpNode.h"
+#include "src/core/ColorNode.h"
+#include "src/core/ColorUtil.h"
 #include "src/core/TransformNode.h"
+#include "src/core/RotateExtrudeNode.h"
+#include "src/utils/degree_trig.h"
 
 #include "src/core/primitives.h"
+extern std::unordered_map<std::string, Color4f> webcolors;
 
 PyObject *python_cube(PyObject *self, PyObject *args, PyObject *kwargs)
 {
@@ -69,6 +74,229 @@ PyObject *python_cube(PyObject *self, PyObject *args, PyObject *kwargs)
   }
   return PyOpenSCADObjectFromNode(&PyOpenSCADType, node);
 }
+
+PyObject *python_sphere(PyObject *self, PyObject *args, PyObject *kwargs)
+{
+  DECLARE_INSTANCE
+  auto node = std::make_shared<SphereNode>(instance);
+
+  char *kwlist[] = {"r", "d", "fn", "fa", "fs", NULL};
+  double r = NAN;
+  PyObject *rp = nullptr;
+  double d = NAN;
+  double fn = NAN, fa = NAN, fs = NAN;
+
+  double vr = 1;
+
+  if (!PyArg_ParseTupleAndKeywords(args, kwargs, "|Odddd", kwlist,
+                                   &rp, &d, &fn, &fa, &fs
+                                   )) {
+    PyErr_SetString(PyExc_TypeError, "Error during parsing sphere(r|d)");
+    return NULL;
+  } 
+  if(rp != nullptr) {
+    python_numberval(rp, &r);
+  }
+  if (!isnan(r)) {
+    if(r <= 0) {
+      PyErr_SetString(PyExc_TypeError, "Parameter r must be positive");
+      return NULL;
+    }	    
+    vr = r;
+    if(!isnan(d)) {
+      PyErr_SetString(PyExc_TypeError, "Cant specify r and d at the same time for sphere");
+      return NULL;
+    }
+  } 
+  if (!isnan(d)) {
+    if(d <= 0) {
+      PyErr_SetString(PyExc_TypeError, "Parameter d must be positive");
+      return NULL;
+    }	    
+    vr = d / 2.0;
+  }
+
+  get_fnas(node->fn, node->fa, node->fs);
+  if (!isnan(fn)) node->fn = fn;
+  if (!isnan(fa)) node->fa = fa;
+  if (!isnan(fs)) node->fs = fs;
+
+  node->r = vr;
+
+  return PyOpenSCADObjectFromNode(&PyOpenSCADType, node);
+}
+
+PyObject *python_cylinder(PyObject *self, PyObject *args, PyObject *kwargs)
+{
+  DECLARE_INSTANCE
+  auto node = std::make_shared<CylinderNode>(instance);
+
+  char *kwlist[] = {"h", "r1", "r2", "center",  "r", "d", "d1", "d2",  "fn", "fa", "fs", NULL};
+  double h = NAN;
+  double r = NAN;
+  double r1 = NAN;
+  double r2 = NAN;
+  double d = NAN;
+  double d1 = NAN;
+  double d2 = NAN;
+
+  double fn = NAN, fa = NAN, fs = NAN;
+
+  PyObject *center = NULL;
+  double vr1 = 1, vr2 = 1, vh = 1;
+
+
+  if (!PyArg_ParseTupleAndKeywords(args, kwargs, "|dddOddddddd", kwlist, &h, &r1, &r2, &center, &r, &d, &d1, &d2, &fn, &fa, &fs)) {
+    PyErr_SetString(PyExc_TypeError, "Error during parsing cylinder(h,r|r1+r2|d1+d2)");
+    return NULL;
+  }
+
+  if(h <= 0) {
+    PyErr_SetString(PyExc_TypeError, "Cylinder height must be positive");
+    return NULL;
+  }
+  vh = h;
+
+  if(!isnan(d) && d <= 0) {
+    PyErr_SetString(PyExc_TypeError, "Cylinder d must be positive");
+    return NULL;
+  }
+  if(!isnan(r1) && r1 < 0) {
+    PyErr_SetString(PyExc_TypeError, "Cylinder r1 must not be negative");
+    return NULL;
+  }
+  if(!isnan(r2) && r2 < 0) {
+    PyErr_SetString(PyExc_TypeError, "Cylinder r2 must not be negative");
+    return NULL;
+  }
+  if(!isnan(d1) && d1 < 0) {
+    PyErr_SetString(PyExc_TypeError, "Cylinder d1 must not be negative");
+    return NULL;
+  }
+  if(!isnan(d2) && d2 < 0) {
+    PyErr_SetString(PyExc_TypeError, "Cylinder d2 must not be negative");
+    return NULL;
+  }
+
+  if (!isnan(r1) && !isnan(r2)) {
+    vr1 = r1; vr2 = r2;
+  } else if (!isnan(r1) && isnan(r2)) {
+    vr1 = r1; vr2 = r1;
+  } else if (!isnan(d1) && !isnan(d2)) {
+    vr1 = d1 / 2.0; vr2 = d2 / 2.0;
+  } else if (!isnan(r)) {
+    vr1 = r; vr2 = r;
+  } else if (!isnan(d)) {
+    vr1 = d / 2.0; vr2 = d / 2.0;
+  }
+
+  get_fnas(node->fn, node->fa, node->fs);
+  if (!isnan(fn)) node->fn = fn;
+  if (!isnan(fa)) node->fa = fa;
+  if (!isnan(fs)) node->fs = fs;
+
+  node->r1 = vr1;
+  node->r2 = vr2;
+  node->h = vh;
+
+  if (center == Py_True) node->center = 1;
+  else if (center == Py_False || center == NULL )  node->center = 0;
+  else {
+      PyErr_SetString(PyExc_TypeError, "Unknown Value for center parameter");
+      return NULL;
+  }
+
+  return PyOpenSCADObjectFromNode(&PyOpenSCADType, node);
+}
+
+PyObject *python_rotate_sub(PyObject *obj, Vector3d vec3, double angle)
+{
+  Matrix3d M;
+  if(isnan(angle)) {	
+    double sx = 0, sy = 0, sz = 0;
+    double cx = 1, cy = 1, cz = 1;
+    double a = 0.0;
+    if(vec3[2] != 0) {
+      a = vec3[2];
+      sz = sin_degrees(a);
+      cz = cos_degrees(a);
+    }
+    if(vec3[1] != 0) {
+      a = vec3[1];
+      sy = sin_degrees(a);
+      cy = cos_degrees(a);
+    }
+    if(vec3[0] != 0) {
+      a = vec3[0];
+      sx = sin_degrees(a);
+      cx = cos_degrees(a);
+    }
+
+    M << cy * cz,  cz *sx *sy - cx * sz,   cx *cz *sy + sx * sz,
+      cy *sz,  cx *cz + sx * sy * sz,  -cz * sx + cx * sy * sz,
+      -sy,       cy *sx,                  cx *cy;
+  } else {
+    M = angle_axis_degrees(angle, vec3);
+  }
+
+  DECLARE_INSTANCE
+  auto node = std::make_shared<TransformNode>(instance, "rotate");
+
+  std::shared_ptr<AbstractNode> child = PyOpenSCADObjectToNodeMulti(obj);
+  if (child == NULL) {
+    PyErr_SetString(PyExc_TypeError, "Invalid type for Object in rotate");
+    return NULL;
+  }
+  node->matrix.rotate(M);
+
+  node->children.push_back(child);
+  PyObject *pyresult =  PyOpenSCADObjectFromNode(&PyOpenSCADType, node);
+  return pyresult;
+}
+
+PyObject *python_rotate_core(PyObject *obj, PyObject *val_a, PyObject *val_v)
+{
+  Vector3d vec3(0,0,0);
+  double angle;
+  if (PyList_Check(val_a) && val_v == nullptr) {
+    if(PyList_Size(val_a) >= 1) vec3[0]= PyFloat_AsDouble(PyList_GetItem(val_a, 0));
+    if(PyList_Size(val_a) >= 2) vec3[1]= PyFloat_AsDouble(PyList_GetItem(val_a, 1));
+    if(PyList_Size(val_a) >= 3) vec3[2]= PyFloat_AsDouble(PyList_GetItem(val_a, 2));
+    return python_rotate_sub(obj, vec3, NAN);
+  } else if (!python_numberval(val_a,&angle) && PyList_Check(val_v) && PyList_Size(val_v) == 3) {
+    vec3[0]= PyFloat_AsDouble(PyList_GetItem(val_v, 0));
+    vec3[1]= PyFloat_AsDouble(PyList_GetItem(val_v, 1));
+    vec3[2]= PyFloat_AsDouble(PyList_GetItem(val_v, 2));
+    return python_rotate_sub(obj, vec3, angle);
+  }
+  return obj;
+}
+
+PyObject *python_rotate(PyObject *self, PyObject *args, PyObject *kwargs)
+{
+  char *kwlist[] = {"obj", "a", "v", nullptr};
+  PyObject *val_a = nullptr;
+  PyObject *val_v = nullptr;
+  PyObject *obj = nullptr;
+  if (!PyArg_ParseTupleAndKeywords(args, kwargs, "OO|O", kwlist, &obj, &val_a, &val_v)) {
+    PyErr_SetString(PyExc_TypeError, "Error during parsing rotate(object, vec3)");
+    return NULL;
+  }
+  return python_rotate_core(obj, val_a, val_v);
+}
+
+PyObject *python_oo_rotate(PyObject *obj, PyObject *args, PyObject *kwargs)
+{
+  char *kwlist[] = {"a", "v", nullptr};
+  PyObject *val_a = nullptr;
+  PyObject *val_v = nullptr;
+  if (!PyArg_ParseTupleAndKeywords(args, kwargs, "O|O", kwlist, &val_a, &val_v)) {
+    PyErr_SetString(PyExc_TypeError, "Error during parsing rotate(object, vec3)");
+    return NULL;
+  }
+  return python_rotate_core(obj, val_a, val_v);
+}
+
 PyObject *python_nb_sub_vec3(PyObject *arg1, PyObject *arg2, int mode);
 PyObject *python_translate_core(PyObject *obj, PyObject *v) 
 {
@@ -134,6 +362,81 @@ PyObject *python_oo_show(PyObject *obj, PyObject *args, PyObject *kwargs)
     return NULL;
   }
   return python_show_core(obj);
+}
+
+PyObject *python_color_core(PyObject *obj, PyObject *color, double alpha)
+{
+  std::shared_ptr<AbstractNode> child;
+  child = PyOpenSCADObjectToNodeMulti(obj);
+  if (child == NULL) {
+    PyErr_SetString(PyExc_TypeError, "Invalid type for Object in color");
+    return NULL;
+  }
+  DECLARE_INSTANCE
+  auto node = std::make_shared<ColorNode>(instance);
+
+  Vector4d col(0,0,0,alpha);
+  if(!python_vectorval(color, 3, 4, &col[0], &col[1], &col[2], &col[3])) {
+	  for(int i=0;i<4;i++) node->color[i] = col[i];
+  }
+  else if(PyUnicode_Check(color)) {
+    PyObject* value = PyUnicode_AsEncodedString(color, "utf-8", "~");
+    char *colorname =  PyBytes_AS_STRING(value);
+    boost::algorithm::to_lower(colorname);
+    if (webcolors.find(colorname) != webcolors.end()) {
+      node->color = webcolors.at(colorname);
+      node->color[3]=alpha;
+    } else {
+    // Try parsing it as a hex color such as "#rrggbb".
+      const auto hexColor = OpenSCAD::parse_hex_color(colorname);
+      if (hexColor) {
+        node->color = *hexColor;
+      } else {
+	printf("Error created\n");
+        PyErr_SetString(PyExc_TypeError, "Cannot parse color");
+        return NULL;
+      }
+    }
+  } else {
+    PyErr_SetString(PyExc_TypeError, "Unknown color representation");
+    return nullptr;
+  }
+	
+  node->children.push_back(child);
+
+  PyObject *pyresult = PyOpenSCADObjectFromNode(&PyOpenSCADType, node);
+  return pyresult;
+
+}
+
+PyObject *python_color(PyObject *self, PyObject *args, PyObject *kwargs)
+{
+  char *kwlist[] = {"obj", "c", "alpha", NULL};
+  PyObject *obj = NULL;
+  PyObject *color = NULL;
+  double alpha = 1.0;
+  if (!PyArg_ParseTupleAndKeywords(args, kwargs, "O|Od", kwlist,
+                                   &obj,
+                                   &color, &alpha
+                                   )) {
+    PyErr_SetString(PyExc_TypeError, "error during parsing color");
+    return NULL;
+  }
+  return python_color_core(obj, color, alpha);
+}
+
+PyObject *python_oo_color(PyObject *obj, PyObject *args, PyObject *kwargs)
+{
+  char *kwlist[] = {"c", "alpha", NULL};
+  PyObject *color = NULL;
+  double alpha = 1.0;
+  if (!PyArg_ParseTupleAndKeywords(args, kwargs, "|Od", kwlist,
+                                   &color, &alpha
+                                   )) {
+    PyErr_SetString(PyExc_TypeError, "error during parsing color");
+    return NULL;
+  }
+  return python_color_core(obj, color, alpha);
 }
 
 PyObject *python_csg_sub(PyObject *self, PyObject *args, PyObject *kwargs, OpenSCADOperator mode)
@@ -338,7 +641,11 @@ PyObject *python_nb_and(PyObject *arg1, PyObject *arg2) { return python_nb_sub(a
 
 PyMethodDef PyOpenSCADFunctions[] = {
   {"cube", (PyCFunction) python_cube, METH_VARARGS | METH_KEYWORDS, "Create Cube."},
+  {"cylinder", (PyCFunction) python_cylinder, METH_VARARGS | METH_KEYWORDS, "Create Cylinder."},
+  {"sphere", (PyCFunction) python_sphere, METH_VARARGS | METH_KEYWORDS, "Create Sphere."},
   {"translate", (PyCFunction) python_translate, METH_VARARGS | METH_KEYWORDS, "Move  Object."},
+  {"rotate", (PyCFunction) python_rotate, METH_VARARGS | METH_KEYWORDS, "Rotate Object."},
+  {"color", (PyCFunction) python_color, METH_VARARGS | METH_KEYWORDS, "Color Object."},
   {"show", (PyCFunction) python_show, METH_VARARGS | METH_KEYWORDS, "Show the result."},
   {"union", (PyCFunction) python_union, METH_VARARGS | METH_KEYWORDS, "Union Object."},
   {"difference", (PyCFunction) python_difference, METH_VARARGS | METH_KEYWORDS, "Difference Object."},
@@ -350,10 +657,14 @@ PyMethodDef PyOpenSCADFunctions[] = {
   {#name, (PyCFunction) python_oo_##name, METH_VARARGS | METH_KEYWORDS, desc},
 
 PyMethodDef PyOpenSCADMethods[] = {
-  OO_METHOD_ENTRY(translate,"Move Object")	
+  OO_METHOD_ENTRY(translate,"Move Object")
+  OO_METHOD_ENTRY(rotate,"Rotate Object")	
+
   OO_METHOD_ENTRY(union,"Union Object")	
   OO_METHOD_ENTRY(difference,"Difference Object")	
-  OO_METHOD_ENTRY(intersection,"Intersection Object")	
+  OO_METHOD_ENTRY(intersection,"Intersection Object")
+  OO_METHOD_ENTRY(color,"Color Object")	
+
   OO_METHOD_ENTRY(show,"Show Object")	
   {NULL, NULL, 0, NULL}
 };

commit 832c1bdca8ed406155b246b657c3c84dd7368acb

python: Fix `fs` is required when generating some objects
The `get_fnas` function accidentally tries to load `fs` even if it is
not defined, resulting in an exception when generating some objects
like sphere() or text().

Fixes: c3d173854efafdc95aa9ff962e5a69b69e7f1590

src/python/pyopenscad.cc
@@ -276,8 +276,8 @@ void get_fnas(double& fn, double& fa, double& fs)
     }
   }
 
-  PyObjectUniquePtr varFs(PyObject_GetAttrString(mainModule, "fs"), PyObjectDeleter);
   if (PyObject_HasAttrString(mainModule, "fs")) {
+    PyObjectUniquePtr varFs(PyObject_GetAttrString(mainModule, "fs"), PyObjectDeleter);
     if (varFs.get() != nullptr) {
       fs = PyFloat_AsDouble(varFs.get());
     }