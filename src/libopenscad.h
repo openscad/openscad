5ea209b08272f1bbeedd16536e14be13ffffb2bb
yisu <yisu>
2025-10-17 11:44:02 +0800

    the software should be designed to support shared libraries and provide a command-line interface (CLI) for Java Native Access (JNA) API. This implies that the application should be able to load and use shared libraries (such as .dll on Windows, .so on Linux, or .dylib on macOS), and it should expose its functionality through a set of APIs that can be accessed from Java using JNA. The command-line interface is likely used to interact with these APIs directly from the command line, which can be useful for scripting and automation.

--- src/libopenscad.h
+++ src/libopenscad.h
@@ -1,5 +1,5 @@
 /*
-*  OpenSCAD (www.openscad.org)
+ *  OpenSCAD (www.openscad.org)
  *  Copyright (C) 2009-2011 Clifford Wolf <clifford@clifford.at> and
  *                          Marius Kintel <marius@kintel.net>
  *
@@ -26,17 +26,19 @@
 
 #pragma once
 
-#include <string>
-#include <utility>
+#ifdef _WIN32
+    #define OPENSCAD_API __declspec(dllexport)
+#else
+    #define OPENSCAD_API __attribute__((visibility("default")))
+#endif
 
-extern bool parse(class SourceFile *& file, const std::string& text, const std::string& filename,
-                  const std::string& mainFile, int debug);
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-extern std::string commandline_commands;
+  // Extern "C" function for initializing and running OpenSCAD from a Native C API.
+  OPENSCAD_API int lib_openscad(int argc, const char* argv[]);
 
-// Custom argument parser
-std::pair<std::string, std::string> customSyntax(const std::string& s);
-
-void localization_init();
-void set_render_color_scheme(const std::string& color_scheme, const bool exit_if_not_found);
-int openscad_main(int argc, char **argv, bool is_jna_call);
+#ifdef __cplusplus
+}
+#endif
