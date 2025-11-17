75949547f394d818811109fa410023bbec264e79
Cory Cross <github@corycross.org>
2025-11-16 23:41:04 -0800

    The icons are back in the executable!
    Apparently cmake projects using a static library must use extern on
    and call the qInitResources_*() functions for each qrc added.

--- src/main_wrapper.cc
+++ src/main_wrapper.cc
@@ -1,3 +1,17 @@
 extern int openscad_main(int argc, char **argv);
+extern int qInitResources_common();
+extern int qInitResources_icons_chokusen();
+extern int qInitResources_icons_chokusen_dark();
+extern int qInitResources_mac();
 
-int main(int argc, char **argv) { return openscad_main(argc, argv); }
+int main(int argc, char **argv) {
+#ifndef OPENSCAD_NOGUI
+  (void)qInitResources_common();
+  (void)qInitResources_icons_chokusen();
+  (void)qInitResources_icons_chokusen_dark();
+#endif
+#ifdef __APPLE__
+  (void)qInitResources_mac();
+#endif
+  return openscad_main(argc, argv);
+}
