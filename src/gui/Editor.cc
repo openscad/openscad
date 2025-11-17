0a3ce732f69d49059c12c7e13d3f492433cead69
Alex Kent Hajnal <software@alephnull.net>
2025-11-17 04:12:46 -0500

    Enables "Ctrl/Cmd-Mouse-wheel zooms text" config option to work  (Preliminary fix for #6368)

--- src/gui/Editor.cc
+++ src/gui/Editor.cc
@@ -9,8 +9,11 @@ void EditorInterface::wheelEvent(QWheelEvent *event)
   QSettingsCached settings;
   bool wheelzoom_enabled = GlobalPreferences::inst()->getValue("editor/ctrlmousewheelzoom").toBool();
   if ((event->modifiers() == Qt::ControlModifier) && wheelzoom_enabled) {
-    if (event->angleDelta().y() > 0) zoomIn();
-    else if (event->angleDelta().y() < 0) zoomOut();
+    // Zooming is already being handled internally by QScintilla/Scintilla/Qt.
+    // Having the zoomIn() and zoomOut() calls here appears to simply double the
+    // amount of zoom applied.
+//    if (event->angleDelta().y() > 0) zoomIn();
+//    else if (event->angleDelta().y() < 0) zoomOut();
   } else {
     QWidget::wheelEvent(event);
   }
