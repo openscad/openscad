a0e2804cc77640a80e08f6850e3854e178b1eb23
Alex Kent Hajnal <software@alephnull.net>
2025-11-17 15:27:10 -0500

    Entirely removed superfluous EditorInterface::wheelEvent method

--- src/gui/Editor.cc
+++ src/gui/Editor.cc
@@ -1,20 +1,4 @@
 #include "gui/Editor.h"
-#include <QWheelEvent>
 #include <QWidget>
 #include "gui/Preferences.h"
 #include "gui/QSettingsCached.h"
-
-void EditorInterface::wheelEvent(QWheelEvent *event)
-{
-  QSettingsCached settings;
-  bool wheelzoom_enabled = GlobalPreferences::inst()->getValue("editor/ctrlmousewheelzoom").toBool();
-  if ((event->modifiers() == Qt::ControlModifier) && wheelzoom_enabled) {
-    // Zooming is already being handled internally by QScintilla/Scintilla/Qt.
-    // Having the zoomIn() and zoomOut() calls here appears to simply double the
-    // amount of zoom applied.
-//    if (event->angleDelta().y() > 0) zoomIn();
-//    else if (event->angleDelta().y() < 0) zoomOut();
-  } else {
-    QWidget::wheelEvent(event);
-  }
-}
