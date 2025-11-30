26ee72ed171dd4a238d10a682074b21d81475835
Cory Cross <github@corycross.org>
2025-11-30 12:46:40 -0800

    Add unit test comparing volume of Manifold and CGAL linear_extrude

--- src/geometry/linear_extrude_test.cc
+++ src/geometry/linear_extrude_test.cc
@@ -11,12 +11,20 @@
 #include <vector>
 
 #include <boost/logic/tribool.hpp>
+#ifdef ENABLE_CGAL
+#include <CGAL/boost/graph/convert_nef_polyhedron_to_polygon_mesh.h>
+#include <CGAL/Polygon_mesh_processing/measure.h>
+#include <CGAL/Polygon_mesh_processing/orientation.h>
+#include <CGAL/Polygon_mesh_processing/triangulate_faces.h>
+#include <CGAL/Surface_mesh.h>
+#endif
 
 #include "geometry/Geometry.h"
 #include "geometry/linalg.h"
 #include "geometry/GeometryUtils.h"
 #include "glview/RenderSettings.h"
 #include "core/LinearExtrudeNode.h"
+#include "geometry/cgal/cgalutils.h"
 #include "geometry/manifold/manifoldutils.h"
 #include "geometry/PolySet.h"
 #include "geometry/PolySetBuilder.h"
@@ -34,6 +42,11 @@ void prepareVerticesAndIndices(const Polygon2d& polyref, Vector3d h1, Vector3d h
                                double scale_x, double scale_y, double twist,
                                std::vector<Vector3d>& vertices, PolygonIndices& indices,
                                int& slice_stride);
+std::unique_ptr<PolySet> assemblePolySetForCGAL(const Polygon2d& polyref,
+                                                std::vector<Vector3d>& vertices, PolygonIndices& indices,
+                                                int convexity, boost::tribool isConvex, double scale_x,
+                                                double scale_y, const Vector3d& h1, const Vector3d& h2,
+                                                double twist);
 }  // namespace LinearExtrudeInternals
 
 Polygon2d makeSquare(double s)
@@ -53,6 +66,8 @@ Polygon2d makeCross(double s)
                 {s, s}, {s / 2, s * 3 / 4}, {0, s}, {s / 4, s / 2}};
   return std::move(Polygon2d(o));
 }
+
+#ifdef ENABLE_MANIFOLD
 
 TEST_CASE("assemblePolySetForManifold passes some basic checks")
 {
@@ -86,3 +101,54 @@ TEST_CASE("assemblePolySetForManifold passes some basic checks")
     CHECK(geom1.Volume() == geom2.Volume());
   }
 }
+
+#ifdef ENABLE_CGAL
+
+TEST_CASE("linear_extrude gives essentially the same results for Manifold and CGAL")
+{
+  auto polyref = makeCross(3.0);
+
+  Vector3d h1 = Vector3d::Zero();
+  Vector3d h2 = Vector3d(0, 0, 5.0);
+  double twist = 0;
+
+  int num_slices = 2;
+  int slice_stride = 0;
+  std::vector<Vector3d> vertices;
+  PolygonIndices indices;
+  LinearExtrudeInternals::prepareVerticesAndIndices(polyref, h1, h2, num_slices, 1.0, 1.0, twist,
+                                                    vertices, indices, slice_stride);
+
+  SECTION("both measure about the same volume")
+  {
+    std::vector<Vector3d> vertices_copy = vertices;
+    PolygonIndices indices_copy = indices;
+    auto mps = LinearExtrudeInternals::assemblePolySetForManifold(
+      polyref, std::move(vertices_copy), std::move(indices_copy), 1, unknown, slice_stride * num_slices);
+    auto mgeom = ManifoldUtils::createManifoldFromPolySet(*mps)->getManifold();
+    auto mvol = mgeom.Volume();
+    CHECK(mvol == 22.5);
+
+    auto cps = LinearExtrudeInternals::assemblePolySetForCGAL(polyref, vertices, indices, 1, unknown,
+                                                              1.0, 1.0, h1, h2, twist);
+    // This works too and has some repair mechanisms for bad inputs, which is not a problem here.
+    // auto nef_shape = *CGALUtils::createNefPolyhedronFromPolySet(*cps)->p3;
+    // CGAL::Surface_mesh<CGAL_Point_3> mesh;
+    // CGAL::convert_nef_polyhedron_to_polygon_mesh(
+    //   nef_shape,
+    //   mesh,
+    //   true /* triangulate_all_faces = true */
+    //   );
+    // namespace PMP = CGAL::Polygon_mesh_processing;
+    // PMP::orient(mesh);
+
+    auto mesh = *CGALUtils::createSurfaceMeshFromPolySet<CGAL_Kernel3Mesh>(*cps);
+    if (!CGAL::is_triangle_mesh(mesh)) {
+      CGAL::Polygon_mesh_processing::triangulate_faces(mesh);
+    }
+    auto cvol = CGAL::Polygon_mesh_processing::volume(mesh);
+    CHECK(mvol == cvol);
+  }
+}
+#endif  // ENABLE_CGAL
+#endif  // ENABLE_MANIFOLD
