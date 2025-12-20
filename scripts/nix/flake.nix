07fcc8f1f187639e8dfdce30dd3db7f260c975b6
Flaviu Tamas <me@flaviutamas.com>
2025-12-20 17:29:32 -0500

    use stable release

--- scripts/nix/flake.nix
+++ scripts/nix/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     flake-parts.url = "github:hercules-ci/flake-parts";
     treefmt-nix.url = "github:numtide/treefmt-nix";
   };
