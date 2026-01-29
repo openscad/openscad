701fbf767aaebe2834336cc3e2a7404c5b671f10
dasbidyendu <143386290+dasbidyendu@users.noreply.github.com>
2026-01-30 00:57:01 +0530

    Style: wrap connect() call in MainWindow.cc to satisfy clang-format check

--- src/gui/MainWindow.cc
+++ src/gui/MainWindow.cc
@@ -343,7 +343,8 @@ MainWindow::MainWindow(const QStringList& filenames) : rubberBandManager(this)
 
   connect(this->console, &Console::openWindowRequested, this, &MainWindow::showLink);
   connect(GlobalPreferences::inst(), &Preferences::consoleFontChanged, this->console, &Console::setFont);
-  connect(GlobalPreferences::inst(), &Preferences::consoleFontChanged, this->console, &Console::setConsoleFont);
+  connect(GlobalPreferences::inst(), &Preferences::consoleFontChanged, this->console,
+          &Console::setConsoleFont);
   this->console->setConsoleFont(
     GlobalPreferences::inst()->getValue("advanced/consoleFontFamily").toString(),
     GlobalPreferences::inst()->getValue("advanced/consoleFontSize").toUInt());
