21f7bf8dcefdeb482cf6e22c068f1adf72b5115e
mconsidine <matt@considine.net>
2026-02-01 20:13:29 -0500

    Add support for DXF export in R14 format

--- src/io/export.cc
+++ src/io/export.cc
@@ -216,6 +216,9 @@ static void exportFile(const std::shared_ptr<const Geometry>& root_geom, std::os
   case FileFormat::WRL:        export_wrl(root_geom, output); break;
   case FileFormat::AMF:        export_amf(root_geom, output); break;
   case FileFormat::_3MF:       export_3mf(root_geom, output, exportInfo); break;
+#ifdef ENABLE_R14
+  case FileFormat::DXF:        export_dxf_R14(root_geom, output); break;
+#endif
   case FileFormat::DXF:        export_dxf(root_geom, output); break;
   case FileFormat::SVG:        export_svg(root_geom, output, exportInfo); break;
   case FileFormat::PDF:        export_pdf(root_geom, output, exportInfo); break;
