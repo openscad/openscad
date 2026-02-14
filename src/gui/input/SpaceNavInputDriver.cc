c688556617ab865baba7bdc8b769fea308b60b45
Torsten Paul <Torsten.Paul@gmx.de>
2026-02-14 21:12:04 +0100

    Rollback header file sorting for spnav driver.
    Temporary revert as spnav.h brings in X11 headers that pollutes the global
    namespace by #defining Status causing build failure with spnav enabled.

--- src/gui/input/SpaceNavInputDriver.cc
+++ src/gui/input/SpaceNavInputDriver.cc
@@ -30,16 +30,14 @@
  */
 
 #include "gui/input/SpaceNavInputDriver.h"
-
-#include <spnav.h>
-#include <unistd.h>
-
-#include <QThread>
-#include <string>
-
 #include "gui/input/InputDriverEvent.h"
 #include "gui/input/InputDriverManager.h"
 #include "utils/printutils.h"
+
+#include <QThread>
+#include <spnav.h>
+#include <unistd.h>
+#include <string>
 
 void SpaceNavInputDriver::run()
 {
