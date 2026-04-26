9540445f36813d5fe5a7c9953c587db7c41d56bb
Doug Colaizzo <dcolaizzo@gmail.com>
2026-04-26 00:19:37 -0700

    Fix headless Linux startup-open test

--- src/gui/OpenSCADApp_test.cc
+++ src/gui/OpenSCADApp_test.cc
@@ -1,9 +1,40 @@
 #include "gui/OpenSCADApp.h"
 
+#include <QByteArray>
+
 #include <catch2/catch_all.hpp>
+
+namespace {
+class ScopedQtPlatformOverride
+{
+public:
+  ScopedQtPlatformOverride(const char *platform)
+    : previousValue(qgetenv("QT_QPA_PLATFORM")), hadPreviousValue(!this->previousValue.isNull())
+  {
+    qputenv("QT_QPA_PLATFORM", platform);
+  }
+
+  ~ScopedQtPlatformOverride()
+  {
+    if (this->hadPreviousValue) {
+      qputenv("QT_QPA_PLATFORM", this->previousValue);
+    } else {
+      qunsetenv("QT_QPA_PLATFORM");
+    }
+  }
+
+private:
+  QByteArray previousValue;
+  bool hadPreviousValue;
+};
+}
 
 TEST_CASE("OpenSCADApp queues multiple startup file opens", "[gui][startup_open]")
 {
+  // This QApplication-based unit test only exercises startup queueing logic,
+  // so force a headless Qt platform instead of depending on X11/Wayland.
+  ScopedQtPlatformOverride platformOverride("offscreen");
+
   int argc = 1;
   char arg0[] = "OpenSCADUnitTests";
   char *argv[] = {arg0, nullptr};
