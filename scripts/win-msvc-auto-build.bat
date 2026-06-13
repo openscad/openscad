8da7aaa6129d956d9418df6e8665b596f5493616
ddayjohn <shokekub@gmail.com>
2026-06-13 10:30:51 +0700

    Update Script win-msvc-auto-buid.bat

--- scripts/win-msvc-auto-build.bat
+++ scripts/win-msvc-auto-build.bat
@@ -10,29 +10,42 @@ set "BUILD_TYPE=Release"
 
 set "MODE_MANIFEST=ON"
 set "VCPKG_TARGET_TRIPLET=x64-windows"
-set "MSVC_TOOLS=Visual Studio 17 2022"
-rem set "MSVC_TOOLS=Visual Studio 18 2026"
 
-rem ---------------------------------------------------------------------------
+set "CMAKE_VS_GENERATOR=Visual Studio 17 2022"
+rem set "CMAKE_VS_GENERATOR=Visual Studio 18 2026"
+rem set "CMAKE_VS_GENERATOR=Ninja"
+rem set "CMAKE_VS_GENERATOR="
+
+rem Note -----------------------------------------------------------
+rem if CMAKE_VS_GENERATOR is empty ,it will try auto-detct
+rem for Ninja ,must have enveronments before this script ,like open this script in x64-msvc-developer-prompt 
+rem End Config---------------------------------------------------------------------------
 
 
 rem check Visual Studio Tool
 echo [INFO] Checking for Visual Studio (MSVC Compiler)...
 
-@echo off
+set "VS_STUDIO_VERSION="
 
-if not exist "%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" goto ERROR_NO_VS
+if "%CMAKE_VS_GENERATOR%" equ "" (
+    for /f "usebackq delims=. tokens=1" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationVersion`) do set "VS_STUDIO_VERSION=%%i"
+)
 
-for /f "usebackq tokens=*" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property displayName`) do set "VS_NAME=%%i"
+if "%VS_STUDIO_VERSION%"=="18" (
+    echo [INFO] Auto-Detect MSVC Version %VS_STUDIO_VERSION%
+    set "CMAKE_VS_GENERATOR=Visual Studio 18 2026"
+) else if "%VS_STUDIO_VERSION%"=="17" (
+    echo [INFO] Auto-Detect MSVC Version %VS_STUDIO_VERSION%
+    set "CMAKE_VS_GENERATOR=Visual Studio 17 2022"
+)
 
-if "%VS_NAME%"=="" goto ERROR_NO_VS
 
-echo [SUCCESS] Found %VS_NAME%. Ready to compile.
+if "%CMAKE_VS_GENERATOR%" equ "" (
+    echo [ERROR] No build tools found!
+    exit /b 1 
+)
 
-:ERROR_NO_VS
-echo [ERROR] Visual Studio C++ build tools (MSVC) not found!
-echo         Please install Visual Studio with "Desktop development with C++" workload.
-exit /b 1
+echo [INFO] CMake Generator : "%CMAKE_VS_GENERATOR%" Ready to compile.
 
 echo ---------------------------------------------------------
 
@@ -77,23 +90,23 @@ if %errorlevel% neq 0 (
 
 
 rem Find Path ---------------------------------------------------------
-for %%i in ("%~dp0..") do set "OPENSCAD_DIR=%%~fi"
-for %%i in ("%~dp0..\..") do set "ROOT_DIR=%%~fi"
-set "VCPKG_DIR=%ROOT_DIR%\vcpkg"
-set "BUILD_DIR=%OPENSCAD_DIR%\%BUILD_NAME%"
-set "WINFLEXBISON_DIR=%ROOT_DIR%\winflexbison3"
+for %%i in ("%~dp0..") do set "OPENSCAD_PATH=%%~fi"
+for %%i in ("%~dp0..\..") do set "ROOT_PATH=%%~fi"
+set "VCPKG_PATH=%ROOT_PATH%\vcpkg"
+set "BUILD_PATH=%OPENSCAD_PATH%\%BUILD_NAME%"
+set "WINFLEXBISON_PATH=%ROOT_PATH%\winflexbison3"
 
 echo Current Script Folder : %~dp0
-echo Openscad Root Folder  : %OPENSCAD_DIR%
-echo VCPKG Folder Location : %VCPKG_DIR%
-echo Build Folder Location : %BUILD_DIR%
+echo Openscad Root Location : %OPENSCAD_PATH%
+echo VCPKG Folder Location : %VCPKG_PATH%
+echo Build Folder Location : %BUILD_PATH%
 echo Manifest Mode         : %MODE_MANIFEST%
 echo ---------------------------------------------------------
 
 rem  Install openscad submodules
 rem  ---------------------------------------------------------
 echo [INFO] Updating git submodules in openscad...
-cd /d "%OPENSCAD_DIR%"
+cd /d "%OPENSCAD_PATH%"
 git submodule update --init --recursive
 
 if %errorlevel% neq 0 (
@@ -105,19 +118,19 @@ echo ---------------------------------------------------------
 
 rem  Install VCPKG
 rem  ---------------------------------------------------------
-if exist "%VCPKG_DIR%\" (
+if exist "%VCPKG_PATH%\" (
     echo [INFO] Found vcpkg directory. Skipping.
 ) else (
     echo [WARN] vcpkg not found. Cloning repository...
     
-    cd /d "%ROOT_DIR%"
+    cd /d "%ROOT_PATH%"
     git clone https://github.com/microsoft/vcpkg.git
     
     if %errorlevel% equ 0 (
         echo [SUCCESS] vcpkg cloned successfully.
         
         echo [INFO] Bootstrapping vcpkg...
-        cd /d "%VCPKG_DIR%"
+        cd /d "%VCPKG_PATH%"
         call .\bootstrap-vcpkg.bat
         
         if %errorlevel% equ 0 (
@@ -144,12 +157,12 @@ if %SYSTEM_CHECK_BISON_ELV% equ 0 (
     echo [SUCCESS] winflexbison found in system PATH. Skipping auto-install.
 ) else (
     rem Download if not exists
-    if not exist "%WINFLEXBISON_DIR%\win_flex.exe" (
+    if not exist "%WINFLEXBISON_PATH%\win_flex.exe" (
         echo [WARN] winflexbison not found anywhere. Triggering auto-install...
-        mkdir "%WINFLEXBISON_DIR%" 2>nul    
-        curl -L -o "%ROOT_DIR%\win_flex_bison.zip" "https://github.com/lexxmark/winflexbison/releases/download/v2.5.25/win_flex_bison-2.5.25.zip"
-        tar -xf "%ROOT_DIR%\win_flex_bison.zip" -C "%WINFLEXBISON_DIR%"
-        del "%ROOT_DIR%\win_flex_bison.zip" 2>nul
+        mkdir "%WINFLEXBISON_PATH%" 2>nul    
+        curl -L -o "%ROOT_PATH%\win_flex_bison.zip" "https://github.com/lexxmark/winflexbison/releases/download/v2.5.25/win_flex_bison-2.5.25.zip"
+        tar -xf "%ROOT_PATH%\win_flex_bison.zip" -C "%WINFLEXBISON_PATH%"
+        del "%ROOT_PATH%\win_flex_bison.zip" 2>nul
     ) else (
         echo [SUCCESS] winflexbison found in managed folder.
     )
@@ -157,8 +170,8 @@ if %SYSTEM_CHECK_BISON_ELV% equ 0 (
 
 rem set path if necessary
 if %SYSTEM_CHECK_BISON_ELV% neq 0 (
-    if exist "%WINFLEXBISON_DIR%\win_flex.exe" (
-        set "PATH=%PATH%;%WINFLEXBISON_DIR%"
+    if exist "%WINFLEXBISON_PATH%\win_flex.exe" (
+        set "PATH=%PATH%;%WINFLEXBISON_PATH%"
     )
 )
 
@@ -176,15 +189,18 @@ if %errorlevel% equ 0 (
 
 echo ---------------------------------------------------------
 
-rem  Prepare build-dir
+echo [INFO] Enter into Openscad
+cd /d "%OPENSCAD_PATH%"
+
+rem  Prepare build folder
 rem  ---------------------------------------------------------
-if exist "%BUILD_DIR%\" (
-    echo [WARN] Old build directory found. Removing: %BUILD_DIR%
-    rmdir /s /q "%BUILD_DIR%"
+if exist "%BUILD_NAME%\" (
+    echo [WARN] Old build directory found. Removing: %BUILD_NAME%
+    rmdir /s /q "%BUILD_NAME%"
 )
 
-echo [INFO] Creating build directory: %BUILD_DIR%
-mkdir "%BUILD_DIR%"
+echo [INFO] Creating build directory: %BUILD_NAME%
+mkdir "%BUILD_NAME%"
 
 
 rem  CMake Configuration
@@ -200,13 +216,13 @@ if /i "%BUILD_GUI%"=="ON" (
 
 echo [INFO] Running CMake configuration...
 
-cmake -B %BUILD_DIR% -G %MSVC_TOOLS% -A x64 -DCMAKE_BUILD_TYPE=%BUILD_TYPE% ^
+cmake -B "%BUILD_NAME%" -G "%CMAKE_VS_GENERATOR%" -A x64 -DCMAKE_BUILD_TYPE=%BUILD_TYPE% ^
 -DVCPKG_TARGET_TRIPLET=%VCPKG_TARGET_TRIPLET% ^
 -DHEADLESS=%EN_HEADLESS% -DUSE_QT6=%EN_QT% -DENABLE_TESTS=%BUILD_TEST% ^
 -DENABLE_MANIFOLD=ON ^
 -DENABLE_CAIRO=ON ^
 -DUSE_BUILTIN_OPENCSG=ON -DUSE_BUILTIN_MANIFOLD=ON -DUSE_BUILTIN_CLIPPER2=ON ^
--DCMAKE_TOOLCHAIN_FILE="%VCPKG_DIR%/scripts/buildsystems/vcpkg.cmake" ^
+-DCMAKE_TOOLCHAIN_FILE="%VCPKG_PATH%/scripts/buildsystems/vcpkg.cmake" ^
 -DCMAKE_EXE_LINKER_FLAGS="/manifest:no" -DCMAKE_MODULE_LINKER_FLAGS="/manifest:no" -DCMAKE_SHARED_LINKER_FLAGS="/manifest:no" ^
 -DVCPKG_MANIFEST_MODE=%MODE_MANIFEST%
 
@@ -221,16 +237,16 @@ echo ---------------------------------------------------------
 rem  Get Installed VCPKG PATH as manifest mode
 rem  ---------------------------------------------------------
 if /i "%MODE_MANIFEST%"=="ON" (
-    set "MY_INSTALLED_VCPKG_PATH=%BUILD_DIR%\vcpkg_installed\%VCPKG_TARGET_TRIPLET%"
+    set "MY_INSTALLED_VCPKG_PATH=%BUILD_NAME%\vcpkg_installed\%VCPKG_TARGET_TRIPLET%"
 ) else (
-    set "MY_INSTALLED_VCPKG_PATH=%VCPKG_DIR%\installed\%VCPKG_TARGET_TRIPLET%"
+    set "MY_INSTALLED_VCPKG_PATH=%VCPKG_PATH%\installed\%VCPKG_TARGET_TRIPLET%"
 )
 
 echo ---------------------------------------------------------
 
 rem  Build
 rem  ---------------------------------------------------------
-cmake --build %BUILD_DIR% --config %BUILD_TYPE%
+cmake --build %BUILD_NAME% --config %BUILD_TYPE%
 
 if %errorlevel% neq 0 (
     echo [ERROR] Build failed.
@@ -240,17 +256,17 @@ if %errorlevel% neq 0 (
 echo [INFO] Build Done.
 
 
-set "TARGET_BUILD_APP_DIR=%BUILD_DIR%\%BUILD_TYPE%"
+set "TARGET_BUILD_APP_PATH=%BUILD_NAME%\%BUILD_TYPE%"
 
 echo ---------------------------------------------------------
 
-rem  Copy Qt6 Plugins to Result Build Dir
+rem  Copy Qt6 Plugins to Result Build 
 rem  ---------------------------------------------------------
 set "QT_PLUGINS_SRC=%MY_INSTALLED_VCPKG_PATH%\Qt6\plugins"
 
 if exist "%QT_PLUGINS_SRC%\" (
     echo [INFO] Copying contents of Qt6 plugins directly into build directory...
-    xcopy "%QT_PLUGINS_SRC%" "%TARGET_BUILD_APP_DIR%" /e /i /h /y /d
+    xcopy "%QT_PLUGINS_SRC%" "%TARGET_BUILD_APP_PATH%" /e /i /h /y /d
     
     if %errorlevel% neq 0 (
         echo [ERROR] Failed to copy Qt6 plugins contents.
@@ -265,26 +281,26 @@ echo ---------------------------------------------------------
 
 
 rem Copy Qt*.dll ,when testing some .dll missing ,like Qtsvg.dll 
-set "VCPKG_BIN_DIR=%MY_INSTALLED_VCPKG_PATH%\bin"
+set "VCPKG_BIN_PATH=%MY_INSTALLED_VCPKG_PATH%\bin"
 
-if exist "%VCPKG_BIN_DIR%\" (
-    xcopy "%VCPKG_BIN_DIR%\Qt*.dll" "%TARGET_BUILD_APP_DIR%\" /i /h /y /d
+if exist "%VCPKG_BIN_PATH%\" (
+    xcopy "%VCPKG_BIN_PATH%\Qt*.dll" "%TARGET_BUILD_APP_PATH%\" /i /h /y /d
     if %errorlevel% neq 0 (
         echo [ERROR] Failed to copy Qt dll dependencies.
         exit /b 1
     )
     echo [SUCCESS] Copied Qt dll dependencies.
 ) else (
-    echo [WARN] VCPKG-bin folder not found: %VCPKG_BIN_DIR%
+    echo [WARN] VCPKG-bin folder not found: %VCPKG_BIN_PATH%
     exit /b 1
 )
 
 echo ---------------------------------------------------------
 
 
-if exist "%OPENSCAD_DIR%\shaders" (
+if exist "%OPENSCAD_PATH%\shaders" (
     echo [INFO] Copying shaders folder into build directory...
-    xcopy "%OPENSCAD_DIR%\shaders" "%TARGET_BUILD_APP_DIR%\shaders" /e /i /h /y /d
+    xcopy "%OPENSCAD_PATH%\shaders" "%TARGET_BUILD_APP_PATH%\shaders" /e /i /h /y /d
     if %errorlevel% neq 0 (
         echo [ERROR] Failed to copy shaders folder.
         exit /b 1
