f8e72329c1de0a8078ec950bb0e1d4f93d66a54f
Torsten Paul <Torsten.Paul@gmx.de>
2026-08-30 00:09:08 +0200

    OFF: Prevent unbounded allocation with bogus vertex/face count in header.

--- src/io/import_off.cc
+++ src/io/import_off.cc
@@ -23,6 +23,13 @@
 #include "geometry/linalg.h"
 #include "utils/printutils.h"
 
+// Set limit for reserving vector sizes. Using 10⁸ should be
+// plenty for reading files of 1GB. Also this is not a hard
+// limit, but an initial reserve limit preventing malformed
+// files attacking memory allocation with bogus header info.
+constexpr unsigned long max_reserve_vertices = 100'000'000;
+constexpr unsigned long max_reserve_faces = 100'000'000;
+
 // References:
 // http://www.geomview.org/docs/html/OFF.html
 
@@ -188,8 +195,8 @@ std::unique_ptr<PolySet> import_off(const std::string& filename, const Location&
   PRINTDB("%d vertices, %d faces, %d edges.", vertices_count % faces_count % edges_count);
 
   auto ps = PolySet::createEmpty();
-  ps->vertices.reserve(vertices_count);
-  ps->indices.reserve(faces_count);
+  ps->vertices.reserve(std::min(vertices_count, max_reserve_vertices));
+  ps->indices.reserve(std::min(faces_count, max_reserve_faces));
 
   while ((!f.eof()) && (vertex++ < vertices_count)) {
     if (!getline_clean("reading vertices: end of file")) {
