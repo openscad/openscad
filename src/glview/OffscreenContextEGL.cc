181f3eca4153a72e83d697334b0256916937d86f
Marius Kintel <marius@kintel.net>
2026-09-21 22:16:57 -0400

    Remove stray std::cout prints from OffscreenContextEGL.cc

--- src/glview/OffscreenContextEGL.cc
+++ src/glview/OffscreenContextEGL.cc
@@ -103,7 +103,6 @@ public:
   {
     std::set<std::string> clientExtensions;
     std::string ext = eglQueryString(EGL_NO_DISPLAY, EGL_EXTENSIONS);
-    std::cout << ext << std::endl;
     std::istringstream iss(ext);
     while (iss) {
       std::string extension;
@@ -115,7 +114,6 @@ public:
       return;
     }
 
-    std::cout << "Trying Platform display..." << std::endl;
     if (eglQueryDevicesEXT && eglGetPlatformDisplayEXT) {
       EGLDeviceEXT eglDevice;
       EGLint numDevices = 0;
@@ -192,7 +190,7 @@ std::shared_ptr<OffscreenContext> CreateOffscreenContextEGL(size_t width, size_t
 
   if (!drmNode.empty()) {
 #ifdef HAS_GBM
-    std::cout << "Using GBM..." << std::endl;
+    PRINTDB("Using GBM...");
     ctx->getDisplayFromDrmNode(drmNode);
 #endif
   } else {
@@ -200,7 +198,6 @@ std::shared_ptr<OffscreenContext> CreateOffscreenContextEGL(size_t width, size_t
     // If so, we also have to try initializing it
     ctx->findPlatformDisplay();
     if (ctx->eglDisplay == EGL_NO_DISPLAY) {
-      std::cout << "Trying default EGL display..." << std::endl;
       ctx->eglDisplay = eglGetDisplay(EGL_DEFAULT_DISPLAY);
     }
   }
@@ -227,16 +224,8 @@ std::shared_ptr<OffscreenContext> CreateOffscreenContextEGL(size_t width, size_t
   PRINTDB("GLAD: Loaded EGL %d.%d after reload",
           GLAD_VERSION_MAJOR(eglVersion) % GLAD_VERSION_MINOR(eglVersion));
 
-#ifdef EGL_MESA_query_driver
-  if (eglGetDisplayDriverName) {
-    const char *name = eglGetDisplayDriverName(ctx->eglDisplay);
-    if (name) {
-      std::cout << "Got EGL display with driver name: " << name << std::endl;
-    }
-  }
-#endif
-
   EGLint numConfigs;
+
   EGLConfig config;
   bool gotConfig = eglChooseConfig(ctx->eglDisplay, configAttribs, &config, 1, &numConfigs);
   if (!gotConfig || numConfigs == 0) {
