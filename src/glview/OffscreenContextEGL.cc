5d128cd663aae2ec0a562ff3a18e0c2dfa8df961
Marius Kintel <marius@kintel.net>
2026-09-21 22:20:17 -0400

    Use PRINTD instead of PRINTDB for single argument GBM message

--- src/glview/OffscreenContextEGL.cc
+++ src/glview/OffscreenContextEGL.cc
@@ -190,7 +190,7 @@ std::shared_ptr<OffscreenContext> CreateOffscreenContextEGL(size_t width, size_t
 
   if (!drmNode.empty()) {
 #ifdef HAS_GBM
-    PRINTDB("Using GBM...");
+    PRINTD("Using GBM...");
     ctx->getDisplayFromDrmNode(drmNode);
 #endif
   } else {
